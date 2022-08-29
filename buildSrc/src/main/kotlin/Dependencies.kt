5a3a5e81d06166200ebd682bca99069b35b1f0d2
KimBeomJin <sdpthf@naver.com>
2022-08-30 01:47:49 +0900

    build: buildSrc settnigs

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,41 @@
+object Dependencies {
+
+    // kotlin
+    const val KOTLIN_REFLECT = "org.jetbrains.kotlin:kotlin-reflect"
+    const val KOTLIN_JDK = "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
+    const val JACKSON = "com.fasterxml.jackson.module:jackson-module-kotlin"
+
+    // java servlet
+    const val JAVA_SERVLET = "javax.servlet:javax.servlet-api:${DependencyVersions.SERVLET}"
+
+    // web
+    const val SPRING_WEB = "org.springframework.boot:spring-boot-starter-web"
+
+    // validation
+    const val SPRING_VALIDATION = "org.springframework.boot:spring-boot-starter-validation"
+
+    // configuration
+    const val CONFIGURATION_PROCESSOR = "org.springframework.boot:spring-boot-configuration-processor"
+
+    // database
+    const val SPRING_DATA_JPA = "org.springframework.boot:spring-boot-starter-data-jpa"
+    const val MYSQL_CONNECTOR = "mysql:mysql-connector-java"
+    const val SPRING_REDIS = "org.springframework.boot:spring-boot-starter-data-redis"
+    const val REDIS = "org.springframework.data:spring-data-redis:${DependencyVersions.REDIS_VERSION}"
+
+    // security
+    const val SPRING_SECURITY = "org.springframework.boot:spring-boot-starter-security"
+
+    // jwt
+    const val JWT = "io.jsonwebtoken:jjwt:${DependencyVersions.JWT_VERSION}"
+
+    // mapstruct
+    const val MAPSTRUCT = "org.mapstruct:mapstruct:${DependencyVersions.MAPSTRUCT_VERSION}"
+    const val MAPSTRUCT_PROCESSER = "org.mapstruct:mapstruct-processor:${DependencyVersions.MAPSTRUCT_VERSION}"
+
+    // aws
+    const val SPRING_AWS = "org.springframework.cloud:spring-cloud-starter-aws:${DependencyVersions.AWS_VERSION}"
+
+    // test
+    const val SPRING_TEST = "org.springframework.boot:spring-boot-starter-test:${PluginVersions.SPRING_BOOT_VERSION}"
+}
---
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -0,0 +1,8 @@
+object PluginVersions {
+    const val SPRING_BOOT_VERSION = "2.7.1"
+    const val DEPENDENCY_MANAGER_VERSION = "1.0.11.RELEASE"
+    const val JVM_VERSION = "1.6.21"
+    const val SPRING_PLUGIN_VERSION = "1.6.21"
+    const val JPA_PLUGIN_VERSION = "1.6.21"
+    const val KAPT_VERSION = "1.7.10"
+}
