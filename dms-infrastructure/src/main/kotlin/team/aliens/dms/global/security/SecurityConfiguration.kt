8937d91b193440be3ab31807d66cbe3e58d1d740
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-09 17:20:49 +0900

    feat: security configuration setting

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
@@ -0,0 +1,31 @@
+package team.aliens.dms.global.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfiguration {
+
+    @Bean
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf().disable()
+            .cors()
+
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .antMatchers("*").permitAll()
+
+        return http.build()
+    }
+
+    @Bean
+    protected fun passwordEncoder() = BCryptPasswordEncoder()
+}
