04bfa7d523437aec0e87c12fdc9577486ab919b8
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-10 19:10:30 +0900

    feat: global error handler

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/error/GlobalErrorHandler.kt
@@ -0,0 +1,15 @@
+package team.aliens.dms.global.error
+
+import org.springframework.http.HttpStatus
+import org.springframework.validation.BindException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalErrorHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException::class)
+    protected fun handleBindException(e: BindException): ErrorResponse? = ErrorResponse.of(e)
+}
