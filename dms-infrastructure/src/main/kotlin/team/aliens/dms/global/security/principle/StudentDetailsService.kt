c2c7ef7d17bf20485fda393e9aa317232affe755
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-14 17:18:21 +0900

    refactor: @Service -> @Component

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/StudentDetailsService.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/StudentDetailsService.kt
@@ -2,9 +2,10 @@ package team.aliens.dms.global.security.principle
 
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
-@Service
+@Component
 class StudentDetailsService : UserDetailsService {
 
     override fun loadUserByUsername(username: String?): UserDetails {
