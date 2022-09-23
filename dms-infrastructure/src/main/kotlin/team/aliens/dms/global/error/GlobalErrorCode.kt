6cc0fb293fdb00e4a59ebd17548fbfca5a130079
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-23 16:05:00 +0900

    refactor: status code type HttpStatus -> Int

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/error/GlobalErrorCode.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/error/GlobalErrorCode.kt
@@ -1,7 +1,5 @@
 package team.aliens.dms.global.error
 
-import org.springframework.http.HttpStatus
-
 /**
  *
  * 전역으로 사용되는 ErrorCode 를 모아둔 GlobalErrorCode
@@ -11,13 +9,13 @@ import org.springframework.http.HttpStatus
  * @version 1.0.0
  **/
 enum class GlobalErrorCode(
-    private val status: HttpStatus,
+    private val status: Int,
     private val message: String
 ) : ErrorProperty {
 
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Bad Request"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+    BAD_REQUEST(404, "Bad Request"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
-    override fun status(): Int = status.value()
+    override fun status(): Int = status
     override fun message(): String = message
 }
