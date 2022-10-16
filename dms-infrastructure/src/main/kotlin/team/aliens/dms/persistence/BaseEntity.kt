bd1aaef7af11e863922ac7a2f274326d33720ce3
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-16 21:00:30 +0900

    feat: (#5) base entity 세팅

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/BaseEntity.kt
@@ -0,0 +1,16 @@
+package team.aliens.dms.persistence
+
+import com.fasterxml.uuid.Generators
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseEntity(
+
+    @Id
+    @Column(columnDefinition = "BINARY(16)", nullable = false)
+    val id: UUID = Generators.timeBasedGenerator().generate()
+
+) : BaseTimeEntity()
