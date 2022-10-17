23b4f7ea670edec4033a283bd82e58a7b5416762
KimBeomJin <sdpthf@naver.com>
2022-10-17 23:44:20 +0900

    add: (#6) RefreshTokenEntity

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/auth/model/RefreshTokenEntity.kt
@@ -0,0 +1,20 @@
+package team.aliens.dms.persistence.auth.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import java.util.*
+import javax.validation.constraints.NotNull
+
+@RedisHash("tbl_refresh_token")
+data class RefreshTokenEntity(
+    @Id
+    val token: String,
+
+    @field:NotNull
+    val userId: UUID,
+
+    @field:NotNull
+    @TimeToLive
+    val expirationTime: Int
+)
