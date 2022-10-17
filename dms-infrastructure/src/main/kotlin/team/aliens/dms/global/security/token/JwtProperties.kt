672dcfad685aef85a417fb72c0c6306ddd03e6d9
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-18 01:17:46 +0900

    feat: (#9) jwt properties

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/token/JwtProperties.kt
@@ -0,0 +1,14 @@
+package team.aliens.dms.global.security.token
+
+object JwtProperties {
+
+    const val HEADER = "Authorization"
+
+    const val PREFIX = "Bearer "
+
+    const val ACCESS = "access"
+
+    const val REFRESH = "refresh"
+
+    const val AUTHORITY = "authority"
+}
