cadc9a2f1175e4b1dc0b9f844d160bc1db163201
KimBeomJin <sdpthf@naver.com>
2022-10-18 19:41:59 +0900

    chore: (#24) /schools permission

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.global.security
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -28,8 +29,12 @@ class SecurityConfiguration(
 
         http
             .authorizeRequests()
-            .antMatchers("/managers/account-id/{school-id}").permitAll()
-            .antMatchers("*").permitAll()
+
+            // /managers
+            .antMatchers(HttpMethod.GET, "/managers/account-id/{school-id}").permitAll()
+
+            // /schools
+            .antMatchers(HttpMethod.GET, "/schools").permitAll()
 
         http
             .apply(FilterConfig(jwtParser, objectMapper))
