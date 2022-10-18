beb7cb519e76ce2778f89d9b84f52ad3d895ef27
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-18 17:31:37 +0900

    style: (#22) user email exeists exception

--- dms-domain/src/main/kotlin/team/aliens/dms/domain/user/exception/UserEmailExistsException.kt
+++ dms-domain/src/main/kotlin/team/aliens/dms/domain/user/exception/UserEmailExistsException.kt
@@ -3,6 +3,6 @@ package team.aliens.dms.domain.user.exception
 import team.aliens.dms.domain.user.error.UserErrorCode
 import team.aliens.dms.global.error.DmsException
 
-object StudentEmailExistsException : DmsException(
+object UserEmailExistsException : DmsException(
     UserErrorCode.STUDENT_EMAIL_EXISTS
 )
