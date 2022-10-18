d289a0b6e996b8553dd3ea8672898a18fac0b857
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-18 20:29:34 +0900

    feat: (#29) exists by account id port

--- dms-application/src/main/kotlin/team/aliens/dms/domain/student/spi/StudentQueryUserPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/student/spi/StudentQueryUserPort.kt
@@ -2,4 +2,5 @@ package team.aliens.dms.domain.student.spi
 
 interface StudentQueryUserPort {
     fun existsByEmail(email: String): Boolean
+    fun existsByAccountId(accountId: String): Boolean
 }
