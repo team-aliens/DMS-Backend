fc3ac141d0aadae96d8f74cf1372ed528cbb5546
이준서 <ljchan71@gmail.com>
2022-10-18 16:11:08 +0900

    feat: (#19) StringUtils

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/utils/StingUtils.kt
@@ -0,0 +1,21 @@
+package team.aliens.dms.global.utils
+
+import org.springframework.stereotype.Component
+import team.aliens.dms.global.util.CoveredEmailPort
+import java.lang.StringBuilder
+
+@Component
+class StingUtils : CoveredEmailPort {
+
+    fun coveredEmail(email: String) : String {
+        val index = email.indexOf('@')
+
+        val sb = StringBuilder(email).also {
+            for (i in (index/3) until index) {
+                it.setCharAt(i, '*')
+            }
+        }
+
+        return sb.toString();
+    }
+}
