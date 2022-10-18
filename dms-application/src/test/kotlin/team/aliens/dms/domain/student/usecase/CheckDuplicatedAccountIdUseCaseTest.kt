1d5a40d6b9369919c346ec6220d53c0b97e867ac
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-18 20:30:20 +0900

    test: (#29) check duplicated account id usecase test

---
+++ dms-application/src/test/kotlin/team/aliens/dms/domain/student/usecase/CheckDuplicatedAccountIdUseCaseTest.kt
@@ -0,0 +1,54 @@
+package team.aliens.dms.domain.student.usecase
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import team.aliens.dms.domain.student.spi.StudentQueryUserPort
+import team.aliens.dms.domain.user.exception.UserAccountIdExistsException
+
+@ExtendWith(SpringExtension::class)
+class CheckDuplicatedAccountIdUseCaseTest {
+
+    @MockBean
+    private lateinit var studentQueryUserPort: StudentQueryUserPort
+
+    private lateinit var checkDuplicatedAccountIdUseCase: CheckDuplicatedAccountIdUseCase
+
+    private val accountId = "계정아이디입니다하하하"
+
+    @BeforeEach
+    fun setUp() {
+        checkDuplicatedAccountIdUseCase = CheckDuplicatedAccountIdUseCase(studentQueryUserPort)
+    }
+
+    @Test
+    fun `계정 아이디 중복 없음`() {
+
+        // given
+        given(studentQueryUserPort.existsByAccountId(accountId))
+            .willReturn(false)
+
+        // when & then
+        assertDoesNotThrow {
+            checkDuplicatedAccountIdUseCase.execute(accountId)
+        }
+    }
+
+    @Test
+    fun `계정 아이디 중복`() {
+
+        // given
+        given(studentQueryUserPort.existsByAccountId(accountId))
+            .willReturn(true)
+
+        // when & then
+        assertThrows<UserAccountIdExistsException> {
+            checkDuplicatedAccountIdUseCase.execute(accountId)
+        }
+    }
+ }
