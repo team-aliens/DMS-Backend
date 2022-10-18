3261a1154bf88efca2ef2ecc3ec1ca185a11c2cd
이준서 <ljchan71@gmail.com>
2022-10-18 18:39:42 +0900

    refactor: (#19) FindAccountIdUseCase

--- dms-application/src/main/kotlin/team/aliens/dms/domain/manager/usecase/FindAccountIdUseCase.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/manager/usecase/FindAccountIdUseCase.kt
@@ -1,11 +1,11 @@
 package team.aliens.dms.domain.manager.usecase
 
 import team.aliens.dms.domain.manager.exception.AnswerNotMatchedException
+import team.aliens.dms.domain.manager.exception.ManagerNotFoundException
 import team.aliens.dms.domain.manager.spi.ManagerQuerySchoolPort
 import team.aliens.dms.domain.manager.spi.ManagerQueryUserPort
 import team.aliens.dms.domain.manager.spi.ManagerSecurityPort
 import team.aliens.dms.domain.school.exception.SchoolNotFoundException
-import team.aliens.dms.domain.user.exception.UserNotFoundException
 import team.aliens.dms.global.annotation.UseCase
 import team.aliens.dms.global.spi.CoveredEmailPort
 import java.util.UUID
@@ -14,7 +14,6 @@ import java.util.UUID
 class FindAccountIdUseCase(
     val managerQuerySchoolPort: ManagerQuerySchoolPort,
     val managerQueryUserPort: ManagerQueryUserPort,
-    val managerSecurityPort: ManagerSecurityPort,
     val coveredEmailPort: CoveredEmailPort
 ) {
     fun execute(schoolId: UUID, answer: String): String {
@@ -25,8 +24,8 @@ class FindAccountIdUseCase(
             throw AnswerNotMatchedException
         }
 
-        val user = managerQueryUserPort.queryUserById(managerSecurityPort.getCurrentUserId())
-            ?: throw UserNotFoundException
+        var user =
+            managerQueryUserPort.queryUserBySchoolId(schoolId) ?: throw ManagerNotFoundException
 
         return coveredEmailPort.coveredEmail(user.email)
     }
