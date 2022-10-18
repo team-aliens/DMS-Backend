a86c0c59e3e3e6dce2f9a3f933120fe870f90d3e
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-18 16:10:35 +0900

    feat: (#22) check email duplicate usecase

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/student/usecase/CheckEmailDuplicateUseCase.kt
@@ -0,0 +1,17 @@
+package team.aliens.dms.domain.student.usecase
+
+import team.aliens.dms.domain.student.exception.StudentEmailExistsException
+import team.aliens.dms.domain.student.spi.StudentQueryUserPort
+import team.aliens.dms.global.annotation.ReadOnlyUseCase
+
+@ReadOnlyUseCase
+class CheckEmailDuplicateUseCase(
+    private val studentQueryUserPort: StudentQueryUserPort
+) {
+
+    fun execute(email: String) {
+        if (studentQueryUserPort.existsByEmail(email)) {
+            throw StudentEmailExistsException
+        }
+    }
+}
