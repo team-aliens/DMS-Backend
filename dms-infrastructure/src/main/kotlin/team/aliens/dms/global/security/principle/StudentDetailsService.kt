410f91f38c46884cbdf48620a100d4568def8613
KimBeomJin <sdpthf@naver.com>
2022-10-18 14:05:47 +0900

    chore: (#21) StudentDetailsService

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/StudentDetailsService.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/StudentDetailsService.kt
@@ -1,13 +1,22 @@
 package team.aliens.dms.global.security.principle
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
+import team.aliens.dms.global.security.exception.InvalidTokenException
+import team.aliens.dms.persistence.student.repository.StudentJpaRepository
+import java.util.*
 
 @Component
-class StudentDetailsService : UserDetailsService {
+class StudentDetailsService(
+    private val studentRepository: StudentJpaRepository
+) : UserDetailsService {
 
     override fun loadUserByUsername(username: String?): UserDetails {
-        TODO("Not yet implemented")
+        val studentId = UUID.fromString(username)
+        return studentRepository.findByIdOrNull(studentId)?.let {
+            StudentDetails(studentId)
+        } ?: throw InvalidTokenException
     }
 }
