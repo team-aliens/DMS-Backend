265387beacb5d34a2a29a3fa5c76054307fc33f2
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-19 01:40:43 +0900

    feat: (#34) student web adapter & response

--- dms-presentation/src/main/kotlin/team/aliens/dms/student/StudentWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/student/StudentWebAdapter.kt
@@ -2,11 +2,15 @@ package team.aliens.dms.student
 
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import team.aliens.dms.domain.student.usecase.CheckDuplicatedAccountIdUseCase
 import team.aliens.dms.domain.student.usecase.CheckDuplicatedEmailUseCase
+import team.aliens.dms.domain.student.usecase.FindAccountIdUseCase
+import team.aliens.dms.student.dto.response.CoveredEmailResponse
+import java.util.UUID
 import javax.validation.constraints.NotBlank
 
 @Validated
@@ -14,7 +18,8 @@ import javax.validation.constraints.NotBlank
 @RestController
 class StudentWebAdapter(
     private val checkDuplicatedEmailUseCase: CheckDuplicatedEmailUseCase,
-    private val checkDuplicatedAccountIdUseCase: CheckDuplicatedAccountIdUseCase
+    private val checkDuplicatedAccountIdUseCase: CheckDuplicatedAccountIdUseCase,
+    private val findAccountIdUseCase: FindAccountIdUseCase
 ) {
 
     @GetMapping("/email/duplication")
@@ -25,5 +30,18 @@ class StudentWebAdapter(
     @GetMapping("/account-id/duplication")
     fun checkDuplicatedAccountId(@RequestParam @NotBlank accountId: String) {
         checkDuplicatedAccountIdUseCase.execute(accountId)
+    }
+
+    @GetMapping("/account-id/{school-id}")
+    fun findAccountId(
+        @PathVariable(name = "school-id") schoolId: UUID,
+        @RequestParam @NotBlank name: String,
+        @RequestParam @NotBlank grade: Int,
+        @RequestParam @NotBlank classRoom: Int,
+        @RequestParam @NotBlank number: Int
+    ): CoveredEmailResponse {
+        val result = findAccountIdUseCase.execute(schoolId, name, grade, classRoom, number)
+
+        return CoveredEmailResponse(result)
     }
 }
---
+++ dms-presentation/src/main/kotlin/team/aliens/dms/student/dto/response/CoveredEmailResponse.kt
@@ -0,0 +1,5 @@
+package team.aliens.dms.student.dto.response
+
+data class CoveredEmailResponse(
+    val email: String
+)
