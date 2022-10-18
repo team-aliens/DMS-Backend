6405ffd85a3c722bf23a5c745dbbeaf8e4ea46f5
KimBeomJin <sdpthf@naver.com>
2022-10-18 20:10:05 +0900

    add: (#25) QuerySchoolQuestionUseCase

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/school/usecase/QuerySchoolQuestionUseCase.kt
@@ -0,0 +1,19 @@
+package team.aliens.dms.domain.school.usecase
+
+import team.aliens.dms.domain.school.exception.SchoolNotFoundException
+import team.aliens.dms.domain.school.spi.QuerySchoolPort
+import team.aliens.dms.global.annotation.ReadOnlyUseCase
+import java.util.*
+
+@ReadOnlyUseCase
+class QuerySchoolQuestionUseCase(
+    private val querySchoolPort: QuerySchoolPort
+) {
+
+    fun execute(schoolId: UUID): String {
+        val school = querySchoolPort.querySchoolById(schoolId) ?: throw SchoolNotFoundException
+
+        return school.question
+    }
+
+}
