9ac60353d05f4781a38e29196b35747a0107976b
이준서 <ljchan71@gmail.com>
2022-10-18 16:09:39 +0900

    chore: (#19)  all open annotation

--- dms-application/build.gradle.kts
+++ dms-application/build.gradle.kts
@@ -1,7 +1,16 @@
+plugins {
+    kotlin("plugin.allopen") version PluginVersions.ALLOPEN_VERSION
+}
+
 dependencies {
     // impl project
     implementation(project(":dms-domain"))
 
     // transaction
     implementation(Dependencies.SPRING_TRANSACTION)
+}
+
+allOpen {
+    annotation("team.aliens.dms.global.annotation.UseCase")
+    annotation("team.aliens.dms.global.annotation.UseCase")
 }
