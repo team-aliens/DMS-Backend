8d3aad07c47f553bfb86276f803210c1149a1fb5
KimBeomJin <sdpthf@naver.com>
2022-10-18 14:05:16 +0900

    chore: (#21) ManagerDetails

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/ManagerDetails.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/ManagerDetails.kt
@@ -3,16 +3,20 @@ package team.aliens.dms.global.security.principle
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
+import team.aliens.dms.domain.auth.model.Authority
+import java.util.*
 
-class ManagerDetails : UserDetails {
+class ManagerDetails(
+    private val managerId: UUID
+) : UserDetails {
 
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
-        return mutableListOf(SimpleGrantedAuthority(TODO("ROLE")))
+        return mutableListOf(SimpleGrantedAuthority(Authority.MANAGER.name))
     }
 
     override fun getPassword(): String? = null
 
-    override fun getUsername(): String = TODO("MANAGER_ID")
+    override fun getUsername(): String = managerId.toString()
 
     override fun isAccountNonExpired(): Boolean = true
 
