73713258f9c023e816a58eb5b3883cf3d5e3eab4
이준서 <ljchan71@gmail.com>
2022-10-18 16:10:51 +0900

    feat: (#19) UserPersisenceAdapter

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/user/UserPersistenceAdapter.kt
@@ -0,0 +1,23 @@
+package team.aliens.dms.persistence.user
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import team.aliens.dms.domain.user.model.User
+import team.aliens.dms.domain.user.spi.UserPort
+import team.aliens.dms.persistence.user.mapper.UserMapper
+import team.aliens.dms.persistence.user.repository.UserJpaRepository
+import java.util.*
+
+@Component
+class UserPersistenceAdapter(
+    val userJpaRepository: UserJpaRepository,
+    val userMapper: UserMapper
+) : UserPort{
+
+    override fun queryUserById(id: UUID): User? {
+        return userMapper.toDomain(
+            userJpaRepository.findByIdOrNull(id)
+        )
+    }
+
+}
