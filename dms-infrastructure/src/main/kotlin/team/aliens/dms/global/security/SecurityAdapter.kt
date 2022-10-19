6d978ba4ad435cfd5e36f5f7f859bb48d4b37678
KimBeomJin <sdpthf@naver.com>
2022-10-19 23:22:33 +0900

    feat: (#18) StudentSecurityPort

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityAdapter.kt
@@ -1,14 +1,19 @@
 package team.aliens.dms.global.security
 
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.auth.spi.SecurityPort
-import java.util.UUID
+import java.util.*
 
 @Component
-class SecurityAdapter : SecurityPort {
+class SecurityAdapter(
+    private val passwordEncoder: PasswordEncoder
+) : SecurityPort {
 
-    override fun getCurrentUserId() : UUID {
+    override fun getCurrentUserId(): UUID {
         return UUID.fromString(SecurityContextHolder.getContext().authentication.name);
     }
+
+    override fun encode(password: String): String = passwordEncoder.encode(password)
 }
