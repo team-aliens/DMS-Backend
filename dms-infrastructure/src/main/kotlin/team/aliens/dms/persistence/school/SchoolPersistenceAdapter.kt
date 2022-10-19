049048a4d7203b8d2c18fc4c415d22fa40b6f6fd
KimBeomJin <sdpthf@naver.com>
2022-10-19 23:33:43 +0900

    refactor: (#18) remove import

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/school/SchoolPersistenceAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/school/SchoolPersistenceAdapter.kt
@@ -2,7 +2,6 @@ package team.aliens.dms.persistence.school
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
-import team.aliens.dms.domain.school.model.School
 import team.aliens.dms.domain.school.spi.SchoolPort
 import team.aliens.dms.persistence.school.mapper.SchoolMapper
 import team.aliens.dms.persistence.school.repository.SchoolJpaRepository
