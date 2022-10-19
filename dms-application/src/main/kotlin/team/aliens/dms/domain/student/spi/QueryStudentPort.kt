051eb6a9e88b686aaa5c13a6a79b3c17a03abf87
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-19 11:40:06 +0900

    chore: (#34) code smell & method removed

--- dms-application/src/main/kotlin/team/aliens/dms/domain/student/spi/QueryStudentPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/student/spi/QueryStudentPort.kt
@@ -4,6 +4,5 @@ import team.aliens.dms.domain.student.model.Student
 import java.util.UUID
 
 interface QueryStudentPort {
-    fun existsByGcn(grade: Int, classRoom: Int, number: Int): Boolean
     fun queryStudentBySchoolIdAndGcn(schoolId: UUID, grade: Int, classRoom: Int, number: Int): Student?
 }
--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/student/StudentPersistenceAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/student/StudentPersistenceAdapter.kt
@@ -1,7 +1,6 @@
 package team.aliens.dms.persistence.student
 
 import org.springframework.stereotype.Component
-import team.aliens.dms.domain.student.model.Student
 import team.aliens.dms.domain.student.spi.StudentPort
 import team.aliens.dms.persistence.student.mapper.StudentMapper
 import team.aliens.dms.persistence.student.repository.StudentJpaRepository
@@ -12,9 +11,6 @@ class StudentPersistenceAdapter(
     private val studentMapper: StudentMapper,
     private val studentRepository: StudentJpaRepository
 ) : StudentPort {
-
-    override fun existsByGcn(grade: Int, classRoom: Int, number: Int): Boolean =
-        studentRepository.existsByGradeAndClassRoomAndNumber(grade, classRoom, number)
 
     override fun queryStudentBySchoolIdAndGcn(schoolId: UUID, grade: Int, classRoom: Int, number: Int) = studentMapper.toDomain(
         studentRepository.findByUserSchoolIdAndGradeAndClassRoomAndNumber(schoolId, grade, classRoom, number)
