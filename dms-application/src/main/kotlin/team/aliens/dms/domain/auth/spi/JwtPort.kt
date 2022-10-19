5f0030a032e70926a1d1750b96fe9fa90eea8add
KimBeomJin <sdpthf@naver.com>
2022-10-19 23:23:29 +0900

    chore: (#18) JwtPort

--- dms-application/src/main/kotlin/team/aliens/dms/domain/auth/spi/JwtPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/auth/spi/JwtPort.kt
@@ -1,5 +1,6 @@
 package team.aliens.dms.domain.auth.spi
 
+import team.aliens.dms.domain.student.spi.StudentJwtPort
 import team.aliens.dms.domain.user.spi.UserJwtPort
 
-interface JwtPort : UserJwtPort
+interface JwtPort : UserJwtPort, StudentJwtPort
