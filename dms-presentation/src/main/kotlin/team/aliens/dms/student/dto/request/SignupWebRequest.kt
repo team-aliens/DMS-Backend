c4b8bf08a0bcde93fea9a7a0e8287a0bcabd10ce
KimBeomJin <sdpthf@naver.com>
2022-10-19 23:05:10 +0900

    chore: (#18) SignUpRequest Validation

--- dms-presentation/src/main/kotlin/team/aliens/dms/student/dto/request/SignupWebRequest.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/student/dto/request/SignupWebRequest.kt
@@ -1,14 +1,22 @@
 package team.aliens.dms.student.dto.request
 
+import org.hibernate.validator.constraints.Length
+import team.aliens.dms.RegexUtil
 import javax.validation.constraints.Email
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
 
 data class SignupWebRequest(
 
     @field:NotBlank
+    @field:Length(min = 8, max = 8)
     val schoolCode: String,
 
     @field:NotBlank
+    @field:Length(max = 100)
     val schoolAnswer: String,
 
     @field:NotBlank
@@ -16,21 +24,29 @@ data class SignupWebRequest(
     val email: String,
 
     @field:NotBlank
+    @field:Length(min = 6, max = 6)
     val authCode: String,
 
-    @field:NotBlank
+    @field:NotNull
+    @field:Min(1)
+    @field:Max(6)
     val grade: Int,
 
-    @field:NotBlank
+    @field:NotNull
+    @field:Min(1)
     val classRoom: Int,
 
-    @field:NotBlank
+    @field:NotNull
+    @field:Min(1)
     val number: Int,
 
     @field:NotBlank
+    @field:Length(max = 20)
     val accountId: String,
 
     @field:NotBlank
+    @field:Length(min = 8, max = 20)
+    @field:Pattern(regexp = RegexUtil.SECRET_PATTERN)
     val password: String,
 
     val profileImageUrl: String?
