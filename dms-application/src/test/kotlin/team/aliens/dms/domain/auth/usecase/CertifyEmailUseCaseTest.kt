3746736a7b5a901f812f4e5faa40aa9916dd7d99
이준서 <ljchan71@gmail.com>
2022-10-19 22:54:39 +0900

    test: (#30) CertifyEmailUseCaseTest

---
+++ dms-application/src/test/kotlin/team/aliens/dms/domain/auth/usecase/CertifyEmailUseCaseTest.kt
@@ -0,0 +1,94 @@
+package team.aliens.dms.domain.auth.usecase
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import team.aliens.dms.domain.auth.dto.CertifyEmailRequest
+import team.aliens.dms.domain.auth.exception.EmailMismatchException
+import team.aliens.dms.domain.auth.spi.AuthQueryUserPort
+import team.aliens.dms.domain.user.exception.UserNotFoundException
+import team.aliens.dms.domain.user.model.User
+import java.util.*
+
+@ExtendWith(SpringExtension::class)
+class CertifyEmailUseCaseTest {
+
+    @MockBean
+    private lateinit var queryUserPort: AuthQueryUserPort
+
+    private lateinit var certifyEmailUseCase: CertifyEmailUseCase
+
+    @BeforeEach
+    fun setUp() {
+        certifyEmailUseCase = CertifyEmailUseCase(queryUserPort)
+    }
+
+    private val id = UUID.randomUUID()
+
+    private val email = "email@dsm.hs.kr"
+
+    private val accountId = "accountId"
+
+    private val user = User(
+        id = id,
+        schoolId = id,
+        accountId = accountId,
+        password = "password",
+        email = email,
+        name = "김범지인",
+        profileImageUrl = "https://~~",
+        createdAt = null,
+        deletedAt = null
+    )
+
+    @Test
+    fun `이메일 검증 성공`() {
+        val request = CertifyEmailRequest(
+            accountId, email
+        )
+
+        //given
+        given(queryUserPort.queryUserByAccountId(request.accountId))
+            .willReturn(user)
+
+        //when then
+        certifyEmailUseCase.execute(request)
+    }
+
+    @Test
+    fun `유저를 찾을 수 없음`() {
+        val request = CertifyEmailRequest(
+            accountId, email
+        )
+
+        //given
+        given(queryUserPort.queryUserByAccountId(request.accountId))
+            .willReturn(null)
+
+        //when then
+        assertThrows<UserNotFoundException> {
+            certifyEmailUseCase.execute(request)
+        }
+    }
+
+    @Test
+    fun `이메일이 일치하지 않음`() {
+        val mismatchEmail = "mis@match.com"
+        val request = CertifyEmailRequest(
+            accountId, mismatchEmail
+        )
+
+        //given
+        given(queryUserPort.queryUserByAccountId(request.accountId))
+            .willReturn(user)
+
+        //when then
+        assertThrows<EmailMismatchException> {
+            certifyEmailUseCase.execute(request)
+        }
+    }
+}
