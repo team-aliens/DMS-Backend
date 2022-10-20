478b474e4c886f4c08803a9737ac4da586a02ecf
KimBeomJin <sdpthf@naver.com>
2022-10-20 09:45:59 +0900

    test: (#18) SignUpUseCaseTests

---
+++ dms-application/src/test/kotlin/team/aliens/dms/domain/student/usecase/SignUpUseCaseTests.kt
@@ -0,0 +1,11 @@
+package team.aliens.dms.domain.student.usecase
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+class SignUpUseCaseTests {
+
+
+
+}
