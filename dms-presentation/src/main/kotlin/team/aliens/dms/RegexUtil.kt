8ccdc9c01df320f7a3673da4fe4b0a154a7bd26e
KimBeomJin <sdpthf@naver.com>
2022-10-20 11:42:20 +0900

    chore: (#18) SECRET_PATTERN 정규식 수정

--- dms-presentation/src/main/kotlin/team/aliens/dms/RegexUtil.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/RegexUtil.kt
@@ -2,6 +2,6 @@ package team.aliens.dms
 
 object RegexUtil {
 
-    const val SECRET_PATTERN = "/^(?=.*[a-zA-Z])(?=.*[!@#\$%^&*+=-])(?=.*[0-9]).{8,25}\$/"
+    const val SECRET_PATTERN = "/^(?=.*[a-zA-Z])(?=.*[!@#\$%^&*+=-])(?=.*[0-9]).{8,20}\$/"
 
 }
