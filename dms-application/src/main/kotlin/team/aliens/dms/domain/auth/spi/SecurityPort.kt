c93a6c9d34f1055e2abf9d2c40c82aab77b85ebb
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-20 22:18:46 +0900

    refactor: (#41) user security port

--- dms-application/src/main/kotlin/team/aliens/dms/domain/auth/spi/SecurityPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/auth/spi/SecurityPort.kt
@@ -2,7 +2,6 @@ package team.aliens.dms.domain.auth.spi
 
 import team.aliens.dms.domain.manager.spi.ManagerSecurityPort
 import team.aliens.dms.domain.student.spi.StudentSecurityPort
-import team.aliens.dms.domain.user.spi.UserSecurityPort
 
-interface SecurityPort : ManagerSecurityPort, UserSecurityPort, StudentSecurityPort {
+interface SecurityPort : ManagerSecurityPort, StudentSecurityPort {
 }
--- dms-application/src/main/kotlin/team/aliens/dms/domain/user/spi/UserSecurityPort.kt
+++
@@ -1,6 +0,0 @@
-package team.aliens.dms.domain.user.spi
-
-interface UserSecurityPort {
-
-    fun encode(password: String): String
-}
