e99e1da4cfd299b0cac1b19043f26d3c0dd63d26
desktop <ljchan71@gmail.com>
2022-10-22 15:59:53 +0900

    feat: (#54) : ReissueTokenUseCase

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/auth/usecase/ReissueTokenUseCase.kt
@@ -0,0 +1,21 @@
+package team.aliens.dms.domain.auth.usecase
+
+import team.aliens.dms.domain.auth.dto.TokenResponse
+import team.aliens.dms.domain.auth.exception.RefreshTokenNotFoundException
+import team.aliens.dms.domain.auth.spi.JwtPort
+import team.aliens.dms.domain.auth.spi.QueryRefreshTokenPort
+import team.aliens.dms.global.annotation.ReadOnlyUseCase
+
+@ReadOnlyUseCase
+class ReissueTokenUseCase(
+        private val queryRefreshTokenPort: QueryRefreshTokenPort,
+        private val jwtPort: JwtPort
+) {
+
+    fun execute(refreshToken: String): TokenResponse {
+        val token = queryRefreshTokenPort.queryRefreshTokenByToken(refreshToken)
+                ?: throw RefreshTokenNotFoundException
+
+        return jwtPort.receiveToken(token.userId, token.authority)
+    }
+}
