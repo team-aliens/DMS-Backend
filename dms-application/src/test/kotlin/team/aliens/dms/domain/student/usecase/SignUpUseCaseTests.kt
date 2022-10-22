20cbdf98571f38c8637448333775ad8816684704
KimBeomJin <sdpthf@naver.com>
2022-10-22 19:03:13 +0900

    test: (#18) NPE 넌 죽었다

--- dms-application/src/test/kotlin/team/aliens/dms/domain/student/usecase/SignUpUseCaseTests.kt
+++ dms-application/src/test/kotlin/team/aliens/dms/domain/student/usecase/SignUpUseCaseTests.kt
@@ -1,7 +1,9 @@
 package team.aliens.dms.domain.student.usecase
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito.given
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -11,7 +13,9 @@ import team.aliens.dms.domain.auth.model.Authority
 import team.aliens.dms.domain.school.model.School
 import team.aliens.dms.domain.student.dto.SignupRequest
 import team.aliens.dms.domain.student.dto.TokenAndFeaturesResponse
+import team.aliens.dms.domain.student.model.Student
 import team.aliens.dms.domain.student.spi.*
+import team.aliens.dms.domain.user.model.User
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
@@ -52,49 +56,78 @@ class SignUpUseCaseTests {
     }
 
     private val id = UUID.randomUUID()
+    private val code = "12345678"
+    private val email = "test@test.com"
+    private val accountId = "test accountId"
+    private val question = "test question"
+    private val answer = "test answer"
+    private val name = "test name"
+    private val profileImageUrl = "test profileImage"
 
-    private val code = "QWER!@#$"
-
-    private val email = "test@tes.com"
-
-    private val accountId = "accountId"
-
-    private val answer = "정답"
-
-    private val school by lazy {
+    private val schoolStub by lazy {
         School(
             id = id,
-            name = "name",
+            name = "test name",
             code = code,
-            question = "확인 질문",
-            answer = ,
+            question = question,
+            answer = answer,
             address = "주소",
             contractStartedAt = LocalDate.now(),
             contractEndedAt = null
         )
     }
 
-    private val tokenResponse by lazy {
-        TokenResponse(
-            accessToken = "sldagkmlgmldkg",
-            expiredAt = LocalDateTime.now(),
-            refreshToken = "dlkgmsalgmslgmalkmlkam"
+    private val userStub by lazy {
+        User(
+            schoolId = schoolStub.id,
+            accountId = accountId,
+            password = "encoded password",
+            email = email,
+            name = name,
+            profileImageUrl = profileImageUrl,
+            createdAt = null,
+            deletedAt = null
         )
     }
 
-    @Test
-    fun `회원가입 성공`() {
-        val tokenAndFeaturesResponse = TokenAndFeaturesResponse(
-            accessToken = tokenResponse.accessToken,
-            expiredAt = tokenResponse.expiredAt,
-            refreshToken = tokenResponse.refreshToken,
-            features = TokenAndFeaturesResponse.Features(
-                mealService = true,
-                noticeService = true,
-                pointService = true,
-            )
+    private val savedUserStub: User by lazy {
+        User(
+            id = UUID.randomUUID(),
+            schoolId = schoolStub.id,
+            accountId = accountId,
+            password = "encoded password",
+            email = email,
+            name = name,
+            profileImageUrl = profileImageUrl,
+            createdAt = LocalDateTime.now(),
+            deletedAt = null
         )
-        val request = SignupRequest(
+    }
+
+    private val studentStub by lazy {
+        Student(
+            studentId = savedUserStub.id,
+            roomNumber = 318,
+            schoolId = schoolStub.id,
+            grade = 1,
+            classRoom = 1,
+            number = 1
+        )
+    }
+
+    private val savedStudentStub by lazy {
+        Student(
+            studentId = savedUserStub.id,
+            roomNumber = 318,
+            schoolId = schoolStub.id,
+            grade = 1,
+            classRoom = 1,
+            number = 1
+        )
+    }
+
+    private val requestStub by lazy {
+        SignupRequest(
             schoolCode = code,
             schoolAnswer = answer,
             email = email,
@@ -103,22 +136,64 @@ class SignUpUseCaseTests {
             classRoom = 1,
             number = 17,
             accountId = accountId,
-            password = "!@$!@SSDG",
+            password = "test password",
             profileImageUrl = null
         )
+    }
 
+    private val tokenResponseStub by lazy {
+        TokenResponse(
+            accessToken = "test access token",
+            expiredAt = LocalDateTime.now(),
+            refreshToken = "test refresh token"
+        )
+    }
+
+    private val tokenAndFeaturesResponseStub by lazy {
+        TokenAndFeaturesResponse(
+            accessToken = tokenResponseStub.accessToken,
+            expiredAt = tokenResponseStub.expiredAt,
+            refreshToken = tokenResponseStub.refreshToken,
+            features = TokenAndFeaturesResponse.Features(
+                mealService = true,
+                noticeService = true,
+                pointService = true,
+            )
+        )
+    }
+
+    @Test
+    fun `회원가입 성공`() {
+        // given
         given(querySchoolPort.querySchoolByCode(code))
-            .willReturn(school)
+            .willReturn(schoolStub)
+
         given(queryUserPort.existsByEmail(email))
             .willReturn(false)
+
         given(queryUserPort.existsByAccountId(accountId))
             .willReturn(false)
+
+        given(securityPort.encode(requestStub.password))
+            .willReturn(userStub.password)
+
+        given(commandUserPort.saveUser(userStub))
+            .willReturn(savedUserStub)
+
+        given(commandStudentPort.saveStudent(studentStub))
+            .willReturn(savedStudentStub)
+
         given(jwtPort.receiveToken(id, Authority.STUDENT))
-            .willReturn(tokenResponse)
+            .willReturn(tokenResponseStub)
 
-        val response = singUpUseCase.execute(request)
+        // when
+        val response = singUpUseCase.execute(requestStub)
 
-        assertEquals()
+        // then
+        assertDoesNotThrow {
+            singUpUseCase.execute(requestStub)
+        }
+        assertThat(response).isEqualTo(tokenAndFeaturesResponseStub)
     }
 
 }
