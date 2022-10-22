065b58620a6b42ec4d85b98724824b8190ea16e7
desktop <ljchan71@gmail.com>
2022-10-22 22:15:29 +0900

    chore: (#54) : GenerateJwtAdapter
    fuckyou

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/token/GenerateJwtAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/token/GenerateJwtAdapter.kt
@@ -21,7 +21,7 @@ class GenerateJwtAdapter(
     override fun receiveToken(userId: UUID, authority: Authority) = TokenResponse(
         accessToken = generateAccessToken(userId, authority),
         expiredAt = Date(System.currentTimeMillis() + securityProperties.accessExp),
-        refreshToken = generateRefreshToken(userId)
+        refreshToken = generateRefreshToken(userId, authority)
     )
 
     private fun generateAccessToken(userId: UUID, authority: Authority) =
@@ -34,7 +34,7 @@ class GenerateJwtAdapter(
             .setExpiration(Date(System.currentTimeMillis() + securityProperties.accessExp))
             .compact()
 
-     private fun generateRefreshToken(userId: UUID): String {
+     private fun generateRefreshToken(userId: UUID, authority: Authority): String {
         val token = Jwts.builder()
             .signWith(SignatureAlgorithm.HS512, securityProperties.secretKey)
             .setHeaderParam(Header.JWT_TYPE, JwtProperties.REFRESH)
@@ -46,6 +46,7 @@ class GenerateJwtAdapter(
             RefreshTokenEntity(
                 token = token,
                 userId = userId,
+                authority = authority,
                 expirationTime = securityProperties.refreshExp / 1000
             )
         )
