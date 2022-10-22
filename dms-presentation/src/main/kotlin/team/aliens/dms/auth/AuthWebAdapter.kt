f73cf813ae130246dc1b4f2744a3ca06862887a7
desktop <ljchan71@gmail.com>
2022-10-22 16:02:17 +0900

    chore: (#54) : AuthWebAdapter

--- dms-presentation/src/main/kotlin/team/aliens/dms/auth/AuthWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/auth/AuthWebAdapter.kt
@@ -8,8 +8,10 @@ import team.aliens.dms.auth.dto.request.SendEmailCodeWebRequest
 import team.aliens.dms.domain.auth.dto.CertifyEmailCodeRequest
 import team.aliens.dms.domain.auth.dto.CertifyEmailRequest
 import team.aliens.dms.domain.auth.dto.SendEmailCodeRequest
+import team.aliens.dms.domain.auth.dto.TokenResponse
 import team.aliens.dms.domain.auth.usecase.CertifyEmailCodeUseCase
 import team.aliens.dms.domain.auth.usecase.CertifyEmailUseCase
+import team.aliens.dms.domain.auth.usecase.ReissueTokenUseCase
 import team.aliens.dms.domain.auth.usecase.SendEmailCodeUseCase
 import javax.validation.Valid
 
@@ -17,39 +19,45 @@ import javax.validation.Valid
 @RequestMapping("/auth")
 @RestController
 class AuthWebAdapter(
-    private val sendEmailCodeUseCase: SendEmailCodeUseCase,
-    private val certifyEmailCodeUseCase: CertifyEmailCodeUseCase,
-    private val certifyEmailUseCase: CertifyEmailUseCase
+        private val sendEmailCodeUseCase: SendEmailCodeUseCase,
+        private val certifyEmailCodeUseCase: CertifyEmailCodeUseCase,
+        private val certifyEmailUseCase: CertifyEmailUseCase,
+        private val reissueTokenUseCase: ReissueTokenUseCase
 ) {
 
     @GetMapping("/code")
     fun certifyEmailCode(@ModelAttribute request: CertifyEmailCodeWebRequest) {
         certifyEmailCodeUseCase.execute(
-            CertifyEmailCodeRequest(
-                email = request.email,
-                authCode = request.authCode,
-                type = request.type.toString()
-            )
+                CertifyEmailCodeRequest(
+                        email = request.email,
+                        authCode = request.authCode,
+                        type = request.type.toString()
+                )
         )
     }
 
     @PostMapping("/code")
     fun sendEmailCode(@RequestBody @Valid request: SendEmailCodeWebRequest) {
         sendEmailCodeUseCase.execute(
-            SendEmailCodeRequest(
-                email = request.email,
-                type = request.type.toString()
-            )
+                SendEmailCodeRequest(
+                        email = request.email,
+                        type = request.type.toString()
+                )
         )
     }
 
     @GetMapping("/email")
     fun certifyEmail(@ModelAttribute request: CertifyEmailWebRequest) {
         certifyEmailUseCase.execute(
-            CertifyEmailRequest(
-                accountId = request.accountId,
-                email = request.email
-            )
+                CertifyEmailRequest(
+                        accountId = request.accountId,
+                        email = request.email
+                )
         )
+    }
+
+    @PutMapping("/reissue")
+    fun reissueToken(@RequestHeader("refresh-token") refreshToken: String): TokenResponse {
+        return reissueTokenUseCase.execute(refreshToken)
     }
 }
