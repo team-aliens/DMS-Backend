4618f4350451a1ff396df9e42c2b0eaf86d03f42
desktop <ljchan71@gmail.com>
2022-10-22 16:01:17 +0900

    chore: (#54) : JwtPort

--- dms-application/src/main/kotlin/team/aliens/dms/domain/auth/spi/JwtPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/auth/spi/JwtPort.kt
@@ -1,5 +1,11 @@
 package team.aliens.dms.domain.auth.spi
 
+import team.aliens.dms.domain.auth.dto.TokenResponse
+import team.aliens.dms.domain.auth.model.Authority
 import team.aliens.dms.domain.user.spi.UserJwtPort
+import java.util.*
 
-interface JwtPort : UserJwtPort
+interface JwtPort : UserJwtPort{
+
+    fun receiveToken(userId: UUID, authority: Authority): TokenResponse
+}
