191f0c912c23ddfdda75d157560b7042de48eb19
desktop <ljchan71@gmail.com>
2022-10-22 16:00:25 +0900

    feat: (#54) : RefreshTokenPersistenceAdapter

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/auth/RefreshTokenPersistenceAdapter.kt
@@ -0,0 +1,18 @@
+package team.aliens.dms.persistence.auth
+
+import org.springframework.stereotype.Component
+import team.aliens.dms.domain.auth.model.RefreshToken
+import team.aliens.dms.domain.auth.spi.RefreshTokenPort
+import team.aliens.dms.persistence.auth.mapper.RefreshTokenMapper
+import team.aliens.dms.persistence.auth.repository.RefreshTokenRepository
+
+@Component
+class RefreshTokenPersistenceAdapter(
+        private val refreshTokenMapper: RefreshTokenMapper,
+        private val refreshTokenRepository: RefreshTokenRepository
+) : RefreshTokenPort {
+
+    override fun queryRefreshTokenByToken(token: String) = refreshTokenMapper.toDomain(
+            refreshTokenRepository.findByToken(token)
+    )
+}
