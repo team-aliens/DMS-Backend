8a955e1d94aab254fa2e87c7e6cc86647cc63393
이준서 <ljchan71@gmail.com>
2022-10-23 23:37:05 +0900

    fix: (#54) 이상한 의존관계

--- dms-application/src/main/kotlin/team/aliens/dms/domain/auth/spi/JwtPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/auth/spi/JwtPort.kt
@@ -1,12 +1,9 @@
 package team.aliens.dms.domain.auth.spi
 
-import team.aliens.dms.domain.auth.dto.TokenResponse
-import team.aliens.dms.domain.auth.model.Authority
 import team.aliens.dms.domain.student.spi.StudentJwtPort
 import team.aliens.dms.domain.user.spi.UserJwtPort
 import java.util.*
 
-interface JwtPort : UserJwtPort, StudentJwtPort{
+interface JwtPort : UserJwtPort, StudentJwtPort, AuthJwtPort{
 
-    fun receiveToken(userId: UUID, authority: Authority): TokenResponse
 }
--- dms-application/src/main/kotlin/team/aliens/dms/domain/user/spi/UserJwtPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/user/spi/UserJwtPort.kt
@@ -1,4 +1,9 @@
 package team.aliens.dms.domain.user.spi
 
+import team.aliens.dms.domain.auth.dto.TokenResponse
+import team.aliens.dms.domain.auth.model.Authority
+import java.util.*
+
 interface UserJwtPort {
+    fun receiveToken(userId: UUID, authority: Authority): TokenResponse
 }
