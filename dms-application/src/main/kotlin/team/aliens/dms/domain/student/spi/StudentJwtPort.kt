2a49990e0f85fd73cfa8480415febb9fc0d65f0f
이준서 <ljchan71@gmail.com>
2022-10-23 23:24:10 +0900

    fix: (#54) 에러 해결

--- dms-application/src/main/kotlin/team/aliens/dms/domain/student/spi/StudentJwtPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/student/spi/StudentJwtPort.kt
@@ -1,11 +1,4 @@
 package team.aliens.dms.domain.student.spi
 
-import team.aliens.dms.domain.auth.dto.TokenResponse
-import team.aliens.dms.domain.auth.model.Authority
-import java.util.*
-
 interface StudentJwtPort {
-
-    fun receiveToken(userId: UUID, authority: Authority): TokenResponse
-
 }
--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/auth/RefreshTokenPersistenceAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/auth/RefreshTokenPersistenceAdapter.kt
@@ -1,7 +1,6 @@
 package team.aliens.dms.persistence.auth
 
 import org.springframework.stereotype.Component
-import team.aliens.dms.domain.auth.model.RefreshToken
 import team.aliens.dms.domain.auth.spi.RefreshTokenPort
 import team.aliens.dms.persistence.auth.mapper.RefreshTokenMapper
 import team.aliens.dms.persistence.auth.repository.RefreshTokenRepository
