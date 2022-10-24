1683dccdb3d4b6e3b20f8706b1e91c45dd00cac1
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-24 15:31:49 +0900

    chore: (#61) http url

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import team.aliens.dms.domain.auth.model.Authority
 import team.aliens.dms.global.filter.FilterConfig
 import team.aliens.dms.global.security.token.JwtParser
 
@@ -52,6 +53,9 @@ class SecurityConfiguration(
 
             // /files
             .antMatchers(HttpMethod.POST, "/files").permitAll()
+
+            // /meals
+            .antMatchers(HttpMethod.GET, "/meals/{date}").hasAuthority(Authority.STUDENT.name)
 
         http
             .apply(FilterConfig(jwtParser, objectMapper))
