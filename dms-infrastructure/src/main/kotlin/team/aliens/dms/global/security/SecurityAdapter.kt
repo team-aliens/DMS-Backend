03185bd1d8c235471a41c54933322912dc1986d8
이준서 <ljchan71@gmail.com>
2022-10-25 22:36:09 +0900

    chore: (#45) SecurityAdapter

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityAdapter.kt
@@ -1,19 +1,20 @@
 package team.aliens.dms.global.security
 
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.auth.spi.SecurityPort
 import java.util.*
 
 @Component
 class SecurityAdapter(
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: BCryptPasswordEncoder
 ) : SecurityPort {
 
     override fun getCurrentUserId(): UUID {
-        return UUID.fromString(SecurityContextHolder.getContext().authentication.name);
+        return UUID.fromString(SecurityContextHolder.getContext().authentication.name)
     }
+    override fun isPasswordMatch(rawPassword: String, encodedPassword: String) =
+        passwordEncoder.matches(rawPassword, encodedPassword)
 
     override fun encode(password: String): String = passwordEncoder.encode(password)
 }
