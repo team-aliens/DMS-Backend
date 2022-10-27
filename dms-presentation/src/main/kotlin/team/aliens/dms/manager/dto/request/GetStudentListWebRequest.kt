ac844adc37598505c8099be30b99c9f98731b834
desktop <ljchan71@gmail.com>
2022-10-28 07:18:11 +0900

    feat: (#70) GetStudentListWebRequest

---
+++ dms-presentation/src/main/kotlin/team/aliens/dms/manager/dto/request/GetStudentListWebRequest.kt
@@ -0,0 +1,14 @@
+package team.aliens.dms.manager.dto.request
+
+import team.aliens.dms.domain.manager.dto.Sort
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+data class GetStudentListWebRequest(
+
+    @field:NotBlank
+    val name: String,
+
+    @field:NotNull
+    val sort: Sort
+)
