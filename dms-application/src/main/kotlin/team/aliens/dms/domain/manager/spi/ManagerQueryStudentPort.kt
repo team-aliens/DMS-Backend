1f54781249eba6c1775f9659049f7f4ffa876222
desktop <ljchan71@gmail.com>
2022-10-28 03:19:46 +0900

    feat: (#71) ManagerQueryStudentPort

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/manager/spi/ManagerQueryStudentPort.kt
@@ -0,0 +1,8 @@
+package team.aliens.dms.domain.manager.spi
+
+import team.aliens.dms.domain.student.model.Student
+import java.util.UUID
+
+interface ManagerQueryStudentPort {
+    fun queryByUserId(userId: UUID): Student?
+}
