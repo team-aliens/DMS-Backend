3a8fd34566ce6e0eed91cd00d00f386fd5262ded
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-03 22:33:51 +0900

    feat: (#81) notice web & persistence adapter

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/notice/NoticePersistenceAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/notice/NoticePersistenceAdapter.kt
@@ -1,14 +1,18 @@
 package team.aliens.dms.persistence.notice
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.notice.spi.NoticePort
+import team.aliens.dms.persistence.notice.mapper.NoticeMapper
 import team.aliens.dms.persistence.notice.repository.NoticeJpaRepository
 import java.time.LocalDate
 import java.time.LocalTime
+import java.util.*
 
 @Component
 class NoticePersistenceAdapter(
-    private val noticeRepository: NoticeJpaRepository
+    private val noticeRepository: NoticeJpaRepository,
+    private val noticeMapper: NoticeMapper
 ) : NoticePort {
 
     override fun existsNoticeByDateBetween(to: LocalDate, from: LocalDate): Boolean {
@@ -17,4 +21,8 @@ class NoticePersistenceAdapter(
 
         return noticeRepository.existsByCreatedAtBetween(toLocalDateTime, fromLocalDateTime)
     }
+
+    override fun queryNoticeById(noticeId: UUID) = noticeMapper.toDomain(
+        noticeRepository.findByIdOrNull(noticeId)
+    )
 }
--- dms-presentation/src/main/kotlin/team/aliens/dms/notice/NoticeWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/notice/NoticeWebAdapter.kt
@@ -1,15 +1,20 @@
 package team.aliens.dms.notice
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import team.aliens.dms.domain.notice.usecase.NoticeDetailsUseCase
 import team.aliens.dms.domain.notice.usecase.QueryNoticeStatusUseCase
 import team.aliens.dms.notice.dto.response.GetNoticeStatusResponse
+import team.aliens.dms.domain.notice.dto.NoticeDetailsResponse
+import java.util.UUID
 
 @RequestMapping("/notices")
 @RestController
 class NoticeWebAdapter(
-    private val queryNoticeStatusUseCase: QueryNoticeStatusUseCase
+    private val queryNoticeStatusUseCase: QueryNoticeStatusUseCase,
+    private val noticeDetailsUseCase: NoticeDetailsUseCase
 ) {
 
     @GetMapping("/status")
@@ -17,5 +22,10 @@ class NoticeWebAdapter(
         val result = queryNoticeStatusUseCase.execute()
 
         return GetNoticeStatusResponse(result)
+    }
+
+    @GetMapping("/{notice-id}")
+    fun details(@PathVariable("notice-id") noticeId: UUID): NoticeDetailsResponse {
+        return noticeDetailsUseCase.execute(noticeId)
     }
 }
