00bd8e913527ca8bb5f2e0344b36f45d6f615202
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-03 22:35:56 +0900

    feat: (#81) notice user port

--- dms-application/src/main/kotlin/team/aliens/dms/domain/user/spi/UserPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/user/spi/UserPort.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.domain.user.spi
 import team.aliens.dms.domain.auth.spi.AuthQueryUserPort
 import team.aliens.dms.domain.manager.spi.ManagerCommandUserPort
 import team.aliens.dms.domain.manager.spi.ManagerQueryUserPort
+import team.aliens.dms.domain.notice.spi.NoticeQueryUserPort
 import team.aliens.dms.domain.student.spi.StudentCommandUserPort
 import team.aliens.dms.domain.student.spi.StudentQueryUserPort
 
@@ -11,5 +12,6 @@ interface UserPort :
     StudentCommandUserPort,
     ManagerQueryUserPort,
     ManagerCommandUserPort,
-    AuthQueryUserPort {
+    AuthQueryUserPort,
+    NoticeQueryUserPort {
 }
