736e4f30bc9cdc5c44084437d45d92554a548fd4
KimBeomJin <sdpthf@naver.com>
2022-11-03 20:26:27 +0900

    chore: (#79) queryNoticeById

--- dms-application/src/main/kotlin/team/aliens/dms/domain/notice/spi/QueryNoticePort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/notice/spi/QueryNoticePort.kt
@@ -1,9 +1,13 @@
 package team.aliens.dms.domain.notice.spi
 
+import team.aliens.dms.domain.notice.model.Notice
 import java.time.LocalDate
+import java.util.UUID
 
 interface QueryNoticePort {
 
     fun existsNoticeByDateBetween(to: LocalDate, from: LocalDate): Boolean
+
+    fun queryNoticeById(noticeId: UUID): Notice?
 
 }
