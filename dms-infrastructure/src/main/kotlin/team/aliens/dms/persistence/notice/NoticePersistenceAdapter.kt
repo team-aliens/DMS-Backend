a8729643c3d0704053464193d57225ea40568367
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-04 09:34:32 +0900

    feat: (#83) notice web & persistence adapter

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/notice/NoticePersistenceAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/notice/NoticePersistenceAdapter.kt
@@ -1,14 +1,19 @@
 package team.aliens.dms.persistence.notice
 
 import org.springframework.stereotype.Component
+import team.aliens.dms.domain.notice.model.Notice
+import team.aliens.dms.domain.notice.model.OrderType
 import team.aliens.dms.domain.notice.spi.NoticePort
+import team.aliens.dms.persistence.notice.mapper.NoticeMapper
 import team.aliens.dms.persistence.notice.repository.NoticeJpaRepository
 import java.time.LocalDate
 import java.time.LocalTime
+import java.util.*
 
 @Component
 class NoticePersistenceAdapter(
-    private val noticeRepository: NoticeJpaRepository
+    private val noticeRepository: NoticeJpaRepository,
+    private val noticeMapper: NoticeMapper
 ) : NoticePort {
 
     override fun existsNoticeByDateBetween(to: LocalDate, from: LocalDate): Boolean {
@@ -16,5 +21,21 @@ class NoticePersistenceAdapter(
         val fromLocalDateTime = from.atTime(LocalTime.now())
 
         return noticeRepository.existsByCreatedAtBetween(toLocalDateTime, fromLocalDateTime)
+    }
+
+    override fun queryAllNoticesBySchoolIdOrder(schoolId: UUID, orderType: OrderType): List<Notice> {
+        return when (orderType.name) {
+            "NEW" -> noticeRepository.findAllByManagerUserSchoolIdOrderByCreatedAtDesc(schoolId).map {
+                noticeMapper.toDomain(it)!!
+            }
+
+            "OLD" ->  noticeRepository.findAllByManagerUserSchoolIdOrderByCreatedAtAsc(schoolId).map {
+                noticeMapper.toDomain(it)!!
+            }
+
+            else -> noticeRepository.findAllByManagerUserSchoolIdOrderByCreatedAtDesc(schoolId).map {
+                noticeMapper.toDomain(it)!!
+            }
+        }
     }
 }
--- dms-presentation/src/main/kotlin/team/aliens/dms/notice/NoticeWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/notice/NoticeWebAdapter.kt
@@ -1,15 +1,23 @@
 package team.aliens.dms.notice
 
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import team.aliens.dms.domain.notice.dto.QueryAllNoticesResponse
+import team.aliens.dms.domain.notice.usecase.QueryAllNoticesUseCase
 import team.aliens.dms.domain.notice.usecase.QueryNoticeStatusUseCase
+import team.aliens.dms.notice.dto.request.OrderType
 import team.aliens.dms.notice.dto.response.GetNoticeStatusResponse
+import javax.validation.constraints.NotBlank
 
+@Validated
 @RequestMapping("/notices")
 @RestController
 class NoticeWebAdapter(
-    private val queryNoticeStatusUseCase: QueryNoticeStatusUseCase
+    private val queryNoticeStatusUseCase: QueryNoticeStatusUseCase,
+    private val queryAllNoticesUseCase: QueryAllNoticesUseCase
 ) {
 
     @GetMapping("/status")
@@ -17,5 +25,10 @@ class NoticeWebAdapter(
         val result = queryNoticeStatusUseCase.execute()
 
         return GetNoticeStatusResponse(result)
+    }
+
+    @GetMapping("/")
+    fun queryAllNotices(@RequestParam @NotBlank orderType: OrderType): QueryAllNoticesResponse {
+        return queryAllNoticesUseCase.execute(orderType.name)
     }
 }
