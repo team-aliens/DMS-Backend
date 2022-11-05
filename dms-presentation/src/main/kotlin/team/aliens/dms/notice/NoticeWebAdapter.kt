16c5e17b4175daa926230151b4f116fb85d1c17a
desktop <ljchan71@gmail.com>
2022-11-05 12:57:01 +0900

    chore: (#85) NoticeWebAdapter

--- dms-presentation/src/main/kotlin/team/aliens/dms/notice/NoticeWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/notice/NoticeWebAdapter.kt
@@ -5,19 +5,20 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import team.aliens.dms.domain.notice.usecase.QueryNoticeDetailsUseCase
 import team.aliens.dms.domain.notice.dto.QueryAllNoticesResponse
-import team.aliens.dms.domain.notice.usecase.QueryAllNoticesUseCase
-import team.aliens.dms.domain.notice.usecase.QueryNoticeStatusUseCase
-import team.aliens.dms.domain.notice.usecase.RemoveNoticeUseCase
 import team.aliens.dms.notice.dto.request.OrderType
 import team.aliens.dms.notice.dto.response.GetNoticeStatusResponse
 import team.aliens.dms.domain.notice.dto.QueryNoticeDetailsResponse
+import team.aliens.dms.domain.notice.usecase.*
+import team.aliens.dms.notice.dto.request.PostNoticeWebRequest
 import java.util.UUID
+import javax.validation.Valid
 import javax.validation.constraints.NotNull
 
 @Validated
@@ -27,7 +28,8 @@ class NoticeWebAdapter(
     private val queryNoticeStatusUseCase: QueryNoticeStatusUseCase,
     private val queryNoticeDetailsUseCase: QueryNoticeDetailsUseCase,
     private val queryAllNoticesUseCase: QueryAllNoticesUseCase,
-    private val removeNoticeUseCase: RemoveNoticeUseCase
+    private val removeNoticeUseCase: RemoveNoticeUseCase,
+    private val postNoticeUseCase: PostNoticeUseCase
 ) {
 
     @GetMapping("/status")
@@ -51,5 +53,13 @@ class NoticeWebAdapter(
     @DeleteMapping("/{notice-id}")
     fun removeNotice(@PathVariable("notice-id") noticeId: UUID) {
         removeNoticeUseCase.execute(noticeId)
+    }
+
+    @PostMapping
+    fun postNotice(@RequestBody @Valid postNoticeWebRequest: PostNoticeWebRequest) {
+        return postNoticeUseCase.execute(
+            title = postNoticeWebRequest.title,
+            content = postNoticeWebRequest.content
+        )
     }
 }
