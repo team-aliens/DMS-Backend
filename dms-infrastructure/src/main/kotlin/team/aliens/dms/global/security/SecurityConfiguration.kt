6f5c8f303f6726bcdf7f72626442b4ea8fb6feb8
desktop <ljchan71@gmail.com>
2022-11-05 12:57:53 +0900

    chore: (#85) SecurityConfig

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
@@ -63,6 +63,7 @@ class SecurityConfiguration(
             .antMatchers(HttpMethod.GET, "/notices/{notice-id}").hasAnyAuthority(STUDENT.name, MANAGER.name)
             .antMatchers(HttpMethod.GET, "/notices/").hasAnyAuthority(STUDENT.name, MANAGER.name)
             .antMatchers(HttpMethod.DELETE, "/notices/{notice-id}").hasAuthority(MANAGER.name)
+            .antMatchers(HttpMethod.POST, "/notices").hasAuthority(MANAGER.name)
 
             // /files
             .antMatchers(HttpMethod.POST, "/files").permitAll()
