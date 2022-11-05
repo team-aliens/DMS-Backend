41fac0c81712e04695c3708642d698e5d5a84ff2
desktop <ljchan71@gmail.com>
2022-11-05 13:40:44 +0900

    feat: (#87) UpdateNoticeUsecase

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/notice/usecase/UpdateNoticeUseCase.kt
@@ -0,0 +1,23 @@
+package team.aliens.dms.domain.notice.usecase
+
+import team.aliens.dms.common.annotation.UseCase
+import team.aliens.dms.domain.notice.exception.NoticeNotFoundException
+import team.aliens.dms.domain.notice.spi.CommandNoticePort
+import team.aliens.dms.domain.notice.spi.NoticeSecurityPort
+import team.aliens.dms.domain.notice.spi.QueryNoticePort
+import java.util.UUID
+
+@UseCase
+class UpdateNoticeUseCase(
+    private val queryNoticePort: QueryNoticePort,
+    private val commandNoticePort: CommandNoticePort,
+    private val securityPort: NoticeSecurityPort
+) {
+
+    fun execute(noticeId: UUID, title: String, content: String) {
+        val managerId = securityPort.getCurrentUserId()
+        val notice = queryNoticePort.queryNoticeByIdAndManagerId(noticeId, managerId) ?: NoticeNotFoundException
+
+        commandNoticePort.saveNotice(notice)
+    }
+}
