64eded472cf11c0b01ff266649a70cce18699e3d
junseo <ljchan71@gmail.com>
2022-11-09 11:04:48 +0900

    chore: (#70) GetStudentListUseCase

--- dms-application/src/main/kotlin/team/aliens/dms/domain/manager/usecase/GetStudentListUseCase.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/manager/usecase/GetStudentListUseCase.kt
@@ -1,32 +1,41 @@
 package team.aliens.dms.domain.manager.usecase
 
+import team.aliens.dms.common.annotation.ReadOnlyUseCase
+import team.aliens.dms.common.util.StringUtil
 import team.aliens.dms.domain.manager.dto.GetStudentListResponse
 import team.aliens.dms.domain.manager.dto.Sort
+import team.aliens.dms.domain.manager.spi.ManagerQueryStudentPort
 import team.aliens.dms.domain.manager.spi.ManagerQueryUserPort
-import team.aliens.dms.domain.student.model.Student
-import team.aliens.dms.global.annotation.ReadOnlyUseCase
+import team.aliens.dms.domain.student.exception.StudentNotFoundException
 
 @ReadOnlyUseCase
 class GetStudentListUseCase(
     private val queryUserPort: ManagerQueryUserPort,
-//    private val queryStudentPort: ManageRQueryStudentPort
+    private val queryStudentPort: ManagerQueryStudentPort
 ) {
 
-    fun execute(name: String, sort: Sort) {
+    fun execute(name: String, sort: Sort): GetStudentListResponse {
 
         val user = queryUserPort.searchStudent(name, sort)
-//        val users = user.map {
-//            queryStudentPort.queryStudentByUserId(it.id)
+        val students = user.map {
+            queryStudentPort.queryStudentById(it.id)
 
-//        val students = user.map {
-//            val student = queryStudentPort.queryByUserId(it.id)
-//            GetStudentListResponse.StudentElement(
-//                id = it.id,
-//                name = it.name,
-//                gcn =
-//            )
-//        }
+            val student = queryStudentPort.queryStudentById(it.id) ?: throw StudentNotFoundException
+            GetStudentListResponse.StudentElement(
+                id = it.id,
+                name = it.name,
+                gcn = StringUtil.gcnToString(
+                    grade = student.grade,
+                    classRoom = student.classRoom,
+                    number = student.number
+                ),
+                roomNumber = student.roomNumber,
+                profileImageUrl = it.profileImageUrl!!
+            )
+        }
 
-        TODO("#71 merge")
+        return GetStudentListResponse(students)
     }
+
+
 }
