8eb6d676d6d08f18a395ad7fc6f04f387040a24c
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-13 00:51:18 +0900

    chore: (#97) validation not blank 추가

--- dms-presentation/src/main/kotlin/team/aliens/dms/user/dto/request/UpdateUserPasswordWebRequest.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/user/dto/request/UpdateUserPasswordWebRequest.kt
@@ -2,12 +2,14 @@ package team.aliens.dms.user.dto.request
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import team.aliens.dms.manager.dto.request.Password
+import javax.validation.constraints.NotBlank
 
 data class UpdateUserPasswordWebRequest(
 
     @field:JsonFormat(shape = JsonFormat.Shape.STRING)
     val oldPassword: Password,
 
+    @field:NotBlank
     @field:JsonFormat(shape = JsonFormat.Shape.STRING)
     val newPassword: Password
 
