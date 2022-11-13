4a5b78c4248ac379cd6b386b1ae966b3d68a65a6
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-13 22:04:39 +0900

    feat: (#95) gcn 필드 추가 & number 한자리 숫자일때 0 붙히기

--- dms-domain/src/main/kotlin/team/aliens/dms/domain/student/model/Student.kt
+++ dms-domain/src/main/kotlin/team/aliens/dms/domain/student/model/Student.kt
@@ -18,4 +18,9 @@ data class Student(
 
     val number: Int
 
-)
+) {
+
+    val gcn: String = "${this.grade}${this.classRoom}${number()}"
+
+    private fun number() = if (number < 10) "0${number}".toInt() else number
+}
