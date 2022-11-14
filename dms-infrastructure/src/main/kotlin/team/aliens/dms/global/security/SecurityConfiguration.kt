2ba303072c27ab6f3191e3ae7429c52be601c26e
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-14 18:46:09 +0900

    style: (#103) url 위치 변경

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
@@ -60,10 +60,10 @@ class SecurityConfiguration(
             .antMatchers(HttpMethod.GET, "/managers/account-id/{school-id}").permitAll()
             .antMatchers(HttpMethod.GET, "/managers/students").hasAuthority(MANAGER.name)
             .antMatchers(HttpMethod.GET, "/managers/students/{student-id}").hasAuthority(MANAGER.name)
+            .antMatchers(HttpMethod.GET, "/managers/profile").hasAuthority(MANAGER.name)
             .antMatchers(HttpMethod.PATCH, "managers/password/initialization").permitAll()
             .antMatchers(HttpMethod.PATCH, "/managers/password/initialization").permitAll()
             .antMatchers(HttpMethod.DELETE, "/managers/students/{student-id}").hasAuthority(MANAGER.name)
-            .antMatchers(HttpMethod.GET, "/managers/profile").hasAuthority(MANAGER.name)
 
             // /schools
             .antMatchers(HttpMethod.GET, "/schools").permitAll()
