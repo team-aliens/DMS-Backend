3c961bdf5418290742da4fc07b92fd4ea55c80e0
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-14 14:28:04 +0900

    chore: (#105) http url

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/SecurityConfiguration.kt
@@ -43,6 +43,7 @@ class SecurityConfiguration(
             .antMatchers(HttpMethod.PUT, "/auth/reissue").permitAll()
 
             // /users
+            .antMatchers(HttpMethod.GET, "/users/password").hasAnyAuthority(STUDENT.name, MANAGER.name)
             .antMatchers(HttpMethod.PATCH, "/users/password").hasAnyAuthority(STUDENT.name, MANAGER.name)
 
             // /students
