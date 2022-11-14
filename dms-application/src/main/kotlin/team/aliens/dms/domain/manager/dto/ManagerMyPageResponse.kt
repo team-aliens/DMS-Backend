c76f2ca68b1bcff05509d23290a76132706ff5ee
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-14 13:29:23 +0900

    feat: (#103) manager my page response

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/manager/dto/ManagerMyPageResponse.kt
@@ -0,0 +1,17 @@
+package team.aliens.dms.domain.manager.dto
+
+import java.util.UUID
+
+data class ManagerMyPageResponse(
+
+    val schoolId: UUID,
+
+    val schoolName: String,
+
+    val code: String,
+
+    val question: String,
+
+    val answer: String
+
+)
