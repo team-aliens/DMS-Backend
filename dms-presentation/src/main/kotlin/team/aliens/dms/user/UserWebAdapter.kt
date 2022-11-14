8a17cf2e2a565533c4c48a814daea495ea6d2920
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-14 14:28:11 +0900

    feat: (#105) user password compare api

--- dms-presentation/src/main/kotlin/team/aliens/dms/user/UserWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/user/UserWebAdapter.kt
@@ -1,23 +1,37 @@
 package team.aliens.dms.user
 
 import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import team.aliens.dms.domain.user.dto.UpdateUserPasswordRequest
 import team.aliens.dms.domain.user.usecase.UpdateUserPasswordUseCase
+import team.aliens.dms.domain.user.usecase.UserPasswordCompareUseCase
 import team.aliens.dms.user.dto.request.UpdateUserPasswordWebRequest
+import javax.validation.Valid
+import javax.validation.constraints.NotBlank
 
+@Validated
 @RequestMapping("/users")
 @RestController
 class UserWebAdapter(
+    private val userPasswordCompareUseCase: UserPasswordCompareUseCase,
     private val updateUserPasswordUseCase: UpdateUserPasswordUseCase
 ) {
 
+    @GetMapping("/password")
+    fun passwordCompare(@RequestParam @NotBlank password: String) {
+        userPasswordCompareUseCase.execute(password)
+    }
+
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/password")
-    fun updatePassword(webRequest: UpdateUserPasswordWebRequest) {
+    fun updatePassword(@RequestBody @Valid webRequest: UpdateUserPasswordWebRequest) {
         val request = UpdateUserPasswordRequest(
             oldPassword = webRequest.oldPassword.value,
             newPassword = webRequest.newPassword.value
