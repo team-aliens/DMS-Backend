31c1eb62ce6064990212fce6a534940233b70f89
KimBeomJin <sdpthf@naver.com>
2022-11-14 20:15:49 +0900

    style: (#102) unused import

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/point/repository/vo/QueryPointHistoryVO.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/point/repository/vo/QueryPointHistoryVO.kt
@@ -2,7 +2,6 @@ package team.aliens.dms.persistence.point.repository.vo
 
 import com.querydsl.core.annotations.QueryProjection
 import team.aliens.dms.domain.point.model.PointType
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.UUID
 
