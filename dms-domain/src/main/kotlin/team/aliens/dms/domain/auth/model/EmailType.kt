c2f71d293f5ba6d736a219c9c9ac7f140332178a
KimBeomJin <sdpthf@naver.com>
2022-11-16 19:48:12 +0900

    chore: (#112) templateSubject 추가

--- dms-domain/src/main/kotlin/team/aliens/dms/domain/auth/model/EmailType.kt
+++ dms-domain/src/main/kotlin/team/aliens/dms/domain/auth/model/EmailType.kt
@@ -1,9 +1,10 @@
 package team.aliens.dms.domain.auth.model
 
 enum class EmailType(
-    val templateName: String
+    val templateName: String,
+    val templateSubject: String
 ) {
 
-    // TODO 템플릿 이름 추가
-    SIGNUP(""), PASSWORD("")
+    SIGNUP("DMS_SIGNUP_TEMPLATE", "DMS 회원가입 이메일 승인코드"),
+    PASSWORD("DMS_PASSWORD_TEMPLATE", "DMS 비밀번호 변경 이메일 승인코드")
 }
