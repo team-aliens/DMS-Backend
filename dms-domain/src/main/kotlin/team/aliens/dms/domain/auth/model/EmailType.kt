db6a773ef76b857d1aaad1faf7425e890a3b8666
KimBeomJin <sdpthf@naver.com>
2022-11-16 22:40:56 +0900

    chore: (#112) EmailType에 file name 추가

--- dms-domain/src/main/kotlin/team/aliens/dms/domain/auth/model/EmailType.kt
+++ dms-domain/src/main/kotlin/team/aliens/dms/domain/auth/model/EmailType.kt
@@ -1,10 +1,11 @@
 package team.aliens.dms.domain.auth.model
 
 enum class EmailType(
+    val fileName: String,
     val templateName: String,
     val templateSubject: String
 ) {
 
-    SIGNUP("DMS_SIGNUP_TEMPLATE", "DMS 회원가입 이메일 승인코드"),
-    PASSWORD("DMS_PASSWORD_TEMPLATE", "DMS 비밀번호 변경 이메일 승인코드")
+    SIGNUP("signup_template", "DMS_SIGNUP_TEMPLATE", "DMS 회원가입 이메일 승인코드"),
+    PASSWORD("password_template", "DMS_PASSWORD_TEMPLATE", "DMS 비밀번호 변경 이메일 승인코드")
 }
