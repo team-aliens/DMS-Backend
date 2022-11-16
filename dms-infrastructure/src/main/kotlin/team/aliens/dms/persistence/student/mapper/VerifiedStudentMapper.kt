9b47f4e3baa089b195a0d4eb5f39fa0474f3dd97
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-16 11:23:41 +0900

    feat: (#111) verified student entity & repository & domain & mapper 추가

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/student/mapper/VerifiedStudentMapper.kt
@@ -0,0 +1,40 @@
+package team.aliens.dms.persistence.student.mapper
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import team.aliens.dms.domain.student.model.VerifiedStudent
+import team.aliens.dms.persistence.GenericMapper
+import team.aliens.dms.persistence.school.repository.SchoolJpaRepository
+import team.aliens.dms.persistence.student.entity.VerifiedStudentJpaEntity
+import team.aliens.dms.persistence.student.entity.VerifiedStudentJpaEntityId
+
+@Component
+class VerifiedStudentMapper(
+    private val schoolJpaRepository: SchoolJpaRepository
+) : GenericMapper<VerifiedStudent, VerifiedStudentJpaEntity> {
+
+    override fun toDomain(entity: VerifiedStudentJpaEntity?): VerifiedStudent? {
+        return entity?.let {
+            VerifiedStudent(
+                gcn = entity.id.gcn,
+                schoolId = entity.id.schoolId,
+                name = entity.name,
+                roomNumber = entity.roomNumber
+            )
+        }
+    }
+
+    override fun toEntity(domain: VerifiedStudent): VerifiedStudentJpaEntity {
+        val school = schoolJpaRepository.findByIdOrNull(domain.schoolId)
+
+        return VerifiedStudentJpaEntity(
+            id = VerifiedStudentJpaEntityId(
+                gcn = domain.gcn,
+                schoolId = domain.schoolId
+            ),
+            school = school,
+            name = domain.name,
+            roomNumber = domain.roomNumber
+        )
+    }
+}
