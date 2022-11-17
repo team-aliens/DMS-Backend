41f2e178c6d3bdfff50110c51728fccd07138e9a
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-18 08:37:22 +0900

    fix: (#117) query point history usecase

--- dms-application/src/main/kotlin/team/aliens/dms/domain/point/usecase/QueryPointHistoryUseCase.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/point/usecase/QueryPointHistoryUseCase.kt
@@ -3,28 +3,23 @@ package team.aliens.dms.domain.point.usecase
 import team.aliens.dms.common.annotation.ReadOnlyUseCase
 import team.aliens.dms.domain.point.dto.PointRequestType
 import team.aliens.dms.domain.point.dto.QueryPointHistoryResponse
-import team.aliens.dms.domain.point.spi.PointQueryUserPort
 import team.aliens.dms.domain.point.spi.PointSecurityPort
 import team.aliens.dms.domain.point.spi.QueryPointPort
-import team.aliens.dms.domain.user.exception.UserNotFoundException
 
 @ReadOnlyUseCase
 class QueryPointHistoryUseCase(
-    private val queryPointPort: QueryPointPort,
-    private val queryUserPort: PointQueryUserPort,
-    private val securityPort: PointSecurityPort
+    private val securityPort: PointSecurityPort,
+    private val queryPointPort: QueryPointPort
 ) {
 
     fun execute(type: PointRequestType): QueryPointHistoryResponse {
         val pointType = PointRequestType.toPointType(type)
-
-        val currentUserId = securityPort.getCurrentUserId()
-        val student = queryUserPort.queryUserById(currentUserId) ?: throw UserNotFoundException
+        val currentStudentId = securityPort.getCurrentUserId()
 
         val pointHistory = if (pointType != null) {
-            queryPointPort.queryPointHistoryByStudentIdAndType(student.id, pointType)
+            queryPointPort.queryPointHistoryByStudentIdAndType(currentStudentId, pointType)
         } else {
-            queryPointPort.queryAllPointHistoryByStudentId(student.id)
+            queryPointPort.queryAllPointHistoryByStudentId(currentStudentId)
         }
 
         return QueryPointHistoryResponse(
