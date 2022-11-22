e6e79e02d180d61c9c16f7db2e3848295810b36d
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-22 18:55:40 +0900

    feat: cors config

---
+++ dms-presentation/src/main/kotlin/team/aliens/dms/common/WebMvcConfig.kt
@@ -0,0 +1,18 @@
+package team.aliens.dms.common
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://localhost:3002"
+            )
+    }
+}
