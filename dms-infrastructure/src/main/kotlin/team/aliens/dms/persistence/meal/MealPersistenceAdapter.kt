3edcc26329560df959503c9d28971b85d84f54b0
KimBeomJin <sdpthf@naver.com>
2022-11-26 16:11:18 +0900

    fix: (#141) 해당 달의 모든 급식 조회
    Co-authored-by: jeongyoon05 <dlwjddbs14@naver.com>

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/meal/MealPersistenceAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/meal/MealPersistenceAdapter.kt
@@ -3,10 +3,10 @@ package team.aliens.dms.persistence.meal
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.meal.model.Meal
 import team.aliens.dms.domain.meal.spi.MealPort
-import team.aliens.dms.persistence.meal.entity.MealJpaEntityId
 import team.aliens.dms.persistence.meal.mapper.MealMapper
 import team.aliens.dms.persistence.meal.repository.MealJpaRepository
 import java.time.LocalDate
+import java.time.YearMonth
 import java.util.UUID
 
 @Component
@@ -16,9 +16,12 @@ class MealPersistenceAdapter(
 ) : MealPort {
 
     override fun queryAllMealsByMealDateAndSchoolId(mealDate: LocalDate, schoolId: UUID): List<Meal> {
-        val mealId = MealJpaEntityId(mealDate, schoolId)
+        val month = YearMonth.from(mealDate)
 
-        return mealRepository.findAllById(mealId).map {
+        val firstDay = month.atDay(1)
+        val lastDay = month.atEndOfMonth()
+
+        return mealRepository.findBySchoolIdAndIdMealDateBetween(schoolId, firstDay, lastDay).map {
             mealMapper.toDomain(it)!!
         }
     }
