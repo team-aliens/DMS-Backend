f22f41609e841c13129535b05f272cffc582844c
KimBeomJin <sdpthf@naver.com>
2022-11-26 16:49:49 +0900

    chore: (#141) QueryMealsResponse에 팩터리 메서드 패턴 적용

--- dms-application/src/main/kotlin/team/aliens/dms/domain/meal/dto/QueryMealsResponse.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/meal/dto/QueryMealsResponse.kt
@@ -1,14 +1,34 @@
 package team.aliens.dms.domain.meal.dto
 
+import team.aliens.dms.domain.meal.model.Meal
 import java.time.LocalDate
 
 data class QueryMealsResponse(
-    val meals: List<MealDetails>
+    val meals: List<MealDetails?>
 ) {
     data class MealDetails(
         val date: LocalDate,
         val breakfast: List<String?>,
         val lunch: List<String?>,
         val dinner: List<String?>
-    )
+    ) {
+        companion object {
+            private const val emptyMeal = "급식이 없습니다."
+            fun emptyOf(date: LocalDate) = MealDetails(
+                date = date,
+                breakfast = listOf(emptyMeal),
+                lunch = listOf(emptyMeal),
+                dinner = listOf(emptyMeal)
+            )
+
+            fun of(meal: Meal) = meal.run {
+                MealDetails(
+                    date = mealDate,
+                    breakfast = toSplit(breakfast),
+                    lunch = toSplit(lunch),
+                    dinner = toSplit(dinner)
+                )
+            }
+        }
+    }
 }
