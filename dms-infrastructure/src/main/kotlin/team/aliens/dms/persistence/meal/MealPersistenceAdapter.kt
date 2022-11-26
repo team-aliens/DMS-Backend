1e736df6617777d6451fba9db05ab3a67c74992c
KimBeomJin <sdpthf@naver.com>
2022-11-26 16:47:18 +0900

    refactor: (#141) queryAllMealsByMealDateAndSchoolId -> queryAllMealsByMonthAndSchoolId

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/meal/MealPersistenceAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/meal/MealPersistenceAdapter.kt
@@ -6,7 +6,6 @@ import team.aliens.dms.domain.meal.spi.MealPort
 import team.aliens.dms.persistence.meal.mapper.MealMapper
 import team.aliens.dms.persistence.meal.repository.MealJpaRepository
 import java.time.LocalDate
-import java.time.YearMonth
 import java.util.UUID
 
 @Component
@@ -15,12 +14,11 @@ class MealPersistenceAdapter(
     private val mealRepository: MealJpaRepository
 ) : MealPort {
 
-    override fun queryAllMealsByMealDateAndSchoolId(mealDate: LocalDate, schoolId: UUID): List<Meal> {
-        val month = YearMonth.from(mealDate)
-
-        val firstDay = month.atDay(1)
-        val lastDay = month.atEndOfMonth()
-
+    override fun queryAllMealsByMonthAndSchoolId(
+        firstDay: LocalDate,
+        lastDay: LocalDate,
+        schoolId: UUID
+    ): List<Meal> {
         return mealRepository.findBySchoolIdAndIdMealDateBetween(schoolId, firstDay, lastDay).map {
             mealMapper.toDomain(it)!!
         }
