42495cf5e38a7b5f22006e4798e406ee8b911a5d
KimBeomJin <sdpthf@naver.com>
2022-11-26 16:58:44 +0900

    chore: (#141) 해당 달의 모든 급식 조회
    없으면 기본 문구 출력, 있으면 DB 값 출력

--- dms-application/src/main/kotlin/team/aliens/dms/domain/meal/usecase/QueryMealsUseCase.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/meal/usecase/QueryMealsUseCase.kt
@@ -1,6 +1,7 @@
 package team.aliens.dms.domain.meal.usecase
 
 import team.aliens.dms.common.annotation.ReadOnlyUseCase
+import team.aliens.dms.common.iterator
 import team.aliens.dms.domain.meal.dto.QueryMealsResponse
 import team.aliens.dms.domain.meal.dto.QueryMealsResponse.MealDetails
 import team.aliens.dms.domain.meal.spi.MealQueryStudentPort
@@ -8,6 +9,7 @@ import team.aliens.dms.domain.meal.spi.MealSecurityPort
 import team.aliens.dms.domain.meal.spi.QueryMealPort
 import team.aliens.dms.domain.student.exception.StudentNotFoundException
 import java.time.LocalDate
+import java.time.YearMonth
 
 @ReadOnlyUseCase
 class QueryMealsUseCase(
@@ -19,15 +21,26 @@ class QueryMealsUseCase(
     fun execute(mealDate: LocalDate): QueryMealsResponse {
         val currentUserId = securityPort.getCurrentUserId()
         val student = queryStudentPort.queryStudentById(currentUserId) ?: throw StudentNotFoundException
-        val meals = queryMealPort.queryAllMealsByMealDateAndSchoolId(mealDate, student.schoolId)
 
-        val mealDetails = meals.map {
-            MealDetails(
-                date = it.mealDate,
-                breakfast = it.toSplit(it.breakfast),
-                lunch = it.toSplit(it.lunch),
-                dinner = it.toSplit(it.dinner)
-            )
+        val month = YearMonth.from(mealDate)
+        val firstDay = month.atDay(1)
+        val lastDay = month.atEndOfMonth()
+
+        val mealMap = queryMealPort.queryAllMealsByMonthAndSchoolId(
+            firstDay, lastDay, student.schoolId
+        ).groupBy { it.mealDate }
+
+        val mealDetails = mutableListOf<MealDetails?>()
+        for (date in firstDay..lastDay) {
+            val meals = mealMap[date]
+
+            if (meals == null) {
+                mealDetails.add(MealDetails.emptyOf(date))
+            } else {
+                meals[0].apply {
+                    mealDetails.add(MealDetails.of(this))
+                }
+            }
         }
 
         return QueryMealsResponse(mealDetails)
