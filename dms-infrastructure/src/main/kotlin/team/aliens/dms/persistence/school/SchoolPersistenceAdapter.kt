634c72c2a546e9aa036bbba6702b00cff677e65f
KimBeomJin <sdpthf@naver.com>
2022-12-04 22:52:30 +0900

    chore: (#151) queryAvailableFeaturesBySchoolId

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/school/SchoolPersistenceAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/school/SchoolPersistenceAdapter.kt
@@ -2,16 +2,21 @@ package team.aliens.dms.persistence.school
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
+import team.aliens.dms.domain.school.model.AvailableFeature
 import team.aliens.dms.domain.school.model.School
 import team.aliens.dms.domain.school.spi.SchoolPort
+import team.aliens.dms.persistence.school.mapper.AvailableFeatureMapper
 import team.aliens.dms.persistence.school.mapper.SchoolMapper
+import team.aliens.dms.persistence.school.repository.AvailableFeatureJpaRepository
 import team.aliens.dms.persistence.school.repository.SchoolJpaRepository
 import java.util.UUID
 
 @Component
 class SchoolPersistenceAdapter(
     private val schoolMapper: SchoolMapper,
-    private val schoolRepository: SchoolJpaRepository
+    private val availableFeatureMapper: AvailableFeatureMapper,
+    private val schoolRepository: SchoolJpaRepository,
+    private val availableFeatureRepository: AvailableFeatureJpaRepository
 ) : SchoolPort {
 
     override fun queryAllSchools() = schoolRepository.findAll().map {
@@ -23,6 +28,10 @@ class SchoolPersistenceAdapter(
             schoolMapper.toEntity(school)
         )
     )!!
+
+    override fun queryAvailableFeaturesBySchoolId(schoolId: UUID) = availableFeatureMapper.toDomain(
+        availableFeatureRepository.findByIdOrNull(schoolId)
+    )
 
     override fun querySchoolById(schoolId: UUID) = schoolMapper.toDomain(
         schoolRepository.findByIdOrNull(schoolId)
