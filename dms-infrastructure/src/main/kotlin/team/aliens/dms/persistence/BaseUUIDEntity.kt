0f56460dcdf5d904630348b27f7dc6e0a7e034a2
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-08 17:28:04 +0900

    feat: (#153) time bases uuid generator 추가 & 적용

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/BaseUUIDEntity.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/BaseUUIDEntity.kt
@@ -1,16 +1,19 @@
 package team.aliens.dms.persistence
 
-import com.fasterxml.uuid.Generators
 import java.util.UUID
 import javax.persistence.Column
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.MappedSuperclass
+import org.hibernate.annotations.GenericGenerator
 
 @MappedSuperclass
 abstract class BaseUUIDEntity(
 
     @Id
+    @GeneratedValue(generator = "timeBasedUUID")
+    @GenericGenerator(name = "timeBasedUUID", strategy = "team.aliens.dms.persistence.TimeBasedUUIDGenerator")
     @Column(columnDefinition = "BINARY(16)", nullable = false)
-    val id: UUID = Generators.timeBasedGenerator().generate()
+    val id: UUID?
 
 )
