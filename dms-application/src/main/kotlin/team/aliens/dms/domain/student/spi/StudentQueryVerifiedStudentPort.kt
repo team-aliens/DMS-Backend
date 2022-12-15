262c95c4e99f299dfa26d1f867896f77e41c44e4
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-15 23:53:39 +0900

    feat: (#156) 학번 & 학교이름으로 검증된 학생 조회

--- dms-application/src/main/kotlin/team/aliens/dms/domain/student/spi/StudentQueryVerifiedStudentPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/student/spi/StudentQueryVerifiedStudentPort.kt
@@ -1,10 +1,9 @@
 package team.aliens.dms.domain.student.spi
 
-import java.util.UUID
 import team.aliens.dms.domain.student.model.VerifiedStudent
 
 interface StudentQueryVerifiedStudentPort {
 
-    fun queryVerifiedStudentByGcnAndSchoolId(gcn: String, schoolId: UUID): VerifiedStudent?
+    fun queryVerifiedStudentByGcnAndSchoolName(gcn: String, schoolName: String): VerifiedStudent?
 
 }
