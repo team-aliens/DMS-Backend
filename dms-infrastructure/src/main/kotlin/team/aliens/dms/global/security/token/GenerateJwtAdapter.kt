64ed59fd2c2cbc99fd6be5e2a211d374014853a9
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-15 14:51:15 +0900

    style: (#164) 패키지 이름 변경

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/token/GenerateJwtAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/token/GenerateJwtAdapter.kt
@@ -8,7 +8,7 @@ import team.aliens.dms.domain.auth.dto.TokenResponse
 import team.aliens.dms.domain.auth.model.Authority
 import team.aliens.dms.domain.auth.spi.JwtPort
 import team.aliens.dms.global.security.SecurityProperties
-import team.aliens.dms.persistence.auth.model.RefreshTokenEntity
+import team.aliens.dms.persistence.auth.entity.RefreshTokenEntity
 import team.aliens.dms.persistence.auth.repository.RefreshTokenRepository
 import java.time.LocalDateTime
 import java.util.Date
--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/auth/repository/RefreshTokenRepository.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/auth/repository/RefreshTokenRepository.kt
@@ -2,7 +2,7 @@ package team.aliens.dms.persistence.auth.repository
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import team.aliens.dms.persistence.auth.model.RefreshTokenEntity
+import team.aliens.dms.persistence.auth.entity.RefreshTokenEntity
 
 @Repository
 interface RefreshTokenRepository : CrudRepository<RefreshTokenEntity, String> {
