471be480c68be4eb811af95edd23fafc6f1ec3d7
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-20 16:00:23 +0900

    feat: (#169) study room available time jpa entity & repository

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/studyroom/repository/StudyRoomAvailableTimeJpaRepository.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms.persistence.studyroom.repository
+
+import java.util.UUID
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import team.aliens.dms.persistence.studyroom.entity.StudyRoomAvailableTimeJpaEntity
+
+@Repository
+interface StudyRoomAvailableTimeJpaRepository : CrudRepository<StudyRoomAvailableTimeJpaEntity, UUID> {
+}
