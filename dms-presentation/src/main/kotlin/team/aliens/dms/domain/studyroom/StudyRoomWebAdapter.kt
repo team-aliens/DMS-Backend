c704936a9fa2c0d5461cd369a95ef6114b3f2f01
KimBeomJin <sdpthf@naver.com>
2022-12-21 15:11:35 +0900

    chore: (#179) 자습실 신청 시간 설정 web adapter

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/studyroom/StudyRoomWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/studyroom/StudyRoomWebAdapter.kt
@@ -1,18 +1,33 @@
 package team.aliens.dms.domain.studyroom
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import team.aliens.dms.domain.studyroom.dto.QueryStudyRoomAvailableTimeResponse
+import team.aliens.dms.domain.studyroom.dto.UpdateAvailableTimeWebRequest
 
 @RequestMapping("/study-rooms")
 @RestController
 class StudyRoomWebAdapter(
-    private val queryStudyRoomAvailableTimeUseCase: QueryStudyRoomAvailableTimeUseCase
+    private val queryStudyRoomAvailableTimeUseCase: QueryStudyRoomAvailableTimeUseCase,
+    private val updateAvailableTimeUseCase: UpdateAvailableTimeUseCase
 ) {
 
     @GetMapping("/available-time")
     fun getAvailableTime(): QueryStudyRoomAvailableTimeResponse {
         return queryStudyRoomAvailableTimeUseCase.execute()
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/available-time")
+    fun updateAvailableTime(@RequestBody request: UpdateAvailableTimeWebRequest) {
+        updateAvailableTimeUseCase.execute(
+            startAt = request.startAt,
+            endAt = request.endAt
+        )
     }
 }
