010717a0a86e4455c8fbcc2dead42d2df31c6f39
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-21 17:06:35 +0900

    feat: (#178) school 조인 외래키 추가

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/studyroom/mapper/SeatTypeMapper.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/studyroom/mapper/SeatTypeMapper.kt
@@ -1,17 +1,22 @@
 package team.aliens.dms.persistence.studyroom.mapper
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.studyroom.model.SeatType
 import team.aliens.dms.persistence.GenericMapper
+import team.aliens.dms.persistence.school.repository.SchoolJpaRepository
 import team.aliens.dms.persistence.studyroom.entity.SeatTypeJpaEntity
 
 @Component
-class SeatTypeMapper : GenericMapper<SeatType, SeatTypeJpaEntity>{
+class SeatTypeMapper(
+    private val schoolRepository: SchoolJpaRepository
+) : GenericMapper<SeatType, SeatTypeJpaEntity>{
 
     override fun toDomain(entity: SeatTypeJpaEntity?): SeatType? {
         return entity?.let {
             SeatType(
                 id = it.id!!,
+                schoolId = it.school!!.id!!,
                 name = it.name,
                 color = it.color
             )
@@ -19,8 +24,11 @@ class SeatTypeMapper : GenericMapper<SeatType, SeatTypeJpaEntity>{
     }
 
     override fun toEntity(domain: SeatType): SeatTypeJpaEntity {
+        val school = schoolRepository.findByIdOrNull(domain.schoolId)
+
         return SeatTypeJpaEntity(
             id = domain.id,
+            school = school,
             name = domain.name,
             color = domain.color
         )
