20270e5f49a1ff141db51f0957710dd0598fdb71
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-21 23:49:03 +0900

    feat: (#191) 학교 비교 로직 추가

--- dms-application/src/main/kotlin/team/aliens/dms/domain/studyroom/usecase/ApplySeatUseCase.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/studyroom/usecase/ApplySeatUseCase.kt
@@ -2,24 +2,37 @@ package team.aliens.dms.domain.studyroom.usecase
 
 import java.util.UUID
 import team.aliens.dms.common.annotation.UseCase
+import team.aliens.dms.domain.school.exception.SchoolMismatchException
 import team.aliens.dms.domain.studyroom.exception.SeatAlreadyAppliedException
 import team.aliens.dms.domain.studyroom.exception.SeatNotFoundException
+import team.aliens.dms.domain.studyroom.exception.StudyRoomNotFoundException
 import team.aliens.dms.domain.studyroom.model.Seat
 import team.aliens.dms.domain.studyroom.spi.CommandSeatPort
 import team.aliens.dms.domain.studyroom.spi.QuerySeatPort
+import team.aliens.dms.domain.studyroom.spi.QueryStudyRoomPort
+import team.aliens.dms.domain.studyroom.spi.StudyRoomQueryUserPort
 import team.aliens.dms.domain.studyroom.spi.StudyRoomSecurityPort
+import team.aliens.dms.domain.user.exception.UserNotFoundException
 
 @UseCase
 class ApplySeatUseCase(
     private val securityPort: StudyRoomSecurityPort,
+    private val queryUserPort: StudyRoomQueryUserPort,
     private val querySeatPort: QuerySeatPort,
+    private val queryStudyRoomPort: QueryStudyRoomPort,
     private val commandSeatPort: CommandSeatPort
 ) {
 
     fun execute(seatId: UUID) {
         val currentUserId = securityPort.getCurrentUserId()
+        val user = queryUserPort.queryUserById(currentUserId) ?: throw UserNotFoundException
 
         val seat = querySeatPort.querySeatById(seatId) ?: throw SeatNotFoundException
+        val studyRoom = queryStudyRoomPort.queryStudyRoomById(seat.studyRoomId) ?: throw StudyRoomNotFoundException
+
+        if (user.schoolId != studyRoom.schoolId) {
+            throw SchoolMismatchException
+        }
 
         val saveSeat = seat.studentId?.run {
             throw SeatAlreadyAppliedException
