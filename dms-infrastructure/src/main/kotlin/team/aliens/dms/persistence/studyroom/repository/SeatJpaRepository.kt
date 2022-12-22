6da59cf8a6afd9bd32631816c076f32ff52a293d
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-22 23:27:51 +0900

    fix: (#196) status로 카운트 필터링 추가

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/studyroom/repository/SeatJpaRepository.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/studyroom/repository/SeatJpaRepository.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.persistence.studyroom.repository
 import java.util.UUID
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import team.aliens.dms.domain.studyroom.model.SeatStatus
 import team.aliens.dms.persistence.studyroom.entity.SeatJpaEntity
 
 @Repository
@@ -10,7 +11,7 @@ interface SeatJpaRepository : CrudRepository<SeatJpaEntity, UUID> {
 
     fun findByStudentId(studentId: UUID): SeatJpaEntity?
 
-    fun countByStudyRoomId(studyRoomId: UUID): Int
+    fun countByStudyRoomIdAndStatus(studyRoomId: UUID, status: SeatStatus): Int
 
     fun findAllByStudyRoomId(studyRoomId: UUID): List<SeatJpaEntity>
 
