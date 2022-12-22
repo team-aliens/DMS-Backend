3e798382b444c91fce8333df623508f5fc3eb176
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-23 08:31:54 +0900

    feat: (#199) save study room port 추가 & 구현

--- dms-application/src/main/kotlin/team/aliens/dms/domain/studyroom/spi/CommandStudyRoomPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/studyroom/spi/CommandStudyRoomPort.kt
@@ -1,9 +1,12 @@
 package team.aliens.dms.domain.studyroom.spi
 
 import team.aliens.dms.domain.studyroom.model.Seat
+import team.aliens.dms.domain.studyroom.model.StudyRoom
 
 interface CommandStudyRoomPort {
 
     fun saveSeat(seat: Seat): Seat
+
+    fun saveStudyRoom(studyRoom: StudyRoom): StudyRoom
 
 }
--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/studyroom/StudyRoomPersistenceAdapter.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/studyroom/StudyRoomPersistenceAdapter.kt
@@ -4,6 +4,7 @@ import java.util.UUID
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.studyroom.model.Seat
+import team.aliens.dms.domain.studyroom.model.StudyRoom
 import team.aliens.dms.domain.studyroom.spi.StudyRoomPort
 import team.aliens.dms.persistence.studyroom.mapper.SeatMapper
 import team.aliens.dms.persistence.studyroom.mapper.StudyRoomMapper
@@ -33,6 +34,12 @@ class StudyRoomPersistenceAdapter(
     override fun saveSeat(seat: Seat) = seatMapper.toDomain(
         seatRepository.save(
             seatMapper.toEntity(seat)
+        )
+    )!!
+
+    override fun saveStudyRoom(studyRoom: StudyRoom) = studyRoomMapper.toDomain(
+        studyRoomRepository.save(
+            studyRoomMapper.toEntity(studyRoom)
         )
     )!!
 }
