ea1be2ba590ebba66973dfa36b4a15d1119d7bb5
KimBeomJin <sdpthf@naver.com>
2022-12-24 18:09:57 +0900

    chore: (#182, #186) 자습실 생성, 수정 시 web request validation

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/studyroom/dto/CreateStudyRoomWebRequest.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/studyroom/dto/CreateStudyRoomWebRequest.kt
@@ -1,25 +1,55 @@
 package team.aliens.dms.domain.studyroom.dto
 
 import java.util.UUID
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class CreateStudyRoomWebRequest(
+
+    @field:NotNull
     val floor: Int,
+
+    @field:NotBlank
     val name: String,
+
+    @field:NotNull
     val totalWidthSize: Int,
+
+    @field:NotNull
     val totalHeightSize: Int,
+
+    @field:NotBlank
     val eastDescription: String,
+
+    @field:NotBlank
     val westDescription: String,
+
+    @field:NotBlank
     val southDescription: String,
+
+    @field:NotBlank
     val northDescription: String,
+
+    @field:NotBlank
     val availableSex: String,
+
+    @field:NotNull
     val availableGrade: Int,
+
     val seats: List<SeatRequest>
 ) {
     data class SeatRequest(
+
+        @field:NotNull
         val widthLocation: Int,
+
+        @field:NotNull
         val heightLocation: Int,
+
         val number: Int?,
         val typeId: UUID?,
+
+        @field:NotNull
         val status: WebSeatStatus
     )
 }
