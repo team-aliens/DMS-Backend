87a28d07f2e623e256c69b22a7fdc7af2cee9209
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-24 19:20:12 +0900

    chore: (#196) 사용하지 않는 메소드 제거

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/studyroom/repository/SeatJpaRepository.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/studyroom/repository/SeatJpaRepository.kt
@@ -3,14 +3,11 @@ package team.aliens.dms.persistence.studyroom.repository
 import java.util.UUID
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import team.aliens.dms.domain.studyroom.model.SeatStatus
 import team.aliens.dms.persistence.studyroom.entity.SeatJpaEntity
 
 @Repository
 interface SeatJpaRepository : CrudRepository<SeatJpaEntity, UUID> {
 
     fun findByStudentId(studentId: UUID): SeatJpaEntity?
-
-    fun countByStudyRoomIdAndStatus(studyRoomId: UUID, status: SeatStatus): Int
 
 }
