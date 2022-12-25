17b74e9c64fab7372500c19792c3b4857166b4c0
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-26 02:45:17 +0900

    feat: (#197) query study room manager router & response

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/studyroom/dto/ManagerQueryStudyRoomResponse.kt
@@ -0,0 +1,46 @@
+package team.aliens.dms.domain.studyroom.dto
+
+import java.util.UUID
+import team.aliens.dms.domain.student.model.Sex
+import team.aliens.dms.domain.studyroom.model.SeatStatus
+
+data class ManagerQueryStudyRoomResponse(
+    val floor: Int,
+    val name: String,
+    val totalAvailableSeat: Int,
+    val availableSex: Sex,
+    val availableGrade: Int,
+    val eastDescription: String,
+    val westDescription: String,
+    val southDescription: String,
+    val northDescription: String,
+    val totalWidthSize: Int,
+    val totalHeightSize: Int,
+    val seats: List<SeatElement>
+) {
+
+    data class SeatElement(
+        val id: UUID,
+        val widthLocation: Int,
+        val heightLocation: Int,
+        val number: Int?,
+        val type: TypeElement?,
+        val status: SeatStatus,
+        val student: StudentElement?,
+    ) {
+
+        data class TypeElement(
+            val id: UUID,
+            val name: String,
+            val color: String
+        )
+
+        data class StudentElement(
+            val id: UUID,
+            val gcn: String,
+            val name: String,
+            val profileImageUrl: String
+        )
+    }
+}
+
