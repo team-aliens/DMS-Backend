6bdecebc459a07d30002284f10d5ca6331e70939
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-26 15:52:03 +0900

    feat: (#204) 바뀐 쿼리에 맞게 변경

--- dms-application/src/main/kotlin/team/aliens/dms/domain/studyroom/usecase/StudentQueryStudyRoomsUseCase.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/studyroom/usecase/StudentQueryStudyRoomsUseCase.kt
@@ -1,6 +1,5 @@
 package team.aliens.dms.domain.studyroom.usecase
 
-import java.util.UUID
 import team.aliens.dms.common.annotation.ReadOnlyUseCase
 import team.aliens.dms.domain.studyroom.dto.StudentQueryStudyRoomsResponse
 import team.aliens.dms.domain.studyroom.dto.StudentQueryStudyRoomsResponse.StudyRoomElement
@@ -27,23 +26,14 @@ class StudentQueryStudyRoomsUseCase(
                 name = it.name,
                 availableGrade = it.availableGrade,
                 availableSex = it.availableSex,
-                inUseHeadcount = it.inUseHeadcount!!,
-                totalAvailableSeat = queryStudyRoomPort.countSeatByStudyRoomId(it.id),
-                isMine = isMine(
-                    studyRoomId = it.id,
-                    currentUserId = currentUserId
-                )
+                inUseHeadcount = it.inUseHeadcount,
+                totalAvailableSeat = it.totalAvailableSeat,
+                isMine = true // 보류
             )
         }
 
         return StudentQueryStudyRoomsResponse(
             studyRooms = studyRoom
         )
-    }
-
-    private fun isMine(studyRoomId: UUID, currentUserId: UUID) : Boolean {
-        val studentId = queryStudyRoomPort.querySeatByStudyRoomId(studyRoomId)?.studentId
-
-        return studentId == currentUserId
     }
 }
