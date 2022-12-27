9076afe206fd8a7d448dd75a8d376a6a15f3528d
KimBeomJin <sdpthf@naver.com>
2022-12-27 19:25:09 +0900

    fix: 상/벌점 총합 로직

--- dms-application/src/main/kotlin/team/aliens/dms/domain/point/usecase/QueryPointHistoryUseCase.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/point/usecase/QueryPointHistoryUseCase.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.domain.point.usecase
 import team.aliens.dms.common.annotation.ReadOnlyUseCase
 import team.aliens.dms.domain.point.dto.PointRequestType
 import team.aliens.dms.domain.point.dto.QueryPointHistoryResponse
+import team.aliens.dms.domain.point.model.PointType
 import team.aliens.dms.domain.point.spi.PointSecurityPort
 import team.aliens.dms.domain.point.spi.QueryPointPort
 
@@ -22,13 +23,20 @@ class QueryPointHistoryUseCase(
             queryPointPort.queryAllPointHistoryByStudentId(currentStudentId)
         }
 
+        val totalPoint = pointHistory.sumOf {
+            if (it.type == PointType.MINUS) {
+                it.score * -1
+            }
+            it.score
+        }
+
         return QueryPointHistoryResponse(
             /**
-             * BONUS -> 상점의 총합
-             * MINUS -> 벌점의 총합(-로 계산되는 거 아님) ex) 벌점 3점 = 3
+             * BONUS -> 상점의 총합 ex) 상점 3점 = 3
+             * MINUS -> 벌점의 총합 ex) 벌점 3점 = -3
              * ALL -> 상점 + 벌점의 총합
              **/
-            totalPoint = pointHistory.sumOf { it.score },
+            totalPoint = totalPoint,
             points = pointHistory
         )
     }
