bd58c228d97daecea7f6b2aa3d7cf2aa64f2efd6
Jeong Yoon <dlwjddbs14@naver.com>
2022-12-30 11:45:51 +0900

    feat: (#215) student validation 추가

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/student/dto/request/SignUpWebRequest.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/student/dto/request/SignUpWebRequest.kt
@@ -8,40 +8,41 @@ import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
 data class SignUpWebRequest(
 
     @field:NotBlank
-    @field:Length(min = 8, max = 8)
+    @field:Size(min = 8, max = 8, message = "학교 코드는 8글자 이여야 합니다.")
     val schoolCode: String?,
 
     @field:NotBlank
-    @field:Length(max = 100)
+    @field:Size(max = 100)
     val schoolAnswer: String?,
 
     @field:NotBlank
     @field:Email
+    @field:Size(max = 255)
     val email: String?,
 
     @field:NotBlank
-    @field:Length(min = 6, max = 6)
+    @field:Size(min = 6, max = 6, message = "인증 코드는 6글자 이여야 합니다.")
     val authCode: String?,
 
     @field:NotNull
-    @field:Min(1)
-    @field:Max(6)
+    @field:Size(min = 1, max = 6)
     val grade: Int?,
 
     @field:NotNull
-    @field:Min(1)
+    @field:Size(min = 1)
     val classRoom: Int?,
 
     @field:NotNull
-    @field:Min(1)
+    @field:Size(min = 1)
     val number: Int?,
 
     @field:NotBlank
-    @field:Length(max = 20)
+    @field:Size(min = 3, max = 20)
     val accountId: String?,
 
     @field:NotBlank
@@ -49,8 +50,10 @@ data class SignUpWebRequest(
         regexp = Password.PATTERN,
         message = Password.MESSAGE
     )
+    @field:Size(min = 8, max = 20)
     val password: String?,
 
+    @field:Size(max = 500)
     val profileImageUrl: String?
 
 )
