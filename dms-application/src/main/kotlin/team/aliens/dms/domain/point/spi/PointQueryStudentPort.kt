2646cd8723afb8affc814f313eb42ed46deedb3d
rlaisqls <rlaisqls@gmail.com>
2023-02-15 21:44:42 +0900

    init: (#222) PointQueryStudentPort

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/point/spi/PointQueryStudentPort.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms.domain.point.spi
+
+import team.aliens.dms.domain.student.model.Student
+import java.util.UUID
+
+interface PointQueryStudentPort {
+
+    fun queryStudentById(studentId: UUID): Student?
+
+}
