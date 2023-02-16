3235262a31138094c9deb9a8ed2145bad5b2a271
rlaisqls <rlaisqls@gmail.com>
2023-02-16 16:47:40 +0900

    feat: (#225) remain option jpa entity & repository

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/remain/repository/RemainOptionJpaRepository.kt
@@ -0,0 +1,11 @@
+package team.aliens.dms.persistence.remain.repository
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import team.aliens.dms.persistence.remain.entity.RemainOptionJpaEntity
+import java.util.UUID
+
+
+@Repository
+interface RemainOptionJpaRepository : CrudRepository<RemainOptionJpaEntity, UUID> {
+}
