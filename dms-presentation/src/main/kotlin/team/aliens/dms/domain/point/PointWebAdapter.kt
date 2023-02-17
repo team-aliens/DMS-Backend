0e0a4ecdaa9a87c8a027cf93020317678134f608
이하성 <tedsoftj1123@gmail.com>
2023-02-17 17:07:04 +0900

    chore: (#230) set resposeStatus

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/PointWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/PointWebAdapter.kt
@@ -1,5 +1,6 @@
 package team.aliens.dms.domain.point
 
+import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import team.aliens.dms.common.dto.PageData
 import team.aliens.dms.common.dto.PageWebData
@@ -35,6 +37,7 @@ class PointWebAdapter(
         return queryPointHistoryUseCase.execute(type!!)
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/history")
     fun applyPoint(@RequestBody @Valid webRequest: ApplyPointWebRequest) {
         applyPointUseCase.execute(
