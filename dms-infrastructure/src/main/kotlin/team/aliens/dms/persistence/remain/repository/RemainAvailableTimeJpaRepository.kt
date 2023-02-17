08309ff2ee9ca080b7ac8903630e61b875e9a71e
Kangmin <ehfehf0629@gmail.com>
2023-02-17 18:12:18 +0900

    add: (#235) RemainAvailableTimeRepository

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/remain/repository/RemainAvailableTimeJpaRepository.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms.persistence.remain.repository
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import team.aliens.dms.persistence.remain.entity.RemainAvailableTimeJpaEntity
+import java.util.UUID
+
+@Repository
+interface RemainAvailableTimeJpaRepository : CrudRepository<RemainAvailableTimeJpaEntity, UUID> {
+}
