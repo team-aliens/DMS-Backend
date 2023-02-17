c60a696ad93150dacc6b076c9ebf8dce2d09e255
Kangmin <ehfehf0629@gmail.com>
2023-02-17 18:07:06 +0900

    add: (#235) RemainAvailableTime

---
+++ dms-domain/src/main/kotlin/team/aliens/dms/domain/remain/model/RemainAvailableTime.kt
@@ -0,0 +1,21 @@
+package team.aliens.dms.domain.remain.model
+
+import team.aliens.dms.common.annotation.Aggregate
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.util.UUID
+
+@Aggregate
+data class RemainAvailableTime(
+
+    val schoolId: UUID,
+
+    val startTime: LocalDate,
+
+    val startDayOfWalk: DayOfWeek,
+
+    val endTime: LocalDate,
+
+    val endDayOfWalk: DayOfWeek
+
+)
