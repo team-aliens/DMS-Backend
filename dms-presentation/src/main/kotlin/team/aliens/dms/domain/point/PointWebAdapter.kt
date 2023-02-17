22bc93e525c9d79ed480a866daf91e1345f39a6c
이하성 <tedsoftj1123@gmail.com>
2023-02-17 17:01:53 +0900

    feat: (#230) applyPoint web request mapping

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/PointWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/PointWebAdapter.kt
@@ -2,17 +2,23 @@ package team.aliens.dms.domain.point
 
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import team.aliens.dms.common.dto.PageData
 import team.aliens.dms.common.dto.PageWebData
+import team.aliens.dms.domain.point.dto.ApplyPointRequest
 import team.aliens.dms.domain.point.dto.PointRequestType
 import team.aliens.dms.domain.point.dto.QueryAllPointHistoryResponse
 import team.aliens.dms.domain.point.dto.QueryPointHistoryResponse
 import team.aliens.dms.domain.point.usecase.QueryAllPointHistoryUseCase
+import team.aliens.dms.domain.point.dto.request.ApplyPointWebRequest
+import team.aliens.dms.domain.point.usecase.ApplyPointUseCase
 import team.aliens.dms.domain.point.usecase.QueryPointHistoryUseCase
+import javax.validation.Valid
 import javax.validation.constraints.NotNull
 
 @Validated
@@ -20,12 +26,23 @@ import javax.validation.constraints.NotNull
 @RestController
 class PointWebAdapter(
     private val queryPointHistoryUseCase: QueryPointHistoryUseCase,
+    private val applyPointUseCase: ApplyPointUseCase
     private val queryAllPointHistoryUseCase: QueryAllPointHistoryUseCase,
 ) {
 
     @GetMapping
     fun getPointHistory(@RequestParam @NotNull type: PointRequestType?): QueryPointHistoryResponse {
         return queryPointHistoryUseCase.execute(type!!)
+    }
+
+    @PostMapping("/history")
+    fun applyPoint(@RequestBody @Valid webRequest: ApplyPointWebRequest) {
+        applyPointUseCase.execute(
+            ApplyPointRequest(
+                pointOptionId = webRequest.pointOptionId,
+                studentIdList = webRequest.studentIdList
+            )
+        )
     }
 
     @GetMapping("/history")
---
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/dto/request/ApplyPointWebRequest.kt
@@ -0,0 +1,11 @@
+package team.aliens.dms.domain.point.dto.request
+
+import org.jetbrains.annotations.NotNull
+import java.util.*
+
+data class ApplyPointWebRequest(
+    @field:NotNull
+    val pointOptionId: UUID,
+    @field:NotNull
+    val studentIdList: List<UUID>
+)
