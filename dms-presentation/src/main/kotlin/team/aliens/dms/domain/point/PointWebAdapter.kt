6aecb31b27e729995341999c15aab1040614b4c2
rlaisqls <rlaisqls@gmail.com>
2023-02-18 15:39:47 +0900

    feat: (#240) 전체 상벌점 내역 보기 api url

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/PointWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/PointWebAdapter.kt
@@ -2,11 +2,15 @@ package team.aliens.dms.domain.point
 
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import team.aliens.dms.common.dto.PageData
 import team.aliens.dms.domain.point.dto.PointRequestType
+import team.aliens.dms.domain.point.dto.QueryAllPointHistoryResponse
 import team.aliens.dms.domain.point.dto.QueryPointHistoryResponse
+import team.aliens.dms.domain.point.usecase.QueryAllPointHistoryUseCase
 import team.aliens.dms.domain.point.usecase.QueryPointHistoryUseCase
 import javax.validation.constraints.NotNull
 
@@ -14,11 +18,20 @@ import javax.validation.constraints.NotNull
 @RequestMapping("/points")
 @RestController
 class PointWebAdapter(
-    private val queryPointHistoryUseCase: QueryPointHistoryUseCase
+    private val queryPointHistoryUseCase: QueryPointHistoryUseCase,
+    private val queryAllPointHistoryUseCase: QueryAllPointHistoryUseCase,
 ) {
 
     @GetMapping
     fun getPointHistory(@RequestParam @NotNull type: PointRequestType?): QueryPointHistoryResponse {
         return queryPointHistoryUseCase.execute(type!!)
+    }
+
+    @GetMapping("/history")
+    fun getAllPointHistory(
+        @RequestParam @NotNull type: PointRequestType?,
+        @ModelAttribute pageData: PageData
+    ): QueryAllPointHistoryResponse {
+        return queryAllPointHistoryUseCase.execute(type!!, pageData)
     }
 }
