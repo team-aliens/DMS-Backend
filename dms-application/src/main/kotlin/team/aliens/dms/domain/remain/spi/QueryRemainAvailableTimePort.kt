9dfa66a7bde91c5228bfd0e98c34c51d490ac241
Kangmin <ehfehf0629@gmail.com>
2023-02-20 21:44:31 +0900

    add: (#234) RemainAvailableTimePort

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/remain/spi/QueryRemainAvailableTimePort.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms.domain.remain.spi
+
+import team.aliens.dms.domain.remain.model.RemainAvailableTime
+import java.util.UUID
+
+interface QueryRemainAvailableTimePort {
+
+    fun queryRemainAvailableTimeBySchoolId(schoolId: UUID): RemainAvailableTime?
+
+}
