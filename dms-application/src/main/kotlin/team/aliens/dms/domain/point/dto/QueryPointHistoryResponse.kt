1f65f508f8ab21c4f1ba45f4595715d96ebcdbf2
이하성 <tedsoftj1123@gmail.com>
2023-02-21 23:13:32 +0900

    chore: (#256) remove unused import

--- dms-application/src/main/kotlin/team/aliens/dms/domain/point/dto/QueryPointHistoryResponse.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/point/dto/QueryPointHistoryResponse.kt
@@ -1,8 +1,6 @@
 package team.aliens.dms.domain.point.dto
 
-import team.aliens.dms.domain.point.model.PointType
 import team.aliens.dms.domain.point.spi.vo.PointHistoryVO
-import java.time.LocalDate
 
 data class QueryPointHistoryResponse(
     val totalPoint: Int,
