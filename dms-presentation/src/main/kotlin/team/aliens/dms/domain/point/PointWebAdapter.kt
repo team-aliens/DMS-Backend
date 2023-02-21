de10356f288d1972e2a12ebcc74de783190b8ff7
rlaisqls <rlaisqls@gmail.com>
2023-02-21 10:57:33 +0900

    feat: (#249) 상벌점 부여 취소 api
    .

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/PointWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/PointWebAdapter.kt
@@ -1,18 +1,24 @@
 package team.aliens.dms.domain.point
 
+import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import team.aliens.dms.common.dto.PageData
 import team.aliens.dms.common.dto.PageWebData
 import team.aliens.dms.domain.point.dto.PointRequestType
 import team.aliens.dms.domain.point.dto.QueryAllPointHistoryResponse
 import team.aliens.dms.domain.point.dto.QueryPointHistoryResponse
+import team.aliens.dms.domain.point.usecase.CancelGrantedPointUseCase
 import team.aliens.dms.domain.point.usecase.QueryAllPointHistoryUseCase
 import team.aliens.dms.domain.point.usecase.QueryPointHistoryUseCase
+import java.util.UUID
 import javax.validation.constraints.NotNull
 
 @Validated
@@ -21,6 +27,7 @@ import javax.validation.constraints.NotNull
 class PointWebAdapter(
     private val queryPointHistoryUseCase: QueryPointHistoryUseCase,
     private val queryAllPointHistoryUseCase: QueryAllPointHistoryUseCase,
+    private val cancelGrantedPointUseCase: CancelGrantedPointUseCase,
 ) {
 
     @GetMapping
@@ -37,5 +44,11 @@ class PointWebAdapter(
             type = type!!,
             pageData = PageData(pageData.page, pageData.size)
         )
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/history/{point-history-id}")
+    fun cancelGrantedPoint(@PathVariable("point-history-id") pointHistoryId: UUID) {
+        cancelGrantedPointUseCase.execute(pointHistoryId)
     }
 }
