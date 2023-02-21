88b4542c108f9e361f4a0dd65ddc93a7bae6209e
이하성 <tedsoftj1123@gmail.com>
2023-02-21 18:27:30 +0900

    test: (#237) RemovePointOption 테스트

--- dms-application/src/main/kotlin/team/aliens/dms/domain/point/usecase/CreatePointOptionUseCase.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/point/usecase/CreatePointOptionUseCase.kt
@@ -2,12 +2,12 @@ package team.aliens.dms.domain.point.usecase
 
 import team.aliens.dms.common.annotation.UseCase
 import team.aliens.dms.domain.manager.exception.ManagerNotFoundException
-import team.aliens.dms.domain.manager.spi.QueryManagerPort
 import team.aliens.dms.domain.point.dto.CreatePointOptionRequest
 import team.aliens.dms.domain.point.exception.PointOptionNameExistsException
 import team.aliens.dms.domain.point.model.PointOption
 import team.aliens.dms.domain.point.model.PointType
 import team.aliens.dms.domain.point.spi.CommandPointOptionPort
+import team.aliens.dms.domain.point.spi.PointQueryManagerPort
 import team.aliens.dms.domain.point.spi.PointSecurityPort
 import team.aliens.dms.domain.point.spi.QueryPointOptionPort
 import java.util.UUID
@@ -15,7 +15,7 @@ import java.util.UUID
 @UseCase
 class CreatePointOptionUseCase(
     private val securityPort: PointSecurityPort,
-    private val queryManagerPort: QueryManagerPort,
+    private val queryManagerPort: PointQueryManagerPort,
     private val commandPointOptionPort: CommandPointOptionPort,
     private val queryPointOptionPort: QueryPointOptionPort
 ) {
--- dms-application/src/test/kotlin/team/aliens/dms/domain/point.usecase/RemovePointOptionUseCaseTest.kt
+++ dms-application/src/test/kotlin/team/aliens/dms/domain/point.usecase/RemovePointOptionUseCaseTest.kt
@@ -2,39 +2,40 @@ package team.aliens.dms.domain.point.usecase
 
 import io.mockk.every
 import io.mockk.mockk
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import team.aliens.dms.domain.manager.exception.ManagerNotFoundException
 import team.aliens.dms.domain.manager.model.Manager
-import team.aliens.dms.domain.point.dto.CreatePointOptionRequest
-import team.aliens.dms.domain.point.exception.PointOptionNameExistsException
+import team.aliens.dms.domain.point.exception.PointOptionNotFoundException
+import team.aliens.dms.domain.point.exception.PointOptionSchoolMismatchException
+import team.aliens.dms.domain.point.model.PointOption
+import team.aliens.dms.domain.point.model.PointType
 import team.aliens.dms.domain.point.spi.CommandPointOptionPort
 import team.aliens.dms.domain.point.spi.PointQueryManagerPort
 import team.aliens.dms.domain.point.spi.PointSecurityPort
 import team.aliens.dms.domain.point.spi.QueryPointOptionPort
-import java.util.UUID
+import java.util.*
 
 @ExtendWith(SpringExtension::class)
-class CreatePointOptionUseCaseTest {
-
+class RemovePointOptionUseCaseTest {
 
     private val securityPort: PointSecurityPort = mockk()
     private val queryManagerPort: PointQueryManagerPort = mockk()
     private val queryPointOptionPort: QueryPointOptionPort = mockk()
     private val commandPointOptionPort: CommandPointOptionPort = mockk(relaxed = true)
 
-    private val createPointOptionUseCase = CreatePointOptionUseCase(
-        securityPort, queryManagerPort, commandPointOptionPort, queryPointOptionPort
+    private val removePointOptionUseCase = RemovePointOptionUseCase(
+        securityPort, queryManagerPort, queryPointOptionPort, commandPointOptionPort
     )
 
     private val managerId = UUID.randomUUID()
     private val currentUserId = managerId
     private val schoolId = UUID.randomUUID()
+    private val schoolId2 = UUID.randomUUID()
+    private val pointOptionId = UUID.randomUUID()
 
     private val managerStub by lazy {
         Manager(
@@ -44,42 +45,65 @@ class CreatePointOptionUseCaseTest {
         )
     }
 
-    private val requestStub by lazy {
-        CreatePointOptionRequest(
+    private val pointOptionStub by lazy {
+        PointOption(
             name = "호실 청소 우수",
-            type = "BONUS",
-            score = 10
+            type = PointType.BONUS,
+            score = 10,
+            schoolId = schoolId
         )
     }
 
+    private val pointOptionStub2 by lazy {
+        PointOption(
+            name = "호실 청소 우수",
+            type = PointType.BONUS,
+            score = 10,
+            schoolId = schoolId2
+        )
+    }
     @Test
-    fun `상벌점 항목 생성 성공`() {
+    fun `상벌점 항목 삭제 성공`() {
         //given
         every { securityPort.getCurrentUserId() } returns currentUserId
 
         every { queryManagerPort.queryManagerById(managerId) } returns managerStub
 
-        every { queryPointOptionPort.existByNameAndSchoolId(requestStub.name, schoolId) } returns false
+        every { queryPointOptionPort.queryPointOptionById(pointOptionId) } returns pointOptionStub
 
-        //when & then
-        assertAll(
-            { assertThat( createPointOptionUseCase.execute(requestStub)).isNotNull },
-            { assertDoesNotThrow {createPointOptionUseCase.execute(requestStub)} }
-        )
+        // when & then
+        assertDoesNotThrow {
+            removePointOptionUseCase.execute(pointOptionId)
+        }
     }
 
     @Test
-    fun `항목이 이미 존재하는 경우`() {
+    fun `상벌점 항목 미존재`() {
         //given
         every { securityPort.getCurrentUserId() } returns currentUserId
 
         every { queryManagerPort.queryManagerById(managerId) } returns managerStub
 
-        every { queryPointOptionPort.existByNameAndSchoolId(requestStub.name, schoolId) } returns true
+        every { queryPointOptionPort.queryPointOptionById(pointOptionId) } returns null
 
-        //when & then
-        assertThrows<PointOptionNameExistsException> {
-            createPointOptionUseCase.execute(requestStub)
+        // when & then
+        assertThrows<PointOptionNotFoundException> {
+            removePointOptionUseCase.execute(pointOptionId)
+        }
+    }
+
+    @Test
+    fun `학교 불일치`() {
+        //given
+        every { securityPort.getCurrentUserId() } returns currentUserId
+
+        every { queryManagerPort.queryManagerById(managerId) } returns managerStub
+
+        every { queryPointOptionPort.queryPointOptionById(pointOptionId) } returns pointOptionStub2
+
+        // when & then
+        assertThrows<PointOptionSchoolMismatchException> {
+            removePointOptionUseCase.execute(pointOptionId)
         }
     }
 
@@ -92,8 +116,9 @@ class CreatePointOptionUseCaseTest {
 
 
         assertThrows<ManagerNotFoundException> {
-            createPointOptionUseCase.execute(requestStub)
+            removePointOptionUseCase.execute(pointOptionId)
         }
     }
+
 
 }
