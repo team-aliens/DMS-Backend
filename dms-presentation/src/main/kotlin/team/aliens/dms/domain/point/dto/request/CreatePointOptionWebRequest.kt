051e81e5a74aec39f76366453854fef7bad1cbd5
이하성 <tedsoftj1123@gmail.com>
2023-02-21 18:38:28 +0900

    refactor: (#237) 길이 validation

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/dto/request/CreatePointOptionWebRequest.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/dto/request/CreatePointOptionWebRequest.kt
@@ -3,9 +3,12 @@ package team.aliens.dms.domain.point.dto.request
 import team.aliens.dms.domain.point.dto.PointRequestType
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+import kotlin.math.max
 
 data class CreatePointOptionWebRequest(
     @field:NotBlank
+    @field:Size(max = 20)
     val name: String?,
 
     @field:NotNull
