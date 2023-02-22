536cd2c630602267dc1fd2478ab84941abbc73dd
Kangmin <ehfehf0629@gmail.com>
2023-02-22 22:18:29 +0900

    feat: (#267) QueryCurrentAppliedRemainOptionUseCase

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/remain/usecase/QueryCurrentAppliedRemainOptionUseCase.kt
@@ -0,0 +1,31 @@
+package team.aliens.dms.domain.remain.usecase
+
+import team.aliens.dms.common.annotation.ReadOnlyUseCase
+import team.aliens.dms.domain.remain.dto.QueryCurrentAppliedRemainOptionResponse
+import team.aliens.dms.domain.remain.exception.RemainOptionNotFoundException
+import team.aliens.dms.domain.remain.exception.RemainStatusNotFound
+import team.aliens.dms.domain.remain.spi.QueryRemainOptionPort
+import team.aliens.dms.domain.remain.spi.RemainQueryRemainStatusPort
+import team.aliens.dms.domain.remain.spi.RemainSecurityPort
+
+@ReadOnlyUseCase
+class QueryCurrentAppliedRemainOptionUseCase(
+    private val securityPort: RemainSecurityPort,
+    private val queryRemainStatusPort: RemainQueryRemainStatusPort,
+    private val queryRemainOptionPort: QueryRemainOptionPort
+) {
+
+    fun execute(): QueryCurrentAppliedRemainOptionResponse {
+        val currentUserId = securityPort.getCurrentUserId()
+
+        val remainStatus = queryRemainStatusPort.queryRemainStatusById(currentUserId)
+            ?: throw RemainStatusNotFound
+
+        val appliedRemainOption = queryRemainOptionPort.queryRemainOptionById(remainStatus.remainOptionId)
+            ?: throw RemainOptionNotFoundException
+
+        return QueryCurrentAppliedRemainOptionResponse(
+            title = appliedRemainOption.title
+        )
+    }
+}
