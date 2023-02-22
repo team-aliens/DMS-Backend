9424239b2150f2834b9267456368bcc03e7a84ba
rlaisqls <rlaisqls@gmail.com>
2023-02-22 13:33:24 +0900

    chore: (#251) content-type 삭제

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/PointWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/point/PointWebAdapter.kt
@@ -77,10 +77,6 @@ class PointWebAdapter(
             HttpHeaders.CONTENT_DISPOSITION,
             "attachment; filename=${URLEncoder.encode(response.fileName, "UTF-8")}.xlsx"
         )
-        httpResponse.setHeader(
-            HttpHeaders.CONTENT_TYPE,
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
         return response.file
     }
 }
