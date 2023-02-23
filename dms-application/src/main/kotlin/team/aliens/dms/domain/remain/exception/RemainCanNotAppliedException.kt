e00d1e3637eb1dbdfb6243fb917e813df008f918
rlaisqls <rlaisqls@gmail.com>
2023-02-23 20:09:17 +0900

    add: (#269) RemainCanNotAppliedException

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/remain/exception/RemainCanNotAppliedException.kt
@@ -0,0 +1,8 @@
+package team.aliens.dms.domain.remain.exception
+
+import team.aliens.dms.common.error.DmsException
+import team.aliens.dms.domain.remain.error.RemainAvailableTimeErrorCode
+
+object RemainCanNotAppliedException : DmsException(
+    RemainAvailableTimeErrorCode.REMAIN_AVAILABLE_TIME_NOT_FOUND
+)
