8007d2030bd0c5c5631e824e6e647fa9ed26854b
rlaisqls <rlaisqls@gmail.com>
2023-02-26 01:19:29 +0900

    feat: (#278) StudentCommandRemainStatusPort

--- dms-application/src/main/kotlin/team/aliens/dms/domain/remain/spi/RemainStatusPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/remain/spi/RemainStatusPort.kt
@@ -1,6 +1,11 @@
 package team.aliens.dms.domain.remain.spi
 
+import team.aliens.dms.domain.manager.spi.ManagerCommandRemainStatusPort
+import team.aliens.dms.domain.student.spi.StudentCommandRemainStatusPort
+
 interface RemainStatusPort :
     CommandRemainStatusPort,
-    QueryRemainStatusPort {
+    QueryRemainStatusPort,
+    StudentCommandRemainStatusPort,
+    ManagerCommandRemainStatusPort {
 }
