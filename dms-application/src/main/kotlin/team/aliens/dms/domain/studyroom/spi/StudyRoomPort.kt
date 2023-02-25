d3aef43496a95ab4bea381b9388874750a7c4e0f
rlaisqls <rlaisqls@gmail.com>
2023-02-26 01:19:48 +0900

    feat: (#278) Manager, Student Port 추가

--- dms-application/src/main/kotlin/team/aliens/dms/domain/studyroom/spi/StudyRoomPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/studyroom/spi/StudyRoomPort.kt
@@ -1,7 +1,16 @@
 package team.aliens.dms.domain.studyroom.spi
 
+import team.aliens.dms.domain.manager.spi.ManagerCommandStudyRoomPort
+import team.aliens.dms.domain.manager.spi.ManagerQueryStudyRoomPort
+import team.aliens.dms.domain.student.spi.StudentCommandStudyRoomPort
+import team.aliens.dms.domain.student.spi.StudentQueryStudyRoomPort
+
 interface StudyRoomPort:
     QueryStudyRoomPort,
     CommandStudyRoomPort,
-    SeatTypeQueryStudyRoomPort {
+    SeatTypeQueryStudyRoomPort,
+    StudentQueryStudyRoomPort,
+    StudentCommandStudyRoomPort,
+    ManagerQueryStudyRoomPort,
+    ManagerCommandStudyRoomPort {
 }
