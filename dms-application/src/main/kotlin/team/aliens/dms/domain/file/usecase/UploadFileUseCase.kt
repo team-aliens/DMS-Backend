05d6e3037ca8eee3bc5458e5c44c6c502b03fdc0
rlaisqls <rlaisqls@gmail.com>
2023-02-28 00:41:04 +0900

    feat: email template 수정

--- dms-application/src/main/kotlin/team/aliens/dms/domain/file/usecase/UploadFileUseCase.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/file/usecase/UploadFileUseCase.kt
@@ -1,7 +1,6 @@
 package team.aliens.dms.domain.file.usecase
 
 import team.aliens.dms.common.annotation.ReadOnlyUseCase
-import team.aliens.dms.domain.file.exception.FileInvalidExtensionException
 import team.aliens.dms.domain.file.spi.UploadFilePort
 import java.io.File
 
@@ -11,16 +10,13 @@ class UploadFileUseCase(
 ) {
 
     fun execute(file: File): String {
-        if (!file.isCorrectExtension(file)) {
-            file.delete()
-            throw FileInvalidExtensionException
-        }
+
 
         return uploadFilePort.upload(file)
     }
-
+/*
     internal fun File.isCorrectExtension(file: File) = when (file.extension.lowercase()) {
         "jpg", "jpeg", "png", "heic" -> true
         else -> false
-    }
+    }*/
 }
