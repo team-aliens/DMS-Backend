471ebfeb4287e9b0fe99537f8e9c51448d66c355
rlaisqls <rlaisqls@gmail.com>
2023-02-28 00:41:04 +0900

    feat: email template 수정

--- dms-domain/src/main/kotlin/team/aliens/dms/domain/auth/model/EmailType.kt
+++ dms-domain/src/main/kotlin/team/aliens/dms/domain/auth/model/EmailType.kt
@@ -7,5 +7,6 @@ enum class EmailType(
 ) {
 
     SIGNUP("signup_template", "DMS_SIGNUP_TEMPLATE", "DMS 회원가입 이메일 승인코드"),
-    PASSWORD("password_template", "DMS_PASSWORD_TEMPLATE", "DMS 비밀번호 변경 이메일 승인코드")
+    PASSWORD("password_template", "DMS_PASSWORD_TEMPLATE", "DMS 비밀번호 변경 이메일 승인코드"),
+    FIND_ACCOUNT_ID("find_account_id_template", "DMS_FIND_ACCOUNT_ID_TEMPLATE", "DMS 아이디 찾기 이메일 승인코드")
 }
