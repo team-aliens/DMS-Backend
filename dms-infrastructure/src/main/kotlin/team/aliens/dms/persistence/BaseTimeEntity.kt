bad77105a301d600a14925611bb95e6524a9bcf6
이하성 <tedsoftj1123@gmail.com>
2023-02-28 22:12:59 +0900

    setting: (#295) 방언 버전 업그레이드

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/BaseTimeEntity.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/BaseTimeEntity.kt
@@ -7,7 +7,7 @@ import javax.persistence.MappedSuperclass
 @MappedSuperclass
 abstract class BaseTimeEntity(
 
-    @Column(nullable = false, updatable = false, columnDefinition = "DATETIME")
+    @Column(nullable = false, updatable = false, columnDefinition = "DATETIME(6)")
     val createdAt: LocalDateTime = LocalDateTime.now()
 
 )
