645722739939e26b224b0e6cedee04f797583b63
이하성 <tedsoftj1123@gmail.com>
2023-03-05 20:03:49 +0900

    feat: (#327) 사용가능 기능 조회 usecase

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/school/usecase/QueryAvailableFeaturesUseCase.kt
@@ -0,0 +1,33 @@
+package team.aliens.dms.domain.school.usecase
+
+import team.aliens.dms.common.annotation.ReadOnlyUseCase
+import team.aliens.dms.domain.school.dto.QueryAvailableFeaturesResponse
+import team.aliens.dms.domain.school.exception.FeatureNotFoundException
+import team.aliens.dms.domain.school.spi.QuerySchoolPort
+import team.aliens.dms.domain.school.spi.SchoolQueryUserPort
+import team.aliens.dms.domain.school.spi.SchoolSecurityPort
+import team.aliens.dms.domain.user.exception.UserNotFoundException
+
+@ReadOnlyUseCase
+class QueryAvailableFeaturesUseCase(
+    private val securityPort: SchoolSecurityPort,
+    private val queryUserPort: SchoolQueryUserPort,
+    private val querySchoolPort: QuerySchoolPort
+) {
+
+    fun execute(): QueryAvailableFeaturesResponse {
+        val currentUserId = securityPort.getCurrentUserId()
+        val manager = queryUserPort.queryUserById(currentUserId) ?: throw UserNotFoundException
+
+        val availableFeatures = querySchoolPort.queryAvailableFeaturesBySchoolId(manager.schoolId)
+            ?: throw FeatureNotFoundException
+
+        return QueryAvailableFeaturesResponse(
+            mealService = availableFeatures.mealService,
+            noticeService = availableFeatures.noticeService,
+            pointService = availableFeatures.pointService,
+            studyRoomService = availableFeatures.studyRoomService,
+            remainService = availableFeatures.remainService
+        )
+    }
+}
