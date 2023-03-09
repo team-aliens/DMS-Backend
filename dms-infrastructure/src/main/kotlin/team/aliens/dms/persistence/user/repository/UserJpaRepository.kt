04b60a29f2d7bbf72f95245e0a9984c1b59bb9fd
rlaisqls <rlaisqls@gmail.com>
2023-03-09 18:07:20 +0900

    bug: email nativequery

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/user/repository/UserJpaRepository.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/user/repository/UserJpaRepository.kt
@@ -1,16 +1,17 @@
 package team.aliens.dms.persistence.user.repository
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import team.aliens.dms.domain.auth.model.Authority
 import team.aliens.dms.persistence.user.entity.UserJpaEntity
 import java.util.UUID
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 
 @Repository
 interface UserJpaRepository : CrudRepository<UserJpaEntity, UUID> {
 
+    @Query("select * from tbl_user as u where u.email=:email", nativeQuery = true)
     fun existsByEmail(email: String): Boolean
 
     @Query("select * from tbl_user as u where u.account_id=:accountId", nativeQuery = true)
