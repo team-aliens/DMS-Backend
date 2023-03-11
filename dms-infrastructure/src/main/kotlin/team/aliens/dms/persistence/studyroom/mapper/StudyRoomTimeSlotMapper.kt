1260388ecc1968ad6137107a70f246f69c62f0c8
rlaisqls <rlaisqls@gmail.com>
2023-03-11 12:34:09 +0900

    create: (#334) StudyRoomTimeSlot domain & entity

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/studyroom/mapper/StudyRoomTimeSlotMapper.kt
@@ -0,0 +1,33 @@
+package team.aliens.dms.persistence.studyroom.mapper
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import team.aliens.dms.domain.studyroom.model.StudyRoomTimeSlot
+import team.aliens.dms.persistence.GenericMapper
+import team.aliens.dms.persistence.school.repository.SchoolJpaRepository
+import team.aliens.dms.persistence.studyroom.entity.StudyRoomTimeSlotJpaEntity
+
+@Component
+class StudyRoomTimeSlotMapper(
+    private val schoolRepository: SchoolJpaRepository
+) : GenericMapper<StudyRoomTimeSlot, StudyRoomTimeSlotJpaEntity> {
+
+    override fun toDomain(entity: StudyRoomTimeSlotJpaEntity?): StudyRoomTimeSlot? {
+        return entity?.let {
+            StudyRoomTimeSlot(
+                id = it.id!!,
+                schoolId = entity.school!!.id!!,
+                name = entity.name
+            )
+        }
+    }
+
+    override fun toEntity(domain: StudyRoomTimeSlot): StudyRoomTimeSlotJpaEntity {
+        val school = schoolRepository.findByIdOrNull(domain.schoolId)
+        return StudyRoomTimeSlotJpaEntity(
+            id = domain.id,
+            school = school,
+            name = domain.name
+        )
+    }
+}
