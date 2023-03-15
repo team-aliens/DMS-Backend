db6b36f9d2b39c76aa968042cba1ebc9a10f396b
Kangmin <ehfehf0629@gmail.com>
2023-03-15 23:28:06 +0900

    feat: (#345) TagJpaRepository

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/student/repository/TagJpaRepository.kt
@@ -0,0 +1,8 @@
+package team.aliens.dms.persistence.student.repository
+
+import org.springframework.data.repository.CrudRepository
+import team.aliens.dms.persistence.student.entity.TagJpaEntity
+import java.util.UUID
+
+interface TagJpaRepository : CrudRepository<TagJpaEntity, UUID> {
+}
