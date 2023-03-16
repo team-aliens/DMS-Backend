67d99f0cc45c07e4400c13430efe5de1442c747b
Kangmin <ehfehf0629@gmail.com>
2023-03-16 16:07:35 +0900

    chore: (#345) 비식별 관계로 변경

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/student/entity/StudentTagJpaEntity.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/student/entity/StudentTagJpaEntity.kt
@@ -1,42 +1,25 @@
 package team.aliens.dms.persistence.student.entity
 
-import java.io.Serializable
+import team.aliens.dms.persistence.BaseEntity
 import java.util.UUID
-import javax.persistence.Column
-import javax.persistence.Embeddable
-import javax.persistence.EmbeddedId
 import javax.persistence.Entity
 import javax.persistence.FetchType
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
-import javax.persistence.MapsId
 import javax.persistence.Table
 
 @Entity
 @Table(name = "tbl_student_tag")
 class StudentTagJpaEntity(
 
-    @EmbeddedId
-    val id: Id,
+    override val id: UUID?,
 
-    @MapsId("studentId")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id", columnDefinition = "BINARY(16)", nullable = false)
     val student: StudentJpaEntity?,
 
-    @MapsId("tagId")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tag_id", columnDefinition = "BINARY(16)", nullable = false)
     val tag: TagJpaEntity?
 
-) {
-
-    @Embeddable
-    data class Id(
-        @Column(columnDefinition = "BINARY(16)", nullable = false)
-        val studentId: UUID,
-
-        @Column(columnDefinition = "BINARY(16)", nullable = false)
-        val tagId: UUID
-    ) : Serializable
-}
+) : BaseEntity(id)
--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/student/mapper/StudentTagMapper.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/student/mapper/StudentTagMapper.kt
@@ -5,20 +5,20 @@ import team.aliens.dms.domain.student.model.StudentTag
 import team.aliens.dms.persistence.GenericMapper
 import team.aliens.dms.persistence.student.entity.StudentTagJpaEntity
 import team.aliens.dms.persistence.student.repository.StudentJpaRepository
-import team.aliens.dms.persistence.student.repository.StudentTagJpaRepository
 import team.aliens.dms.persistence.student.repository.TagJpaRepository
 
 class StudentTagMapper(
     private val studentRepository: StudentJpaRepository,
-    private val tagRepository: TagJpaRepository,
-    private val studentTagRepository: StudentTagJpaRepository
+    private val tagRepository: TagJpaRepository
 ) : GenericMapper<StudentTag, StudentTagJpaEntity> {
 
     override fun toDomain(entity: StudentTagJpaEntity?): StudentTag? {
         return entity?.let {
             StudentTag(
-                studentId = entity.student!!.id,
-                tagId = entity.tag!!.id!!
+                id = it.id!!,
+                studentId = it.student!!.id,
+                tagId = it.tag!!.id!!,
+                createdAt = it.createdAt
             )
         }
     }
@@ -26,13 +26,9 @@ class StudentTagMapper(
     override fun toEntity(domain: StudentTag): StudentTagJpaEntity {
         val student = studentRepository.findByIdOrNull(domain.studentId)
         val tag = tagRepository.findByIdOrNull(domain.tagId)
-        val id = StudentTagJpaEntity.Id(
-            studentId = student!!.id,
-            tagId = tag!!.id!!
-        )
 
         return StudentTagJpaEntity(
-            id = id,
+            id = domain.id,
             student = student,
             tag = tag
         )
