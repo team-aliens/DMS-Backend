bd6c553da97c2d1ca95a4bb832282d9ae093e7de
Kangmin <ehfehf0629@gmail.com>
2023-03-16 16:09:56 +0900

    chore: (#345) 비식별 관계로 변경

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/student/repository/StudentTagJpaRepository.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/student/repository/StudentTagJpaRepository.kt
@@ -2,5 +2,6 @@ package team.aliens.dms.persistence.student.repository
 
 import org.springframework.data.repository.CrudRepository
 import team.aliens.dms.persistence.student.entity.StudentTagJpaEntity
+import java.util.UUID
 
-interface StudentTagJpaRepository : CrudRepository<StudentTagJpaEntity, StudentTagJpaEntity.Id>
+interface StudentTagJpaRepository : CrudRepository<StudentTagJpaEntity, UUID>
