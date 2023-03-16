b2b5fb4bb508bec8f14fa48c49333cb0c6411cd2
Kangmin <ehfehf0629@gmail.com>
2023-03-16 16:07:03 +0900

    refactor: (#345) 코멘트 반영

--- dms-domain/src/main/kotlin/team/aliens/dms/domain/student/model/StudentTag.kt
+++ dms-domain/src/main/kotlin/team/aliens/dms/domain/student/model/StudentTag.kt
@@ -1,11 +1,18 @@
 package team.aliens.dms.domain.student.model
 
+import team.aliens.dms.common.annotation.Aggregate
+import java.time.LocalDateTime
 import java.util.UUID
 
+@Aggregate
 data class StudentTag(
+
+    val id: UUID,
 
     val studentId: UUID,
 
-    val tagId: UUID
+    val tagId: UUID,
+
+    val createdAt: LocalDateTime
 
 )
