b6bafde13a500ad025604d271a43404b8c7ceff5
Kangmin <ehfehf0629@gmail.com>
2023-03-18 22:48:33 +0900

    refactor: (#342) createPointStub

---
+++ dms-application/src/test/kotlin/team/aliens/dms/domain/point/stub/PointOptionStub.kt
@@ -0,0 +1,19 @@
+package team.aliens.dms.domain.point.stub
+
+import team.aliens.dms.domain.point.model.PointOption
+import team.aliens.dms.domain.point.model.PointType
+import java.util.UUID
+
+internal fun createPointOption(
+    id: UUID = UUID.randomUUID(),
+    schoolId: UUID = UUID.randomUUID(),
+    name: String = "기숙사 봉사 활동",
+    score: Int = 5,
+    type: PointType = PointType.BONUS
+) = PointOption(
+    id = id,
+    schoolId = schoolId,
+    name = name,
+    score = score,
+    type = type
+)
