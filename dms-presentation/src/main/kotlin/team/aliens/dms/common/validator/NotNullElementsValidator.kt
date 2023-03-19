1a24ce09e380ad2b4c8cc5580b59740df32ba2ad
rlaisqls <rlaisqls@gmail.com>
2023-03-19 17:03:50 +0900

    add: (#351) NotNullElementsValidator

---
+++ dms-presentation/src/main/kotlin/team/aliens/dms/common/validator/NotNullElementsValidator.kt
@@ -0,0 +1,26 @@
+package team.aliens.dms.common.validator
+
+import javax.validation.Constraint
+import javax.validation.ConstraintValidator
+import javax.validation.ConstraintValidatorContext
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [NotNullElementsValidator::class])
+annotation class NotNullElements(
+    val message: String = "List cannot contain null fields",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Any>> = []
+)
+
+class NotNullElementsValidator : ConstraintValidator<NotNullElements?, List<Any?>> {
+
+    override fun initialize(notNullElements: NotNullElements?) {}
+
+    override fun isValid(
+        objects: List<Any?>,
+        context: ConstraintValidatorContext,
+    ) =  objects.stream().allMatch { it != null }
+
+}
