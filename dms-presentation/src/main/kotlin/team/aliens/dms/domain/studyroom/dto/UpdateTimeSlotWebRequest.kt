07145669a34613cb448f53cf5a7c107ebcc75126
rlaisqls <rlaisqls@gmail.com>
2023-03-21 07:29:19 +0900

    chore: (#355) data class

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/studyroom/dto/UpdateTimeSlotWebRequest.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/studyroom/dto/UpdateTimeSlotWebRequest.kt
@@ -3,7 +3,7 @@ package team.aliens.dms.domain.studyroom.dto
 import java.time.LocalTime
 import javax.validation.constraints.NotNull
 
-class UpdateTimeSlotWebRequest(
+data class UpdateTimeSlotWebRequest(
 
     @field:NotNull
     val startTime: LocalTime?,
