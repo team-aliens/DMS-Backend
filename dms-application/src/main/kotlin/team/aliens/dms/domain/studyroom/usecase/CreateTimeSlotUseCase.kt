e3c59cca769dc688ccf71ae2074043fee33a653c
rlaisqls <rlaisqls@gmail.com>
2023-03-20 10:48:20 +0900

    add: (#355) CreateTimeSlotUseCase

---
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/studyroom/usecase/CreateTimeSlotUseCase.kt
@@ -0,0 +1,32 @@
+package team.aliens.dms.domain.studyroom.usecase
+
+import team.aliens.dms.common.annotation.UseCase
+import team.aliens.dms.domain.studyroom.model.TimeSlot
+import team.aliens.dms.domain.studyroom.spi.CommandStudyRoomPort
+import team.aliens.dms.domain.studyroom.spi.StudyRoomQueryUserPort
+import team.aliens.dms.domain.studyroom.spi.StudyRoomSecurityPort
+import team.aliens.dms.domain.user.exception.UserNotFoundException
+import java.time.LocalTime
+import java.util.UUID
+
+@UseCase
+class CreateTimeSlotUseCase(
+    private val securityPort: StudyRoomSecurityPort,
+    private val queryUserPort: StudyRoomQueryUserPort,
+    private val commandStudyRoomPort: CommandStudyRoomPort
+) {
+    fun execute(startTime: LocalTime, endTime: LocalTime): UUID {
+        val currentManagerId = securityPort.getCurrentUserId()
+        val currentManager = queryUserPort.queryUserById(currentManagerId) ?: throw UserNotFoundException
+
+        val savedTimeSlot = commandStudyRoomPort.saveTimeSlot(
+            TimeSlot(
+                schoolId = currentManager.schoolId,
+                startTime = startTime,
+                endTime = endTime
+            )
+        )
+
+        return savedTimeSlot.id
+    }
+}
