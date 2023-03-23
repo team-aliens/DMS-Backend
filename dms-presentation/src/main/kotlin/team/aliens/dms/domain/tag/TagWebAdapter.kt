a4f5b0d530555774848192ba5f9838dd3ec09d1f
이하성 <tedsoftj1123@gmail.com>
2023-03-23 17:28:48 +0900

    feat: (#372) createTag url매핑

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/tag/TagWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/tag/TagWebAdapter.kt
@@ -2,20 +2,37 @@ package team.aliens.dms.domain.tag
 
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import team.aliens.dms.domain.tag.dto.CreateTagWebRequest
+import team.aliens.dms.domain.tag.dto.CreateTagWebResponse
 import team.aliens.dms.domain.tag.dto.QueryTagsResponse
+import team.aliens.dms.domain.tag.usecase.CreateTagUseCase
 import team.aliens.dms.domain.tag.usecase.QueryTagsUseCase
+import javax.validation.Valid
 
 @Validated
 @RequestMapping("/tags")
 @RestController
 class TagWebAdapter(
-    private val queryTagsUseCase: QueryTagsUseCase
+    private val queryTagsUseCase: QueryTagsUseCase,
+    private val createTagUseCase: CreateTagUseCase
 ) {
 
     @GetMapping
     fun queryTags(): QueryTagsResponse {
         return queryTagsUseCase.execute()
+    }
+
+    @PostMapping
+    fun createTag(@RequestBody @Valid webRequest: CreateTagWebRequest): CreateTagWebResponse {
+        val tagId = createTagUseCase.execute(
+            name = webRequest.name,
+            color = webRequest.color
+        )
+
+        return CreateTagWebResponse(tagId)
     }
 }
---
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/tag/dto/CreateTagWebRequest.kt
@@ -0,0 +1,14 @@
+package team.aliens.dms.domain.tag.dto
+
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+data class CreateTagWebRequest(
+    @field:NotBlank
+    @field:Size(max = 10)
+    val name: String,
+
+    @field:NotBlank
+    @field:Size(min = 7, max = 7)
+    val color: String
+)
