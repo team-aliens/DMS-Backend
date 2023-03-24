41fa8634c8129c676decc2e8c556de95f3230035
Kangmin <ehfehf0629@gmail.com>
2023-03-24 18:20:35 +0900

    test: (#367) GrantTagUseCaseTests

--- dms-application/src/test/kotlin/team/aliens/dms/domain/tag/stub/TagStub.kt
+++ dms-application/src/test/kotlin/team/aliens/dms/domain/tag/stub/TagStub.kt
@@ -1,12 +1,12 @@
 package team.aliens.dms.domain.tag.stub
 
-import java.util.UUID
 import team.aliens.dms.domain.tag.model.Tag
+import java.util.UUID
 
 internal fun createTagStub(
     id: UUID = UUID.randomUUID(),
-    name: String,
-    color: String = "FFFFF",
+    name: String = "소프트웨어개발과",
+    color: String = "#FF4646",
     schoolId: UUID = UUID.randomUUID()
 ) = Tag(
     id = id,
