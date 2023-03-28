5f94050d1a142a51b871c180d468f819e1956e6a
이하성 <tedsoftj1123@gmail.com>
2023-03-28 09:02:37 +0900

    chore: (#392) lint

--- dms-application/src/main/kotlin/team/aliens/dms/domain/tag/spi/StudentTagPort.kt
+++ dms-application/src/main/kotlin/team/aliens/dms/domain/tag/spi/StudentTagPort.kt
@@ -1,4 +1,4 @@
 package team.aliens.dms.domain.tag.spi
 
 interface StudentTagPort :
-        CommandStudentTagPort
+    CommandStudentTagPort
--- dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/tag/repository/StudentTagJpaRepository.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/persistence/tag/repository/StudentTagJpaRepository.kt
@@ -1,9 +1,9 @@
 package team.aliens.dms.persistence.tag.repository
 
-import java.util.UUID
 import org.springframework.data.repository.CrudRepository
 import team.aliens.dms.persistence.tag.entity.StudentTagId
 import team.aliens.dms.persistence.tag.entity.StudentTagJpaEntity
+import java.util.UUID
 
 interface StudentTagJpaRepository : CrudRepository<StudentTagJpaEntity, StudentTagId> {
     fun deleteByTagId(tagId: UUID)
