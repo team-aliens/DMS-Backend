73ed4430781311890df0874ddcaf85cd856ca743
이하성 <tedsoftj1123@gmail.com>
2023-03-28 22:53:15 +0900

    add: (#399) localdate format

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/config/JacksonConfiguration.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/config/JacksonConfiguration.kt
@@ -1,6 +1,7 @@
 package team.aliens.dms.global.config
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer
 import org.springframework.context.annotation.Bean
@@ -15,6 +16,7 @@ class JacksonConfiguration {
         return Jackson2ObjectMapperBuilder()
             .serializers(LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")))
             .serializers(LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm")))
+            .serializers(LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
             .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
     }
 }
