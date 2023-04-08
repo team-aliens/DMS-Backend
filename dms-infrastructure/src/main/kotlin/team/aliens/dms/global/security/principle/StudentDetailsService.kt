94e94a092c0c4734c5c526c4dd2bc60822b45408
rlaisqls <rlaisqls@gmail.com>
2023-04-08 11:14:14 +0900

    chore: (#422) port 변경

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/StudentDetailsService.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/StudentDetailsService.kt
@@ -1,18 +1,19 @@
 package team.aliens.dms.global.security.principle
 
+import java.util.UUID
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
-import team.aliens.dms.domain.studyroom.spi.StudyRoomQueryStudentPort
+import team.aliens.dms.domain.student.spi.QueryStudentPort
 import team.aliens.dms.global.security.exception.InvalidTokenException
-import java.util.UUID
 
 @Component
 class StudentDetailsService(
-    private val queryStudentPort: StudyRoomQueryStudentPort
+    private val queryStudentPort: QueryStudentPort
 ) : UserDetailsService {
 
     override fun loadUserByUsername(username: String?): UserDetails {
+
         val student = queryStudentPort.queryStudentById(UUID.fromString(username)) ?: throw InvalidTokenException
 
         return StudentDetails(student.id)
