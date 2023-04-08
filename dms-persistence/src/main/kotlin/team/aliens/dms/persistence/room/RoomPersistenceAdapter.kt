e9bb4cc4a769ce7babbadee6cd317254554b78e9
rlaisqls <rlaisqls@gmail.com>
2023-04-08 23:59:56 +0900

    chore: (#424) 사용하지 않는 메서드 삭제

--- dms-persistence/src/main/kotlin/team/aliens/dms/persistence/room/RoomPersistenceAdapter.kt
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/room/RoomPersistenceAdapter.kt
@@ -1,24 +1,17 @@
 package team.aliens.dms.persistence.room
 
+import java.util.UUID
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.room.model.Room
 import team.aliens.dms.domain.room.spi.RoomPort
 import team.aliens.dms.persistence.room.mapper.RoomMapper
 import team.aliens.dms.persistence.room.repository.RoomJpaRepository
-import java.util.UUID
 
 @Component
 class RoomPersistenceAdapter(
     private val roomMapper: RoomMapper,
     private val roomRepository: RoomJpaRepository
 ) : RoomPort {
-
-    override fun queryRoomBySchoolIdAndNumber(schoolId: UUID, number: String) = roomMapper.toDomain(
-        roomRepository.findBySchoolIdAndNumber(
-            schoolId = schoolId,
-            number = number
-        )
-    )
 
     override fun queryRoomsByRoomNumbersIn(roomNumbers: List<String>, schoolId: UUID): List<Room> {
         return roomRepository.findAllByNumberIsInAndSchoolId(
