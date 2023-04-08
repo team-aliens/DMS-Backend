7b0bbaedbdf8374fa5d518994423a700b913ea7b
rlaisqls <rlaisqls@gmail.com>
2023-04-09 00:42:04 +0900

    feat: (#424) VerifiedStudent Student로 대체

--- dms-core/src/main/kotlin/team/aliens/dms/domain/student/usecase/CheckStudentGcnUseCase.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/student/usecase/CheckStudentGcnUseCase.kt
@@ -3,25 +3,31 @@ package team.aliens.dms.domain.student.usecase
 import team.aliens.dms.common.annotation.ReadOnlyUseCase
 import team.aliens.dms.domain.school.exception.SchoolNotFoundException
 import team.aliens.dms.domain.student.dto.CheckStudentGcnRequest
-import team.aliens.dms.domain.student.exception.VerifiedStudentNotFoundException
-import team.aliens.dms.domain.student.model.Student
+import team.aliens.dms.domain.student.exception.StudentAlreadyExistsException
+import team.aliens.dms.domain.student.exception.StudentNotFoundException
+import team.aliens.dms.domain.student.spi.QueryStudentPort
 import team.aliens.dms.domain.student.spi.StudentQuerySchoolPort
-import team.aliens.dms.domain.student.spi.StudentQueryVerifiedStudentPort
 
 @ReadOnlyUseCase
 class CheckStudentGcnUseCase(
     private val querySchoolPort: StudentQuerySchoolPort,
-    private val queryVerifiedStudentPort: StudentQueryVerifiedStudentPort
+    private val queryStudentPort: QueryStudentPort
 ) {
 
     fun execute(request: CheckStudentGcnRequest): String {
         val school = querySchoolPort.querySchoolById(request.schoolId) ?: throw SchoolNotFoundException
 
-        val verifiedStudent = queryVerifiedStudentPort.queryVerifiedStudentByGcnAndSchoolName(
-            gcn = Student.processGcn(request.grade, request.classRoom, request.number),
-            schoolName = school.name
-        ) ?: throw VerifiedStudentNotFoundException
+        val student = queryStudentPort.queryStudentBySchoolIdAndGcn(
+            schoolId = school.id,
+            grade = request.grade,
+            classRoom = request.classRoom,
+            number = request.number
+        ) ?: throw StudentNotFoundException
 
-        return verifiedStudent.name
+        if (student.userId != null) {
+            throw StudentAlreadyExistsException
+        }
+
+        return student.name
     }
 }
