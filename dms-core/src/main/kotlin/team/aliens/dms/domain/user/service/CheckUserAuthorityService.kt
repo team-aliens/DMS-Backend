ee5ec47bdb2516059f56c94a1c73676de05a227d
이하성 <tedsoftj1123@gmail.com>
2023-04-13 16:33:59 +0900

    refactor: (#441) 포트 분리

--- dms-core/src/main/kotlin/team/aliens/dms/domain/user/service/CheckUserAuthorityService.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/user/service/CheckUserAuthorityService.kt
@@ -3,12 +3,12 @@ package team.aliens.dms.domain.user.service
 import team.aliens.dms.common.annotation.DomainService
 import team.aliens.dms.domain.auth.model.Authority
 import team.aliens.dms.domain.student.model.Student
-import team.aliens.dms.domain.user.spi.UserQueryStudentPort
 import java.util.UUID
+import team.aliens.dms.domain.student.spi.QueryStudentPort
 
 @DomainService
 class CheckUserAuthorityService(
-    private val queryStudentPort: UserQueryStudentPort
+    private val queryStudentPort: QueryStudentPort
 ) : CheckUserAuthority {
 
     override fun execute(userId: UUID) = when (queryStudentPort.queryStudentByUserId(userId)) {
