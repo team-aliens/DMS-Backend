940db265542ec559f933b7c9a4260c4e0e360e7b
rlaisqls <rlaisqls@gmail.com>
2023-04-16 12:37:40 +0900

    chore: (#444) 트랜잭션 삭제

--- dms-core/src/main/kotlin/team/aliens/dms/common/annotation/Service.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/common/annotation/Service.kt
@@ -1,8 +1,5 @@
 package team.aliens.dms.common.annotation
 
-import org.springframework.transaction.annotation.Transactional
-
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
-@Transactional
 annotation class Service
