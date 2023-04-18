376845c3beac240aed28dcf34cc64720dfb71621
rlaisqls <rlaisqls@gmail.com>
2023-04-18 13:34:44 +0900

    chore: (#450) 탈퇴시 유저만 삭제되도록 수정

--- dms-core/src/main/kotlin/team/aliens/dms/domain/student/usecase/StudentWithdrawalUseCase.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/student/usecase/StudentWithdrawalUseCase.kt
@@ -2,18 +2,12 @@ package team.aliens.dms.domain.student.usecase
 
 import java.time.LocalDateTime
 import team.aliens.dms.common.annotation.UseCase
-import team.aliens.dms.domain.remain.spi.CommandRemainStatusPort
-import team.aliens.dms.domain.student.spi.CommandStudentPort
-import team.aliens.dms.domain.studyroom.spi.CommandStudyRoomPort
 import team.aliens.dms.domain.user.exception.UserNotFoundException
 import team.aliens.dms.domain.user.service.UserService
 
 @UseCase
 class StudentWithdrawalUseCase(
-    private val userService: UserService,
-    private val commandRemainStatusPort: CommandRemainStatusPort,
-    private val commandStudyRoomPort: CommandStudyRoomPort,
-    private val commandStudentPort: CommandStudentPort
+    private val userService: UserService
 ) {
 
     fun execute() {
@@ -21,12 +15,6 @@ class StudentWithdrawalUseCase(
         val student = userService.getCurrentStudent()
         student.userId ?: throw UserNotFoundException
 
-        commandRemainStatusPort.deleteByStudentId(student.id)
-        commandStudyRoomPort.deleteSeatApplicationByStudentId(student.id)
-
-        commandStudentPort.saveStudent(
-            student.copy(deletedAt = LocalDateTime.now())
-        )
         userService.deleteUserById(student.userId)
     }
 }
