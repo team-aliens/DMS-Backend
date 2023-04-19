27d3a76d6e822254b43449185529720426fbf0b6
이하성 <tedsoftj1123@gmail.com>
2023-04-19 10:17:09 +0900

    add: (#449) feature조회 service

--- dms-core/src/main/kotlin/team/aliens/dms/domain/school/service/GetSchoolService.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/school/service/GetSchoolService.kt
@@ -1,7 +1,11 @@
 package team.aliens.dms.domain.school.service
 
+import team.aliens.dms.domain.school.model.AvailableFeature
 import team.aliens.dms.domain.school.model.School
+import java.util.UUID
 
 interface GetSchoolService {
     fun queryAllSchools(): List<School>
+
+    fun getAvailableFeaturesBySchoolId(schoolId: UUID): AvailableFeature
 }
