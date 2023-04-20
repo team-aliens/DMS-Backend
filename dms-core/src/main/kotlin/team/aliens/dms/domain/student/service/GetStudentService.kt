8491ffa6b49e5f67c5f9cba9f453256521be7cb3
rlaisqls <rlaisqls@gmail.com>
2023-04-20 15:00:29 +0900

    create: (#460) StudentService

---
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/student/service/GetStudentService.kt
@@ -0,0 +1,35 @@
+package team.aliens.dms.domain.student.service
+
+import java.util.UUID
+import team.aliens.dms.domain.manager.dto.PointFilter
+import team.aliens.dms.domain.manager.dto.Sort
+import team.aliens.dms.domain.manager.spi.vo.StudentWithTag
+import team.aliens.dms.domain.point.spi.vo.StudentWithPointVO
+import team.aliens.dms.domain.student.model.Student
+
+interface GetStudentService {
+
+    fun getCurrentStudent(): Student
+
+    fun queryStudentBySchoolIdAndGcn(schoolId: UUID, grade: Int, classRoom: Int, number: Int): Student
+
+    fun queryStudentById(studentId: UUID): Student
+
+    fun queryStudentByUserId(userId: UUID): Student
+
+    fun queryStudentsByNameAndSortAndFilter(
+        name: String?,
+        sort: Sort,
+        schoolId: UUID,
+        pointFilter: PointFilter,
+        tagIds: List<UUID>?
+    ): List<StudentWithTag>
+
+    fun queryStudentsByRoomNumberAndSchoolId(roomNumber: String, schoolId: UUID): List<Student>
+
+    fun queryStudentsWithPointHistory(studentIds: List<UUID>): List<StudentWithPointVO>
+
+    fun queryStudentsBySchoolId(schoolId: UUID): List<Student>
+
+    fun queryAllStudentsByIdsIn(studentIds: List<UUID>): List<Student>
+}
