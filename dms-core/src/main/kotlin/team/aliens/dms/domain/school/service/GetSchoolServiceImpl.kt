871742172c72beb7414aa88830934d227b818524
Kangmin <ehfehf0629@gmail.com>
2023-04-22 17:43:02 +0900

    feat: (#468) GetSchoolService

--- dms-core/src/main/kotlin/team/aliens/dms/domain/school/service/GetSchoolServiceImpl.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/school/service/GetSchoolServiceImpl.kt
@@ -2,23 +2,25 @@ package team.aliens.dms.domain.school.service
 
 import team.aliens.dms.common.annotation.Service
 import team.aliens.dms.domain.school.exception.FeatureNotFoundException
-import team.aliens.dms.domain.school.model.AvailableFeature
-import team.aliens.dms.domain.school.model.School
+import team.aliens.dms.domain.school.exception.SchoolCodeMismatchException
+import team.aliens.dms.domain.school.exception.SchoolNotFoundException
 import team.aliens.dms.domain.school.spi.QuerySchoolPort
 import java.util.UUID
 
 @Service
 class GetSchoolServiceImpl(
-    private val querySchoolPort: QuerySchoolPort,
-    private val queryAvailFeaPort: QuerySchoolPort
+    private val querySchoolPort: QuerySchoolPort
 ) : GetSchoolService {
 
-    override fun queryAllSchools(): List<School> {
-        return querySchoolPort.queryAllSchools()
-    }
+    override fun getAllSchools() =
+        querySchoolPort.queryAllSchools()
 
-    override fun getAvailableFeaturesBySchoolId(schoolId: UUID): AvailableFeature {
-        return querySchoolPort.queryAvailableFeaturesBySchoolId(schoolId)
-            ?: throw FeatureNotFoundException
-    }
+    override fun getSchoolById(schoolId: UUID) =
+        querySchoolPort.querySchoolById(schoolId) ?: throw SchoolNotFoundException
+
+    override fun getSchoolByCode(code: String) =
+        querySchoolPort.querySchoolByCode(code) ?: throw SchoolCodeMismatchException
+
+    override fun getAvailableFeaturesBySchoolId(schoolId: UUID) =
+        querySchoolPort.queryAvailableFeaturesBySchoolId(schoolId) ?: throw FeatureNotFoundException
 }
