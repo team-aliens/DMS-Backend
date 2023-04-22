97e10474af65ae90f407184004e080583b690f62
rlaisqls <rlaisqls@gmail.com>
2023-04-22 14:32:12 +0900

    update: (#466) roomPort

--- dms-core/src/main/kotlin/team/aliens/dms/domain/room/spi/CommandRoomPort.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/room/spi/CommandRoomPort.kt
@@ -2,7 +2,7 @@ package team.aliens.dms.domain.room.spi
 
 import team.aliens.dms.domain.room.model.Room
 
-interface FileCommandRoomPort {
+interface CommandRoomPort {
 
     fun saveRooms(rooms: List<Room>): List<Room>
 }
