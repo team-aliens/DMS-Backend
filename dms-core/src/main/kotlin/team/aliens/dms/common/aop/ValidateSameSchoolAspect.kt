04a785a27c5cb9770572398b8796b480f23afa5e
rlaisqls <rlaisqls@gmail.com>
2023-04-28 09:07:33 +0900

    create: (#477) ValidateSameSchoolAspect

---
+++ dms-core/src/main/kotlin/team/aliens/dms/common/aop/ValidateSameSchoolAspect.kt
@@ -0,0 +1,25 @@
+package team.aliens.dms.common.aop
+
+import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.annotation.AfterReturning
+import org.aspectj.lang.annotation.Aspect
+import team.aliens.dms.common.annotation.UseCase
+import team.aliens.dms.common.model.SchoolIdDomain
+import team.aliens.dms.common.spi.SecurityPort
+import team.aliens.dms.domain.school.exception.SchoolMismatchException
+
+@UseCase
+@Aspect
+class ValidateSameSchoolAspect(
+    private val securityPort: SecurityPort,
+) {
+    @AfterReturning(
+        pointcut = "within(*..*Service) && execution (team.aliens.dms.common.model.SchoolIdDomain+ get*(..))",
+        returning = "ret"
+    )
+    fun validateSameSchool(joinPoint: JoinPoint, ret: SchoolIdDomain) {
+        if (ret.schoolId != securityPort.getCurrentUserSchoolId()) {
+            throw SchoolMismatchException
+        }
+    }
+}
