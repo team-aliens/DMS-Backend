2b0ed33a0ae796bcea363acaa3fe2a0d5c1c907c
rlaisqls <rlaisqls@gmail.com>
2023-05-01 10:39:30 +0900

    chore: (#477) 중괄호

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/ManagerDetailsService.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/ManagerDetailsService.kt
@@ -1,12 +1,12 @@
 package team.aliens.dms.global.security.principle
 
+import java.util.UUID
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.auth.model.Authority
 import team.aliens.dms.domain.user.spi.QueryUserPort
 import team.aliens.dms.global.security.exception.InvalidTokenException
-import java.util.UUID
 
 @Component
 class ManagerDetailsService(
@@ -15,7 +15,9 @@ class ManagerDetailsService(
 
     override fun loadUserByUsername(username: String?): UserDetails {
         queryUserPort.queryUserById(UUID.fromString(username)).apply {
-            if (this == null || authority != Authority.MANAGER) throw InvalidTokenException
+            if (this == null || authority != Authority.MANAGER) {
+                throw InvalidTokenException
+            }
             return ManagerDetails(
                 userId = id,
                 schoolId = schoolId
--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/StudentDetailsService.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/principle/StudentDetailsService.kt
@@ -1,12 +1,12 @@
 package team.aliens.dms.global.security.principle
 
+import java.util.UUID
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.auth.model.Authority
 import team.aliens.dms.domain.user.spi.QueryUserPort
 import team.aliens.dms.global.security.exception.InvalidTokenException
-import java.util.UUID
 
 @Component
 class StudentDetailsService(
@@ -15,7 +15,9 @@ class StudentDetailsService(
 
     override fun loadUserByUsername(username: String?): UserDetails {
         queryUserPort.queryUserById(UUID.fromString(username)).apply {
-            if (this == null || authority != Authority.STUDENT) throw InvalidTokenException
+            if (this == null || authority != Authority.STUDENT) {
+                throw InvalidTokenException
+            }
             return StudentDetails(
                 userId = id,
                 schoolId = schoolId
