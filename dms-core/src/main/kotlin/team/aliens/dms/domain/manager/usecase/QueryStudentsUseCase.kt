e46e1c70c56caa0ab9adc1ffcf965cff685c4f00
Kangmin <ehfehf0629@gmail.com>
2023-05-07 22:14:43 +0900

    refactor: (#488) StudentResponse 병합

--- dms-core/src/main/kotlin/team/aliens/dms/domain/manager/usecase/QueryStudentsUseCase.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/manager/usecase/QueryStudentsUseCase.kt
@@ -3,8 +3,8 @@ package team.aliens.dms.domain.manager.usecase
 import team.aliens.dms.common.annotation.ReadOnlyUseCase
 import team.aliens.dms.domain.manager.dto.PointFilter
 import team.aliens.dms.domain.manager.dto.PointFilterType
-import team.aliens.dms.domain.manager.dto.QueryStudentsResponse
 import team.aliens.dms.domain.manager.dto.Sort
+import team.aliens.dms.domain.student.dto.StudentsResponse
 import team.aliens.dms.domain.student.service.StudentService
 import team.aliens.dms.domain.user.service.UserService
 import java.util.UUID
@@ -22,7 +22,7 @@ class QueryStudentsUseCase(
         minPoint: Int?,
         maxPoint: Int?,
         tagIds: List<UUID>?
-    ): QueryStudentsResponse {
+    ): StudentsResponse {
 
         val user = userService.getCurrentUser()
 
@@ -38,26 +38,8 @@ class QueryStudentsUseCase(
             schoolId = user.schoolId,
             pointFilter = pointFilter,
             tagIds = tagIds
-        ).map {
-            QueryStudentsResponse.StudentElement(
-                id = it.id,
-                name = it.name,
-                gcn = it.gcn,
-                roomNumber = it.roomNumber,
-                profileImageUrl = it.profileImageUrl,
-                sex = it.sex,
-                tags = it.tags
-                    .map {
-                            tag ->
-                        QueryStudentsResponse.StudentTagElement(
-                            id = tag.id,
-                            name = tag.name,
-                            color = tag.color
-                        )
-                    }
-            )
-        }
+        )
 
-        return QueryStudentsResponse(students)
+        return StudentsResponse.of(students)
     }
 }
