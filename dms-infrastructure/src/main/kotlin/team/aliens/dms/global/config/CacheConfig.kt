9707aa1782f610278eea8029a862cc5d24b8f305
rlaisqls <rlaisqls@gmail.com>
2023-05-10 07:15:59 +0900

    create: (#490) CacheConfig

---
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/config/CacheConfig.kt
@@ -0,0 +1,8 @@
+package team.aliens.dms.global.config
+
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableCaching
+class CacheConfig
