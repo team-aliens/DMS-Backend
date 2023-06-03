6fc6b84deafc3ffe40127e254af138e2585cc3d9
rlaisqls <rlaisqls@gmail.com>
2023-06-03 11:22:28 +0900

    chore: (#516) file read 방식변경

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/thirdparty/notification/FCMConfig.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/thirdparty/notification/FCMConfig.kt
@@ -3,7 +3,7 @@ package team.aliens.dms.thirdparty.notification
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
-import java.io.ByteArrayInputStream
+import java.io.File
 import java.io.IOException
 import javax.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
@@ -20,7 +20,7 @@ class FCMConfig(
         try {
             if (FirebaseApp.getApps().isEmpty()) {
                 val options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(ByteArrayInputStream(path.toByteArray())))
+                    .setCredentials(GoogleCredentials.fromStream(File(path).inputStream()))
                     .build()
                 FirebaseApp.initializeApp(options)
             }
