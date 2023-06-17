d8349dd432063147bfb53b3ba2cee5e605dc9b99
rlaisqls <rlaisqls@gmail.com>
2023-06-17 09:56:11 +0900

    chore: (#525) transaction 추가

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/event/EventHandler.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/event/EventHandler.kt
@@ -1,6 +1,8 @@
 package team.aliens.dms.event
 
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 import team.aliens.dms.domain.notification.service.NotificationService
@@ -10,26 +12,28 @@ class NotificationEventHandler(
     private val notificationService: NotificationService
 ) {
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun onNotificationEvent(event: TopicNotificationEvent) {
-
-        when (event) {
-            is SingleNotificationEvent -> {
-                notificationService.sendMessage(
-                    deviceToken = event.deviceToken,
-                    notification = event.notification
-                )
-            }
-            is GroupNotificationEvent -> {
-                notificationService.sendMessages(
-                    deviceTokens = event.deviceTokens,
-                    notification = event.notification
-                )
-            }
-            else -> {
-                notificationService.sendMessagesByTopic(
-                    notification = event.notification
-                )
+        event.run {
+            when (this) {
+                is SingleNotificationEvent -> {
+                    notificationService.sendMessage(
+                        deviceToken = deviceToken,
+                        notification = notification
+                    )
+                }
+                is GroupNotificationEvent -> {
+                    notificationService.sendMessages(
+                        deviceTokens = deviceTokens,
+                        notification = notification
+                    )
+                }
+                else -> {
+                    notificationService.sendMessagesByTopic(
+                        notification = event.notification
+                    )
+                }
             }
         }
     }
