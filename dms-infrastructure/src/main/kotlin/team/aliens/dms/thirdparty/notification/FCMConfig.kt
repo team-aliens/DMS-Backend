3d279a4f1829d3cefb58157bfcae8a8ad1a932f2
rlaisqls <rlaisqls@gmail.com>
2023-07-06 16:46:16 +0900

    feat: FCM

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/thirdparty/notification/FCMConfig.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/thirdparty/notification/FCMConfig.kt
@@ -3,29 +3,42 @@ package team.aliens.dms.thirdparty.notification
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Configuration
 import java.io.File
 import java.io.IOException
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Paths
 import javax.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+
 
 @Configuration
 class FCMConfig(
-    @Value("\${fcm.path}")
-    private val path: String
+    @Value("\${fcm.file-url}")
+    private val url: String
 ) {
 
     @PostConstruct
     fun initialize() {
         try {
-            if (FirebaseApp.getApps().isEmpty()) {
-                val options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(File(path).inputStream()))
-                    .build()
-                FirebaseApp.initializeApp(options)
+            URL(url).openStream().use { inputStream ->
+                Files.copy(inputStream, Paths.get(PATH))
+                val file = File(PATH)
+                if (FirebaseApp.getApps().isEmpty()) {
+                    val options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(file.inputStream()))
+                        .build()
+                    FirebaseApp.initializeApp(options)
+                }
+                file.delete()
             }
         } catch (e: IOException) {
             e.printStackTrace()
         }
+    }
+
+    companion object {
+        private const val PATH = "./credentials.json"
     }
 }
