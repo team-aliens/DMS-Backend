13b5df7aa2357cec1e98a8350178402c6a314640
junsuPark <krpjs0508@gmail.com>
2023-07-10 17:37:35 +0900

    feat : saveNotificationOfUserByUserId

--- dms-core/src/main/kotlin/team/aliens/dms/domain/notification/service/CommandNotificationService.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/notification/service/CommandNotificationService.kt
@@ -1,13 +1,24 @@
 package team.aliens.dms.domain.notification.service
 
 import team.aliens.dms.domain.notification.model.DeviceToken
+import team.aliens.dms.domain.notification.model.NotificationOfUser
 import java.util.UUID
 
 interface CommandNotificationService {
 
-    fun saveDeviceToken(deviceToken: DeviceToken)
+    fun saveDeviceToken(deviceToken: DeviceToken): DeviceToken
 
     fun deleteNotificationOfUserByUserIdAndId(userId: UUID, notificationOfUserId: UUID)
 
     fun deleteNotificationOfUserByUserId(userId: UUID)
+
+    fun saveNotificationOfUserByUserId(
+        userId: UUID,
+        notificationOfUser: NotificationOfUser
+    ): NotificationOfUser
+
+    fun saveNotificationsOfUserByUserId(
+        userId: UUID,
+        notificationOfUsers: List<NotificationOfUser>
+    )
 }
