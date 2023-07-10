93fc6708bda848361831ef78fa06eec23c3ce113
Kangmin <ehfehf0629@gmail.com>
2023-07-10 19:11:56 +0900

    feat: ApplicationAvailableTime Enum

---
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/school/model/ApplicationAvailableTimeType.kt
@@ -0,0 +1,7 @@
+package team.aliens.dms.domain.school.model
+
+enum class ApplicationAvailableTimeType {
+    STUDY_ROOM,
+    REMAIN,
+    OUTING
+}
---
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/school/model/CustomDayOfWeek.kt
@@ -0,0 +1,13 @@
+package team.aliens.dms.domain.school.model
+
+enum class CustomDayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+    COMMON,
+    NONE
+}
