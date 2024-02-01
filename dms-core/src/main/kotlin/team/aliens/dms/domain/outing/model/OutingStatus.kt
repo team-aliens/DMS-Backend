f4cb8c7c26f615c2a18a838df357f888e7a331b0
ilyoil2 <0211ilyoil@gmail.com>
2024-02-01 21:51:42 +0900

    feat: (#538) outing type domain & mapper

---
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/outing/model/OutingStatus.kt
@@ -0,0 +1,9 @@
+package team.aliens.dms.domain.outing.model
+
+enum class OutingStatus{
+    REQUESTED,
+    APPROVED,
+    REJECTED,
+    OUTING,
+    DONE
+}
---
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/mapper/OutingTypeMapper.kt
@@ -0,0 +1,36 @@
+package team.aliens.dms.persistence.outing.mapper
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import team.aliens.dms.domain.outing.model.OutingType
+import team.aliens.dms.persistence.GenericMapper
+import team.aliens.dms.persistence.outing.entity.OutingTypeJpaEntity
+import team.aliens.dms.persistence.outing.entity.OutingTypeJpaEntityId
+import team.aliens.dms.persistence.school.repository.SchoolJpaRepository
+
+@Component
+class OutingTypeMapper (
+    private val schoolRepository: SchoolJpaRepository
+) : GenericMapper<OutingType, OutingTypeJpaEntity> {
+
+    override fun toDomain(entity: OutingTypeJpaEntity?): OutingType? {
+        return entity?.let {
+            OutingType(
+                outingType = it.id.outingType,
+                schoolId = it.id.schoolId
+            )
+        }
+    }
+
+    override fun toEntity(domain: OutingType): OutingTypeJpaEntity {
+        val school = schoolRepository.findByIdOrNull(domain.schoolId)
+
+        return OutingTypeJpaEntity(
+            id = OutingTypeJpaEntityId(
+                outingType = domain.outingType,
+                schoolId = domain.schoolId
+            ),
+            school = school
+        )
+    }
+}
