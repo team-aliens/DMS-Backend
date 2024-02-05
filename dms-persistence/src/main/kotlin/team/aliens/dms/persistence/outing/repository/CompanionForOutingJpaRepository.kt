fb673154d884daeabc62a90598618dd58eeb8f52
ilyoil2 <0211ilyoil@gmail.com>
2024-02-06 00:10:34 +0900

    feat: (#542) companion for outing jpa entity & repository

---
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/repository/CompanionForOutingJpaRepository.kt
@@ -0,0 +1,9 @@
+package team.aliens.dms.persistence.outing.repository
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import team.aliens.dms.persistence.outing.entity.CompanionForOutingJpaEntityId
+
+@Repository
+interface CompanionForOutingJpaRepository : CrudRepository<CompanionForOutingJpaRepository, CompanionForOutingJpaEntityId>{
+}
