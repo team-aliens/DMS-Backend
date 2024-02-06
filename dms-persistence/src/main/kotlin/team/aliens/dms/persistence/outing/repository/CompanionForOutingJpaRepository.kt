9fbc06ed2fccc7d854896125f2e52079c2d478fc
ilyoil2 <0211ilyoil@gmail.com>
2024-02-06 23:25:18 +0900

    fix: (#542) 제너릭 타입 수정

--- dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/repository/CompanionForOutingJpaRepository.kt
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/repository/CompanionForOutingJpaRepository.kt
@@ -2,8 +2,9 @@ package team.aliens.dms.persistence.outing.repository
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import team.aliens.dms.persistence.outing.entity.CompanionForOutingJpaEntity
 import team.aliens.dms.persistence.outing.entity.CompanionForOutingJpaEntityId
 
 @Repository
-interface CompanionForOutingJpaRepository : CrudRepository<CompanionForOutingJpaRepository, CompanionForOutingJpaEntityId>{
+interface CompanionForOutingJpaRepository : CrudRepository<CompanionForOutingJpaEntity, CompanionForOutingJpaEntityId>{
 }
