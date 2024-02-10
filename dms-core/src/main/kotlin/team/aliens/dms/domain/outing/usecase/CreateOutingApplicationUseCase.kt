12909c29e5eb3098694071b56ede13840bade55e
ilyoil2 <0211ilyoil@gmail.com>
2024-02-11 07:04:21 +0900

    feat: (#544) 외출 신청 검증 로직

--- dms-core/src/main/kotlin/team/aliens/dms/domain/outing/usecase/CreateOutingApplicationUseCase.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/outing/usecase/CreateOutingApplicationUseCase.kt
@@ -6,21 +6,28 @@ import team.aliens.dms.domain.outing.dto.CreateOutingApplicationResponse
 import team.aliens.dms.domain.outing.model.OutingApplication
 import team.aliens.dms.domain.outing.model.OutingStatus
 import team.aliens.dms.domain.outing.service.OutingService
+import team.aliens.dms.domain.student.service.StudentService
 import team.aliens.dms.domain.user.service.UserService
 import java.time.LocalDateTime
 
 @UseCase
 class CreateOutingApplicationUseCase(
     private val outingService: OutingService,
+    private val studentService: StudentService,
     private val userService: UserService
 ) {
 
     fun execute(request: CreateOutingApplicationRequest): CreateOutingApplicationResponse {
+        val student = studentService.getCurrentStudent()
         val user = userService.getCurrentUser()
+
+        outingService.checkOutingApplicationTimeAvailable(request.outAt)
+
+        outingService.checkOutingApplicationExistsByOutAtAndStudentId(request.outAt, student.id)
 
         val outing = outingService.saveOutingApplication(
             OutingApplication(
-                studentId = user.id,
+                studentId = student.id,
                 createdAt = LocalDateTime.now(),
                 outAt = request.outAt,
                 outingTime = request.outingTime,
@@ -30,7 +37,6 @@ class CreateOutingApplicationUseCase(
                 destination = request.destination,
                 outingTypeTitle = request.outingTypeId.title,
                 schoolId = user.schoolId
-
             )
         )
 
--- dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/OutingPersistenceAdapter.kt
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/OutingPersistenceAdapter.kt
@@ -5,16 +5,22 @@ import team.aliens.dms.domain.outing.model.OutingApplication
 import team.aliens.dms.domain.outing.spi.OutingApplicationPort
 import team.aliens.dms.persistence.outing.mapper.OutingApplicationMapper
 import team.aliens.dms.persistence.outing.repository.OutingApplicationJpaRepository
+import java.time.LocalDate
+import java.util.UUID
 
 @Component
 class OutingPersistenceAdapter(
     private val outingApplicationMapper: OutingApplicationMapper,
     private val outingApplicationRepository: OutingApplicationJpaRepository
 ) : OutingApplicationPort {
-    override fun saveOutingApplication(outingApplication: OutingApplication): OutingApplication {
-        return outingApplicationMapper.toDomain(
-            outingApplicationRepository.save(outingApplicationMapper.toEntity(outingApplication))
-        )!!
-    }
 
+    override fun existOutingApplicationByOutAtAndStudentId(outAt: LocalDate, studentId: UUID) =
+        outingApplicationRepository.existsByOutAtAndStudentId(outAt, studentId)
+
+    override fun saveOutingApplication(outingApplication: OutingApplication) =
+        outingApplicationMapper.toDomain(
+            outingApplicationRepository.save(
+                outingApplicationMapper.toEntity(outingApplication)
+            )
+        )!!
 }
