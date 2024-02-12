1b53c851973dafc540faec23eb9fde6dc36761a8
alsdl0629 <ehfehf0629@gmail.com>
2024-02-12 17:10:48 +0900

    chore: (#544) OutingExceptions 통일

--- dms-core/src/main/kotlin/team/aliens/dms/domain/outing/exception/OutingApplicationAlreadyExistsException.kt
+++
@@ -1,8 +0,0 @@
-package team.aliens.dms.domain.outing.exception
-
-import team.aliens.dms.common.error.DmsException
-import team.aliens.dms.domain.outing.exception.error.OutingErrorCode
-
-object OutingApplicationAlreadyExistsException : DmsException(
-    OutingErrorCode.OUTING_APPLICATION_EXISTS
-)
---
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/outing/exception/OutingExceptions.kt
@@ -0,0 +1,15 @@
+package team.aliens.dms.domain.outing.exception
+
+import team.aliens.dms.common.error.DmsException
+
+object OutingApplicationAlreadyExistsException : DmsException(
+    OutingErrorCode.OUTING_APPLICATION_ALREADY_EXISTS
+)
+
+object OutingApplicationNotFoundException : DmsException(
+    OutingErrorCode.OUTING_APPLICATION_NOT_FOUND
+)
+
+object OutingAvailableTimeMismatchException :DmsException(
+    OutingErrorCode.OUTING_AVAILABLE_TIME_MISMATCH
+)
