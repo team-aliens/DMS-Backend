8f89b256620738bfe156e5c0d55b29c13fe10b86
alsdl0629 <ehfehf0629@gmail.com>
2024-02-12 17:10:48 +0900

    chore: (#544) OutingExceptions 통일

--- dms-core/src/main/kotlin/team/aliens/dms/domain/outing/exception/OutingApplicationNotFoundException.kt
+++
@@ -1,8 +0,0 @@
-package team.aliens.dms.domain.outing.exception
-
-import team.aliens.dms.common.error.DmsException
-import team.aliens.dms.domain.outing.exception.error.OutingErrorCode
-
-object OutingApplicationNotFoundException : DmsException(
-    OutingErrorCode.OUTING_APPLICATION_NOT_FOUND
-)
