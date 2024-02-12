ad3a17215ce9c79ebad433ccc663fc1b443acd9d
ilyoil2 <0211ilyoil@gmail.com>
2024-02-12 16:45:09 +0900

    chore: (#544) 이름 변경

---
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/outing/exception/error/OutingErrorCode.kt
@@ -0,0 +1,20 @@
+package team.aliens.dms.domain.outing.exception.error
+
+import team.aliens.dms.common.error.ErrorProperty
+import team.aliens.dms.common.error.ErrorStatus
+
+enum class OutingErrorCode(
+    private val status: Int,
+    private val message: String,
+    private val sequence: Int
+) : ErrorProperty {
+
+    OUTING_AVAILABLE_TIME_MISMATCH(ErrorStatus.FORBIDDEN, "Outing Available Time Mismatch", 1),
+    OUTING_APPLICATION_EXISTS(ErrorStatus.CONFLICT, "Outing Application Exists", 1),
+    OUTING_APPLICATION_NOT_FOUND(ErrorStatus.NOT_FOUND, "Outing Not Found", 1)
+    ;
+
+    override fun status(): Int = status
+    override fun message(): String = message
+    override fun code(): String = "OUTING-$status-$sequence"
+}
