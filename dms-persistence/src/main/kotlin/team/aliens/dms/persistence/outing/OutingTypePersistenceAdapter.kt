9ebbe9b5e4a39cf85b26d3e40a18e10ff1515be5
Kangmin <ehfehf0629@gmail.com>
2024-02-13 16:53:52 +0900

    feat: (#549) 외출 유형 조회

--- dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/OutingTypePersistenceAdapter.kt
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/OutingTypePersistenceAdapter.kt
@@ -1,17 +1,21 @@
 package team.aliens.dms.persistence.outing
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.outing.model.OutingType
 import team.aliens.dms.domain.outing.spi.OutingTypePort
 import team.aliens.dms.persistence.outing.entity.OutingTypeJpaEntityId
+import team.aliens.dms.persistence.outing.entity.QOutingTypeJpaEntity.outingTypeJpaEntity
 import team.aliens.dms.persistence.outing.mapper.OutingTypeMapper
 import team.aliens.dms.persistence.outing.repository.OutingTypeJpaRepository
+import java.util.UUID
 
 @Component
 class OutingTypePersistenceAdapter(
     private val outingTypeRepository: OutingTypeJpaRepository,
-    private val outingTypeMapper: OutingTypeMapper
+    private val outingTypeMapper: OutingTypeMapper,
+    private val queryFactory: JPAQueryFactory
 ) : OutingTypePort {
 
     override fun existsOutingType(outingType: OutingType): Boolean {
@@ -25,6 +29,13 @@ class OutingTypePersistenceAdapter(
             outingTypeRepository.findByIdOrNull(id)
         )
     }
+
+    override fun queryAllOutingTypeTitlesBySchoolId(schoolId: UUID): List<String> =
+        queryFactory
+            .select(outingTypeJpaEntity.id.title)
+            .from(outingTypeJpaEntity)
+            .where(outingTypeJpaEntity.id.schoolId.eq(schoolId))
+            .fetch()
 
     override fun saveOutingType(outingType: OutingType): OutingType =
         outingTypeMapper.toDomain(
