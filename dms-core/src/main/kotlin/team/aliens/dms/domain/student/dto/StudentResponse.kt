1f7b5850738f6d091031e1e959348d70d0d08d98
Kangmin <ehfehf0629@gmail.com>
2024-02-20 22:51:10 +0900

    feat: (#559) 학생 목록 조회 api

--- dms-core/src/main/kotlin/team/aliens/dms/domain/student/dto/StudentResponse.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/student/dto/StudentResponse.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.domain.student.dto
 import team.aliens.dms.domain.manager.spi.vo.StudentWithTag
 import team.aliens.dms.domain.student.model.Sex
 import team.aliens.dms.domain.student.model.Student
+import team.aliens.dms.domain.student.spi.vo.AllStudentsVO
 import team.aliens.dms.domain.tag.dto.TagResponse
 import team.aliens.dms.domain.tag.model.Tag
 import java.util.UUID
@@ -46,16 +47,16 @@ data class StudentNameResponse(
 )
 
 data class StudentDetailsResponse(
-    val id: UUID,
+    val id: UUID? = null,
     val name: String,
     val gcn: String,
     val profileImageUrl: String,
-    val sex: Sex,
+    val sex: Sex? = null,
     val bonusPoint: Int? = null,
     val minusPoint: Int? = null,
-    val roomNumber: String,
+    val roomNumber: String? = null,
     val roomMates: List<RoomMate>? = null,
-    val tags: List<TagResponse>
+    val tags: List<TagResponse>? = null
 ) {
     data class RoomMate(
         val id: UUID,
@@ -98,9 +99,10 @@ data class StudentDetailsResponse(
 }
 
 data class StudentsResponse(
-    val students: List<StudentDetailsResponse>
+    val students: List<StudentDetailsResponse>,
 ) {
     companion object {
+        @JvmName("ofStudentWithTag")
         fun of(students: List<StudentWithTag>) = StudentsResponse(
             students = students.map {
                 StudentDetailsResponse(
@@ -113,6 +115,17 @@ data class StudentsResponse(
                     tags = it.tags.map { tag ->
                         TagResponse.of(tag)
                     }
+                )
+            }
+        )
+
+        @JvmName("ofAllStudents")
+        fun of(students: List<AllStudentsVO>) = StudentsResponse(
+            students = students.map {
+                StudentDetailsResponse(
+                    name = it.name,
+                    gcn = it.gcn,
+                    profileImageUrl = it.profileImageUrl
                 )
             }
         )
