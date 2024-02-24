f0e57bf537cfa4100e360cb684ff113cec0b6fa0
ilyoil2 <0211ilyoil@gmail.com>
2024-02-25 01:06:01 +0900

    feat: (#565) : 현재 외출 신청 조회 webAdapter & response

--- dms-core/src/main/kotlin/team/aliens/dms/domain/outing/dto/OutingResponse.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/outing/dto/OutingResponse.kt
@@ -1,5 +1,10 @@
 package team.aliens.dms.domain.outing.dto
 
+import team.aliens.dms.domain.outing.model.OutingStatus
+import team.aliens.dms.domain.outing.model.OutingType
+import java.time.LocalDate
+import java.time.LocalTime
+
 data class GetAllOutingTypeTitlesResponse(
     val titles: List<String>
 )
@@ -7,4 +12,14 @@ data class GetAllOutingTypeTitlesResponse(
 data class ExportAllOutingApplicationsResponse(
     val file: ByteArray,
     val fileName: String
+)
+
+data class CurrentOutingApplicationResponse(
+    val outAt: LocalDate,
+    val outingType: OutingType,
+    val status: OutingStatus,
+    val outingTime: LocalTime,
+    val arrivalTime: LocalTime,
+    val students: List<String>,
+    val reason: String
 )
