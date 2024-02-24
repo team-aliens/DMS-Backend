81bf9045572aad38780ba542dbdd8becb4a34701
ilyoil2 <0211ilyoil@gmail.com>
2024-02-25 01:07:43 +0900

    feat: (#565) : getCurrentOutingApplication

--- dms-core/src/main/kotlin/team/aliens/dms/domain/outing/service/GetOutingService.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/outing/service/GetOutingService.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.domain.outing.service
 import team.aliens.dms.domain.outing.model.OutingApplication
 import team.aliens.dms.domain.outing.model.OutingType
 import team.aliens.dms.domain.outing.spi.vo.OutingApplicationVO
+import team.aliens.dms.domain.student.model.Student
 import java.time.LocalDate
 import java.util.UUID
 
@@ -15,4 +16,6 @@ interface GetOutingService {
     fun getOutingApplicationById(outingApplicationId: UUID): OutingApplication
 
     fun getAllOutingApplicationVOsBetweenStartAndEnd(start: LocalDate, end: LocalDate): List<OutingApplicationVO>
+
+    fun getCurrentOutingApplication(student: Student): OutingApplicationVO
 }
