a00b5d7d126cbda5a9a155a2eed1b10c07ffbb23
ilyoil2 <0211ilyoil@gmail.com>
2024-02-25 01:06:40 +0900

    feat: (#565) : QueryCurrentOutingApplicationUseCase

---
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/outing/usecase/QueryCurrentOutingApplicationUseCase.kt
@@ -0,0 +1,20 @@
+package team.aliens.dms.domain.outing.usecase
+
+import team.aliens.dms.common.annotation.ReadOnlyUseCase
+import team.aliens.dms.domain.outing.dto.CurrentOutingApplicationResponse
+import team.aliens.dms.domain.outing.service.OutingService
+import team.aliens.dms.domain.student.service.StudentService
+
+@ReadOnlyUseCase
+class QueryCurrentOutingApplicationUseCase(
+    private val outingService: OutingService,
+    private val studentService: StudentService
+) {
+
+    fun execute(): CurrentOutingApplicationResponse {
+
+        val student = studentService.getCurrentStudent()
+
+        val outingApplication = outingService.getCurrentOutingApplication(student)
+    }
+}
