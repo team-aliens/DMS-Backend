2361697b93e17d3f5b5731fc9d7bbc8380a5429f
ilyoil2 <0211ilyoil@gmail.com>
2024-03-13 13:17:19 +0900

    chore: (#573) 메세지 수정

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/manager/dto/request/AccountIdRegex.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/manager/dto/request/AccountIdRegex.kt
@@ -2,5 +2,5 @@ package team.aliens.dms.domain.manager.dto.request
 
 object AccountIdRegex {
     const val PATTERN = "^[A-Za-z0-9._-]{4,20}\$"
-    const val MESSAGE = "4~20자이어야 합니다."
+    const val MESSAGE = "4~20자여야 합니다."
 }
--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/manager/dto/request/PasswordRegex.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/manager/dto/request/PasswordRegex.kt
@@ -2,5 +2,5 @@ package team.aliens.dms.domain.manager.dto.request
 
 object PasswordRegex {
     const val PATTERN = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#\$%&'()*+,./:;<=>?@＼^_`{|}~-])[a-zA-Z0-9!#\$%&'()*+,./:;<=>?@＼^_`{|}~-]{8,20}\$"
-    const val MESSAGE = "영문, 숫자, 기호를 포함한 8~20자이어야 합니다."
+    const val MESSAGE = "영문, 숫자, 기호를 포함한 8~20자여야 합니다."
 }
