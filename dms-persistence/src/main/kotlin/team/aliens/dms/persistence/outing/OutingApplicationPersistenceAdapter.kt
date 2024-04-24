d8405cacacddddfdc45e2c0056b3b935315c6468
khyunwook <khyunwook0707@gmail.com>
2024-04-24 19:48:32 +0900

    chore: (#611) 외출 검색 로직 변경

--- dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/OutingApplicationPersistenceAdapter.kt
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/OutingApplicationPersistenceAdapter.kt
@@ -135,7 +135,7 @@ class OutingApplicationPersistenceAdapter(
             .on(outingApplicationJpaEntity.id.eq(outingCompanionJpaEntity.outingApplication.id))
             .leftJoin(outingCompanionJpaEntity.student, outingCompanionStudentJpaEntity)
             .where(
-                studentName?.let { studentJpaEntity.name.eq(it) },
+                studentName?.let { studentJpaEntity.name.contains(it) },
                 outingApplicationJpaEntity.outingDate.eq(date)
             )
             .groupBy(outingApplicationJpaEntity.id)
