f31024ecf98db6076a721fd8e5f855852ef1a005
tae.y <0211ilyoil@gmail.com>
2024-06-16 20:29:29 +0900

    feat: (#657) 예외로직 추가

--- dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/repository/OutingCompanionJpaRepository.kt
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/outing/repository/OutingCompanionJpaRepository.kt
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Repository
 import team.aliens.dms.persistence.outing.entity.OutingApplicationJpaEntity
 import team.aliens.dms.persistence.outing.entity.OutingCompanionJpaEntity
 import team.aliens.dms.persistence.outing.entity.OutingCompanionJpaEntityId
+import java.util.UUID
 
 @Repository
 interface OutingCompanionJpaRepository : CrudRepository<OutingCompanionJpaEntity, OutingCompanionJpaEntityId> {
     fun deleteAllByOutingApplication(outingApplication: OutingApplicationJpaEntity)
+
+    fun existsByStudentId(studentId: UUID): Boolean
 }
