e5c2159545aad8c13e3df455f3f04eb27083f6b1
khyunwook <khyunwook0707@gmail.com>
2024-06-25 12:19:35 +0900

    feat: (#663) @Service 추가

--- dms-core/src/main/kotlin/team/aliens/dms/domain/notification/service/CheckNotificationServiceImpl.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/notification/service/CheckNotificationServiceImpl.kt
@@ -1,8 +1,10 @@
 package team.aliens.dms.domain.notification.service
 
+import team.aliens.dms.common.annotation.Service
 import team.aliens.dms.domain.notification.spi.QueryDeviceTokenPort
 import java.util.UUID
 
+@Service
 class CheckNotificationServiceImpl(
     private val deviceTokenPort: QueryDeviceTokenPort
 ) : CheckNotificationService {
