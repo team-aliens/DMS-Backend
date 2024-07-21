6cee8e98070e8e4f591aa5cfd9a8b4d641ce5b22
4mjeo <4mj.eo@dsm.hs.kr>
2024-07-21 21:51:03 +0900

    ref: (#682) pointHistory 객체 별 알림 수신

--- dms-core/src/main/kotlin/team/aliens/dms/domain/notification/model/Notification.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/notification/model/Notification.kt
@@ -2,8 +2,6 @@ package team.aliens.dms.domain.notification.model
 
 import team.aliens.dms.domain.notice.model.Notice
 import team.aliens.dms.domain.point.model.PointHistory
-import team.aliens.dms.domain.point.model.PointOption
-import team.aliens.dms.domain.point.model.PointType
 import java.time.LocalDateTime
 import java.util.UUID
 
--- dms-core/src/main/kotlin/team/aliens/dms/domain/point/service/CommandPointServiceImpl.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/point/service/CommandPointServiceImpl.kt
@@ -2,6 +2,10 @@ package team.aliens.dms.domain.point.service
 
 import team.aliens.dms.common.annotation.Service
 import team.aliens.dms.common.spi.NotificationEventPort
+import team.aliens.dms.common.spi.SecurityPort
+import team.aliens.dms.domain.notification.exception.DeviceTokenNotFoundException
+import team.aliens.dms.domain.notification.model.Notification
+import team.aliens.dms.domain.notification.spi.QueryDeviceTokenPort
 import team.aliens.dms.domain.point.model.PointHistory
 import team.aliens.dms.domain.point.model.PointOption
 import team.aliens.dms.domain.point.spi.CommandPointHistoryPort
@@ -11,7 +15,9 @@ import team.aliens.dms.domain.point.spi.CommandPointOptionPort
 class CommandPointServiceImpl(
     private val commandPointHistoryPort: CommandPointHistoryPort,
     private val commandPointOptionPort: CommandPointOptionPort,
-    private val notificationEventPort: NotificationEventPort
+    private val notificationEventPort: NotificationEventPort,
+    private val securityPort: SecurityPort,
+    private val deviceTokenPort: QueryDeviceTokenPort
 ) : CommandPointService {
 
     override fun savePointHistory(pointHistory: PointHistory) =
@@ -22,7 +28,18 @@ class CommandPointServiceImpl(
     }
 
     override fun saveAllPointHistories(pointHistories: List<PointHistory>) {
+        val userId = securityPort.getCurrentUserId()
+        val deviceToken = deviceTokenPort.queryDeviceTokenByUserId(userId)
+            ?: throw DeviceTokenNotFoundException
+
         commandPointHistoryPort.saveAllPointHistories(pointHistories)
+
+        pointHistories.forEach { pointHistory ->
+            notificationEventPort.publishNotification(
+                deviceToken,
+                Notification.PointNotification(pointHistory)
+            )
+        }
     }
 
     override fun savePointOption(pointOption: PointOption): PointOption =
