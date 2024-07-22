994e9039de698aec39d30afb88a8466445ee7c5f
4mjeo <4mj.eo@dsm.hs.kr>
2024-07-22 12:27:56 +0900

    chore: (#687) 주석 추가

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/scheduler/NotificationScheduler.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/scheduler/NotificationScheduler.kt
@@ -9,6 +9,9 @@ class NotificationScheduler(
     private val deleteOldNotificationsUseCase: DeleteOldNotificationsUseCase
 ) {
 
+    /**
+     * 매일 밤 12시 마다 60일이 지난 알림들 삭제
+     */
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     fun deleteOldNotifications() {
         deleteOldNotificationsUseCase.execute()
