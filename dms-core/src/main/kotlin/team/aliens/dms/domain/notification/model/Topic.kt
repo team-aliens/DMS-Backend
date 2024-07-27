570526fe1f8c487a989f23229f5d826b5a3f382c
4mjeo <4mj.eo@dsm.hs.kr>
2024-07-27 16:41:18 +0900

    ref: (#689) 외출 설정 상태 조회 response 수정

--- dms-core/src/main/kotlin/team/aliens/dms/domain/notification/model/Topic.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/notification/model/Topic.kt
@@ -10,37 +10,25 @@ enum class TopicGroup(
 }
 
 enum class Topic(
-    val topicGroup: TopicGroup,
-    val title: String,
-    val content: String
+    val topicGroup: TopicGroup
 ) {
     NOTICE(
-        topicGroup = TopicGroup.NOTICE,
-        title = "공지 알림",
-        content = "기숙사 공지에 대한 알림입니다."
+        topicGroup = TopicGroup.NOTICE
     ),
 
     STUDY_ROOM_TIME_SLOT(
-        topicGroup = TopicGroup.STUDY_ROOM,
-        title = "이용 시간 알림",
-        content = "자습실 이용 시작 10분 전에 알림을 받을 수 있습니다."
+        topicGroup = TopicGroup.STUDY_ROOM
     ),
 
     STUDY_ROOM_APPLY(
-        topicGroup = TopicGroup.STUDY_ROOM,
-        title = "신청 시간 알림",
-        content = "자습실 신청 시간을 알리는 알림입니다."
+        topicGroup = TopicGroup.STUDY_ROOM
     ),
 
     POINT(
-        topicGroup = TopicGroup.POINT,
-        title = "상벌점 알림",
-        content = "상벌점을 부여 받았을 때 알림을 받을 수 있습니다."
+        topicGroup = TopicGroup.POINT
     ),
 
     OUTING(
-        topicGroup = TopicGroup.OUTING,
-        title = "외출 알림",
-        content = "외출 신청 시 알림을 받을 수 있습니다"
+        topicGroup = TopicGroup.OUTING
     )
 }
