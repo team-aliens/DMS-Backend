247e47e14d2bce8fad1349056b42f88c77acde3f
khyunwook <khyunwook0707@gmail.com>
2024-08-01 19:43:41 +0900

    feat: (#695) 쿼리시 구독되지 않은 Topic 구독 로직 추가

--- dms-core/src/main/kotlin/team/aliens/dms/domain/notification/usecase/QueryTopicSubscriptionUseCase.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/notification/usecase/QueryTopicSubscriptionUseCase.kt
@@ -1,17 +1,32 @@
 package team.aliens.dms.domain.notification.usecase
 
-import team.aliens.dms.common.annotation.ReadOnlyUseCase
+import team.aliens.dms.common.annotation.UseCase
 import team.aliens.dms.domain.notification.dto.TopicSubscriptionGroupsResponse
+import team.aliens.dms.domain.notification.model.TopicGroup
 import team.aliens.dms.domain.notification.service.NotificationService
 
-@ReadOnlyUseCase
+@UseCase
 class QueryTopicSubscriptionUseCase(
     private val notificationService: NotificationService
 ) {
 
     fun execute(token: String): TopicSubscriptionGroupsResponse {
+        val topicSubscriptions = notificationService.getTopicSubscriptionsByToken(token)
+
+        notificationService.updateSubscribes(
+            token = token,
+            topicsToSubscribe = topicSubscriptions.filter { topicSubscription ->
+                TopicGroup.values().any { topicSubscription.topic.name == it.name }
+            }.map { topicSubscription ->
+                Pair(
+                    topicSubscription.topic,
+                    true
+                )
+            }
+        )
+
         return TopicSubscriptionGroupsResponse.of(
-            notificationService.getTopicSubscriptionsByToken(token)
+            topicSubscriptions = topicSubscriptions
         )
     }
 }
