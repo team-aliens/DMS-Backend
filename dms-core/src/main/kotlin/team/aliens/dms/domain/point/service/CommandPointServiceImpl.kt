85c505e33c6bb5fae5fee382c52c0787db607bfd
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-11 23:21:07 +0900

    fix: publishNotificationToApplicant 사용

--- dms-core/src/main/kotlin/team/aliens/dms/domain/point/service/CommandPointServiceImpl.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/point/service/CommandPointServiceImpl.kt
@@ -3,13 +3,13 @@ package team.aliens.dms.domain.point.service
 import team.aliens.dms.common.annotation.Service
 import team.aliens.dms.common.spi.NotificationEventPort
 import team.aliens.dms.common.spi.SecurityPort
-import team.aliens.dms.domain.notification.exception.DeviceTokenNotFoundException
 import team.aliens.dms.domain.notification.model.Notification
 import team.aliens.dms.domain.notification.spi.QueryDeviceTokenPort
 import team.aliens.dms.domain.point.model.PointHistory
 import team.aliens.dms.domain.point.model.PointOption
 import team.aliens.dms.domain.point.spi.CommandPointHistoryPort
 import team.aliens.dms.domain.point.spi.CommandPointOptionPort
+import team.aliens.dms.domain.student.spi.QueryStudentPort
 
 @Service
 class CommandPointServiceImpl(
@@ -17,7 +17,8 @@ class CommandPointServiceImpl(
     private val commandPointOptionPort: CommandPointOptionPort,
     private val notificationEventPort: NotificationEventPort,
     private val securityPort: SecurityPort,
-    private val deviceTokenPort: QueryDeviceTokenPort
+    private val deviceTokenPort: QueryDeviceTokenPort,
+    private val queryStudentPort: QueryStudentPort
 ) : CommandPointService {
 
     override fun savePointHistory(pointHistory: PointHistory) =
@@ -28,18 +29,31 @@ class CommandPointServiceImpl(
     }
 
     override fun saveAllPointHistories(pointHistories: List<PointHistory>) {
-        val userId = securityPort.getCurrentUserId()
-        val deviceToken = deviceTokenPort.queryDeviceTokenByUserId(userId)
-            ?: throw DeviceTokenNotFoundException
-
-        commandPointHistoryPort.saveAllPointHistories(pointHistories)
+        val schoolId = securityPort.getCurrentUserSchoolId()
 
         pointHistories.forEach { pointHistory ->
-            notificationEventPort.publishNotification(
-                deviceToken,
-                Notification.PointNotification(pointHistory)
-            )
+            val (grade, classRoom, number) = parseGcn(pointHistory.studentGcn)
+
+            val student = queryStudentPort.queryStudentBySchoolIdAndGcn(schoolId, grade, classRoom, number)
+
+            val deviceToken = student?.let {
+                deviceTokenPort.queryDeviceTokenByUserId(it.userId!!)
+            }
+
+            if (deviceToken != null) {
+                notificationEventPort.publishNotificationToApplicant(
+                    listOf(deviceToken),
+                    Notification.PointNotification(pointHistory)
+                )
+            }
         }
+
+        commandPointHistoryPort.saveAllPointHistories(pointHistories)
+    }
+
+    private fun parseGcn(studentGcn: String): Triple<Int, Int, Int> {
+        val parts = studentGcn.split("-").map { it.toInt() }
+        return Triple(parts[0], parts[1], parts[2])
     }
 
     override fun savePointOption(pointOption: PointOption): PointOption =
