111b82e8f4e73bc8bd9a7f567cfdb3085240a501
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-23 19:23:16 +0900

    feat: (#719) 구독여부에 따라 토글

--- dms-core/src/main/kotlin/team/aliens/dms/domain/notification/spi/QueryTopicSubscriptionPort.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/notification/spi/QueryTopicSubscriptionPort.kt
@@ -1,9 +1,12 @@
 package team.aliens.dms.domain.notification.spi
 
+import team.aliens.dms.domain.notification.model.Topic
 import team.aliens.dms.domain.notification.model.TopicSubscription
 import java.util.UUID
 
 interface QueryTopicSubscriptionPort {
 
     fun queryTopicSubscriptionsByDeviceTokenId(deviceTokenId: UUID): List<TopicSubscription>
+
+    fun queryDeviceTokenIdAndTopic(deviceTokenId: UUID, topic: Topic): TopicSubscription?
 }
--- dms-persistence/src/main/kotlin/team/aliens/dms/persistence/notification/repository/TopicSubscriptionJpaRepository.kt
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/notification/repository/TopicSubscriptionJpaRepository.kt
@@ -2,13 +2,17 @@ package team.aliens.dms.persistence.notification.repository
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import team.aliens.dms.domain.notification.model.Topic
 import team.aliens.dms.persistence.notification.entity.TopicSubscriptionJpaEntity
 import team.aliens.dms.persistence.notification.entity.TopicSubscriptionJpaEntityId
 import java.util.UUID
 
 @Repository
 interface TopicSubscriptionJpaRepository : CrudRepository<TopicSubscriptionJpaEntity, TopicSubscriptionJpaEntityId> {
-    fun findByDeviceTokenId(deviceTokenId: UUID): List<TopicSubscriptionJpaEntity>
 
     fun deleteAllByDeviceTokenId(deviceTokenId: UUID)
+
+    fun findById_DeviceTokenIdAndId_Topic(deviceTokenId: UUID, topic: Topic): TopicSubscriptionJpaEntity?
+
+    fun findByDeviceTokenId(deviceTokenId: UUID): List<TopicSubscriptionJpaEntity>
 }
