e2a21ca8231e11140d0354ca1becb4efe2438ac4
tae.y <0211ilyoil@gmail.com>
2024-09-12 16:22:47 +0900

    chore: (#740) timestamp로 변경

--- dms-infrastructure/src/main/resources/db/migration/V4__create_tbl_volunteer_and_attachment.sql
+++ dms-infrastructure/src/main/resources/db/migration/V4__create_tbl_volunteer_and_attachment.sql
@@ -1,7 +1,7 @@
 create table tbl_volunteer
 (
     id binary(16)   not null primary key,
-    created_at      datetime(6)  not null,
+    created_at      timestamp    not null,
     content         varchar(255) not null,
     grade_condition varchar(14)  not null,
     max_applicants  int unsigned not null,
