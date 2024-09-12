0962080714ec6a0e9de24770f91225ba6156fea7
khyunwook <khyunwook0707@gmail.com>
2024-09-12 14:18:20 +0900

    feat: (#733) CreateVolunteerWebRequest

---
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/volunteer/dto/CreateVolunteerRequest.kt
@@ -0,0 +1,11 @@
+package team.aliens.dms.domain.volunteer.dto
+
+data class CreateVolunteerRequest(
+    val name: String,
+    val content: String,
+    val sexCondition: String,
+    val gradeCondition: String,
+    val score: Int,
+    val optionalScore: Int,
+    val maxApplicants: Int
+)
--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/volunteer/VolunteerWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/volunteer/VolunteerWebAdapter.kt
@@ -1,16 +1,21 @@
 package team.aliens.dms.domain.volunteer
 
+import jakarta.validation.Valid
 import org.jetbrains.annotations.NotNull
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.RequestBody
+import team.aliens.dms.domain.volunteer.dto.CreateVolunteerRequest
+import team.aliens.dms.domain.volunteer.dto.CreateVolunteerWebRequest
 import team.aliens.dms.domain.volunteer.usecase.ApplyVolunteerUseCase
 import team.aliens.dms.domain.volunteer.usecase.UnapplyVolunteerUseCase
+import team.aliens.dms.domain.volunteer.usecase.CreateVolunteerUseCase
 import java.util.UUID
 
 @Validated
@@ -18,7 +23,8 @@ import java.util.UUID
 @RestController
 class VolunteerWebAdapter(
     private val applyVolunteerUseCase: ApplyVolunteerUseCase,
-    private val unapplyVolunteerUseCase: UnapplyVolunteerUseCase
+    private val unapplyVolunteerUseCase: UnapplyVolunteerUseCase,
+    private val createVolunteerUseCase: CreateVolunteerUseCase
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -31,5 +37,20 @@ class VolunteerWebAdapter(
     @DeleteMapping("/{volunteer-application-id}")
     fun unapplyVolunteer(@PathVariable("volunteer-application-id") @NotNull volunteerApplicationId: UUID) {
         unapplyVolunteerUseCase.execute(volunteerApplicationId)
+    }
+
+    @PostMapping
+    fun create(@Valid @RequestBody createVolunteerWebRequest: CreateVolunteerWebRequest) {
+        createVolunteerUseCase.execute(
+            CreateVolunteerRequest(
+                name = createVolunteerWebRequest.name,
+                content = createVolunteerWebRequest.content,
+                gradeCondition = createVolunteerWebRequest.gradeCondition,
+                sexCondition = createVolunteerWebRequest.sexCondition,
+                score = createVolunteerWebRequest.score,
+                optionalScore = createVolunteerWebRequest.optionalScore,
+                maxApplicants = createVolunteerWebRequest.maxApplicants,
+            )
+        )
     }
 }
