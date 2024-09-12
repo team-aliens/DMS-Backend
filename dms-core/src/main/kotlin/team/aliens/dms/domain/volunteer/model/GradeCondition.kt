ac7b9d151361243eae33262774d6f199f1450cb6
khyunwook <khyunwook0707@gmail.com>
2024-09-12 13:49:21 +0900

    feat: (#733) GradeCondition 속성값 추가

--- dms-core/src/main/kotlin/team/aliens/dms/domain/volunteer/model/GradeCondition.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/volunteer/model/GradeCondition.kt
@@ -1,11 +1,13 @@
 package team.aliens.dms.domain.volunteer.model
 
-enum class GradeCondition {
-    ALL,
-    FIRST,
-    SECOND,
-    THIRD,
-    FIRSTANDSECOND,
-    SECONDANDTHIRD,
-    FIRSTANDTHIRD
+enum class GradeCondition(
+    val grades: Set<Int>
+) {
+    ALL(setOf(1, 2, 3)),
+    FIRST(setOf(1)),
+    SECOND(setOf(2)),
+    THIRD(setOf(3)),
+    FIRSTANDSECOND(setOf(1, 2)),
+    SECONDANDTHIRD(setOf(2, 3)),
+    FIRSTANDTHIRD(setOf(1, 3))
 }
