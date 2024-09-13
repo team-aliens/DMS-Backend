f33411584f93cc4d7e8d5a3c6a174a722bc783d5
4mjeo <4mj.eo@dsm.hs.kr>
2024-09-13 10:05:05 +0900

    refactor: (#733) 신청가능 봉사 로직 수정

--- dms-persistence/src/main/kotlin/team/aliens/dms/persistence/volunteer/VolunteerPersistenceAdapter.kt
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/volunteer/VolunteerPersistenceAdapter.kt
@@ -3,6 +3,7 @@ package team.aliens.dms.persistence.volunteer
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import team.aliens.dms.domain.student.exception.StudentNotFoundException
+import team.aliens.dms.domain.student.model.Sex
 import team.aliens.dms.domain.volunteer.model.Volunteer
 import team.aliens.dms.domain.volunteer.spi.VolunteerPort
 import team.aliens.dms.persistence.student.repository.StudentJpaRepository
@@ -40,11 +41,12 @@ class VolunteerPersistenceAdapter(
         val volunteers = volunteerJpaRepository.findAll()
         return volunteers
             .filter { volunteer ->
-                volunteer.gradeCondition.grades.contains(student.grade) && volunteer.sexCondition == student.sex
+                (volunteer.gradeCondition.grades.contains(student.grade) &&
+                    (volunteer.sexCondition == student.sex || volunteer.sexCondition == Sex.ALL))
             }
             .mapNotNull { volunteerMapper.toDomain(it) }
     }
-
+    
     override fun queryAllVolunteersBySchoolId(schoolId: UUID): List<Volunteer> {
         return volunteerJpaRepository.findAllBySchoolId(schoolId)
             .map { volunteerMapper.toDomain(it)!! }
