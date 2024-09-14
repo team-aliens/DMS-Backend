1a323a109dacd15689eadba8b0bf076d37d3980f
khyunwook <khyunwook0707@gmail.com>
2024-09-14 12:54:56 +0900

    chore: (#733) 코멘트 반영

--- dms-core/src/main/kotlin/team/aliens/dms/domain/volunteer/usecase/ManagerGetAllVolunteersUseCase.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/volunteer/usecase/ManagerGetAllVolunteersUseCase.kt
@@ -1,12 +1,12 @@
 package team.aliens.dms.domain.volunteer.usecase
 
-import team.aliens.dms.common.annotation.UseCase
+import team.aliens.dms.common.annotation.ReadOnlyUseCase
 import team.aliens.dms.common.service.security.SecurityService
 import team.aliens.dms.domain.volunteer.dto.response.VolunteerResponse
 import team.aliens.dms.domain.volunteer.dto.response.VolunteersResponse
 import team.aliens.dms.domain.volunteer.service.VolunteerService
 
-@UseCase
+@ReadOnlyUseCase
 class ManagerGetAllVolunteersUseCase(
     private val securityService: SecurityService,
     private val volunteerService: VolunteerService
