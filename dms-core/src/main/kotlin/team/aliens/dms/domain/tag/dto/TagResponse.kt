c47bc986fcb56f7ba2e599a316904ac960e9a70f
4mjeo <4mj.eo@dsm.hs.kr>
2024-09-22 12:36:53 +0900

    refactor: (#763) json 역직렬화 문제

--- dms-core/src/main/kotlin/team/aliens/dms/domain/tag/dto/TagResponse.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/tag/dto/TagResponse.kt
@@ -1,12 +1,15 @@
 package team.aliens.dms.domain.tag.dto
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import team.aliens.dms.domain.tag.model.Tag
 import java.util.UUID
 
-data class TagResponse(
-    val id: UUID,
-    val name: String?,
-    val color: String?
+
+data class TagResponse @JsonCreator constructor(
+    @JsonProperty("id") val id: UUID,
+    @JsonProperty("name") val name: String?,
+    @JsonProperty("color") val color: String?
 ) {
     companion object {
         fun of(tag: Tag): TagResponse {
