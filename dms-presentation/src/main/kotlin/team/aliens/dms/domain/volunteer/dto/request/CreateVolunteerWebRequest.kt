cb1185e8ca914f20ab38c5cbcb57857636212089
khyunwook <khyunwook0707@gmail.com>
2024-09-25 20:25:48 +0900

    fix: (#769) Valid 수정

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/volunteer/dto/request/CreateVolunteerWebRequest.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/volunteer/dto/request/CreateVolunteerWebRequest.kt
@@ -14,18 +14,17 @@ data class CreateVolunteerWebRequest(
     @field:NotBlank
     val content: String,
 
-    @field:Size(min = 1, max = 6)
-    @field:NotBlank
+    @field:NotNull
     val availableSex: Sex,
 
-    @field:Size(min = 1, max = 16)
-    @field:NotBlank
+    @field:NotNull
     val availableGrade: AvailableGrade,
 
     @field:NotNull
     val score: Int,
 
-    val optionalScore: Int = 0,
+    @field:NotNull
+    val optionalScore: Int,
 
     @field:NotNull
     val maxApplicants: Int
