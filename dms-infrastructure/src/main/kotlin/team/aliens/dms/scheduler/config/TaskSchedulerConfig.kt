7249cfef16e8863d5b59058a9480c8931dd1aa06
RyuJeaMin <kariel7673@gmail.com>
2025-02-07 17:18:02 +0900

    feat: (#801) taskSchduler 빈 삭제시 shutdown 작업 추가

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/scheduler/config/TaskSchedulerConfig.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/scheduler/config/TaskSchedulerConfig.kt
@@ -4,17 +4,23 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+import javax.annotation.PreDestroy
 
 @Configuration
 @EnableScheduling
-class TaskShedulerConfig {
+class TaskSchedulerConfig {
 
+        private val scheduler = ThreadPoolTaskScheduler()
     @Bean
     fun taskScheduler(): ThreadPoolTaskScheduler {
-        val scheduler = ThreadPoolTaskScheduler()
-        scheduler.poolSize = 2
-        scheduler.setThreadNamePrefix("TeskSheduler-")
+        scheduler.poolSize = 5
+        scheduler.setThreadNamePrefix("TeskScheduler-")
         scheduler.initialize()
         return scheduler
+    }
+
+    @PreDestroy
+    fun shutdown(){
+        scheduler.shutdown()
     }
 }
--- dms-persistence/src/main/kotlin/team/aliens/dms/global/taskschduler/TaskSchedulerAdepter.kt
+++ dms-persistence/src/main/kotlin/team/aliens/dms/global/taskschduler/TaskSchedulerAdepter.kt
@@ -2,14 +2,14 @@ package team.aliens.dms.global.taskschduler
 
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.stereotype.Component
-import team.aliens.dms.common.spi.TaskSchdulerPort
+import team.aliens.dms.common.spi.TaskSchedulerPort
 import java.time.LocalDateTime
 import java.time.ZoneId
 
 @Component
-class TaskSchdulerAdepter(
+class TaskSchedulerAdepter(
     private val taskScheduler: ThreadPoolTaskScheduler
-): TaskSchdulerPort {
+): TaskSchedulerPort {
 
     override fun schduleTask(task: Runnable, time: LocalDateTime) {
         val InstantTime = time.atZone(ZoneId.systemDefault()).toInstant()
