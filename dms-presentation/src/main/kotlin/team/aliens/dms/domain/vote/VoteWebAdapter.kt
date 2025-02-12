6916cc378831b04f7470845c21fe9dc081eb7aa0
박지민 <pjmlove2008@gmail.com>
2025-02-12 17:32:31 +0900

    fix: (#798) API 경로 수정

--- dms-presentation/src/main/kotlin/team/aliens/dms/domain/vote/VoteWebAdapter.kt
+++ dms-presentation/src/main/kotlin/team/aliens/dms/domain/vote/VoteWebAdapter.kt
@@ -4,24 +4,22 @@ import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import team.aliens.dms.domain.vote.dto.response.ModelStudentListResponse
 import team.aliens.dms.domain.vote.usecase.GetModelStudentsUseCase
 import java.time.LocalDate
 
 @RestController
-@RequestMapping("/vote")
+@RequestMapping("/votes")
 class VoteWebAdapter(
     private val getModelStudentsUseCase: GetModelStudentsUseCase
 ) {
-
-    @GetMapping("/candidate-list/{requestDate}")
+    @GetMapping("/candidate-list/{date}")
     fun getModelStudents(
         @PathVariable
         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-        requestDate: LocalDate
+        date: LocalDate
     ): List<ModelStudentListResponse> {
-        return getModelStudentsUseCase.execute(requestDate)
+        return getModelStudentsUseCase.execute(date)
     }
 }
