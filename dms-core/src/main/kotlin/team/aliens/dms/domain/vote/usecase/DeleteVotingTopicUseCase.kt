6f100015333d70e0ff4e21339abbf74753809ef4
RyuJeaMin <kariel7673@gmail.com>
2025-02-13 19:56:32 +0900

    feat: (#801) votingTopic 삭제시 taskSchduler에 할당된 task 삭제 추가

--- dms-core/src/main/kotlin/team/aliens/dms/domain/vote/usecase/DeleteVotingTopicUseCase.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/vote/usecase/DeleteVotingTopicUseCase.kt
@@ -1,15 +1,18 @@
 package team.aliens.dms.domain.vote.usecase
 
 import team.aliens.dms.common.annotation.UseCase
+import team.aliens.dms.common.spi.TaskSchedulerPort
 import team.aliens.dms.domain.vote.service.CommendVotingTopicService
 import java.util.UUID
 
 @UseCase
 class DeleteVotingTopicUseCase(
-    private val commandVotingTopicService: CommendVotingTopicService
+    private val commandVotingTopicService: CommendVotingTopicService,
+    private val schedulerPort: TaskSchedulerPort
 ) {
 
     fun execute(id: UUID) {
         commandVotingTopicService.deleteVotingTopicById(id)
+        schedulerPort.cancelTask(id)
     }
 }
