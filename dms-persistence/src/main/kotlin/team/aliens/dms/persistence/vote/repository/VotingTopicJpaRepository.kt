7086c8243489860b366750b600d743bcb532dd87
하이 <kkm07133@naver.com>
2025-02-14 16:11:05 +0900

    feat: (#806) JpaRepository 추가

---
+++ dms-persistence/src/main/kotlin/team/aliens/dms/persistence/vote/repository/VotingTopicJpaRepository.kt
@@ -0,0 +1,11 @@
+package team.aliens.dms.persistence.vote.repository
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import team.aliens.dms.persistence.vote.entity.VotingTopicJpaEntity
+import java.util.UUID
+
+@Repository
+interface VotingTopicJpaRepository : CrudRepository<VotingTopicJpaEntity, UUID> {
+    fun existsByTopicName(topicName: String): Boolean
+}
