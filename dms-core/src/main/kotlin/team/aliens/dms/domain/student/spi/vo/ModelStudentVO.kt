8eea190b7559d0e58acb62adfb22e867ddc3a345
박지민 <pjmlove2008@gmail.com>
2025-02-17 19:43:36 +0900

    refactor: (#798) spi.vo 디렉토리로 이동

--- dms-core/src/main/kotlin/team/aliens/dms/domain/student/spi/vo/ModelStudentVO.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/student/spi/vo/ModelStudentVO.kt
@@ -1,8 +1,8 @@
-package team.aliens.dms.domain.vote.model
+package team.aliens.dms.domain.student.spi.vo
 
 import java.util.UUID
 
-data class ModelStudent(
+data class ModelStudentVO(
     val id: UUID,
     val studentGcn: String,
     val studentName: String,
