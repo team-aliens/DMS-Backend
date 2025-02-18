eeeb5f44f4a7a8219bc1c08606d1ddd24b6f7092
RyuJeaMin <kariel7673@gmail.com>
2025-02-18 23:46:08 +0900

    refactor: (#801) votingTopic 모델로 period 검증로직 옮김

--- dms-core/src/main/kotlin/team/aliens/dms/domain/vote/model/VotingTopic.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/vote/model/VotingTopic.kt
@@ -1,6 +1,7 @@
 package team.aliens.dms.domain.vote.model
 
 import team.aliens.dms.common.annotation.Aggregate
+import team.aliens.dms.domain.vote.exception.NotVotingPeriodException
 import java.time.LocalDateTime
 import java.util.UUID
 
@@ -19,4 +20,12 @@ data class VotingTopic(
     val voteType: VoteType,
 
     val managerId: UUID,
-)
+){
+
+    fun checkVotingPeriod(voteTopicId: UUID) {
+        val currentTime = LocalDateTime.now()
+
+        if (!(currentTime.isAfter(startTime) && currentTime.isBefore(endTime)))
+            throw NotVotingPeriodException
+    }
+}
--- dms-core/src/main/kotlin/team/aliens/dms/domain/vote/service/ValidVotePeriodService.kt
+++
@@ -1,8 +0,0 @@
-package team.aliens.dms.domain.vote.service
-
-import java.util.UUID
-
-interface ValidVotePeriodService {
-
-    fun execute(voteTopicId: UUID)
-}
