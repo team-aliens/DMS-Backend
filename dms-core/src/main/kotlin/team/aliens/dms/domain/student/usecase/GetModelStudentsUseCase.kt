e8f0add67232182b8aa64d9d1b1bb1c8410fd35c
박지민 <pjmlove2008@gmail.com>
2025-02-19 19:54:14 +0900

    refactor: (#798) StudentService 주입으로 변경

--- dms-core/src/main/kotlin/team/aliens/dms/domain/student/usecase/GetModelStudentsUseCase.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/student/usecase/GetModelStudentsUseCase.kt
@@ -1,17 +1,16 @@
 package team.aliens.dms.domain.student.usecase
 
-import org.springframework.beans.factory.annotation.Qualifier
 import team.aliens.dms.common.annotation.ReadOnlyUseCase
-import team.aliens.dms.domain.student.service.GetStudentService
+import team.aliens.dms.domain.student.service.StudentService
 import team.aliens.dms.domain.vote.dto.response.ModelStudentResponse
 import java.time.LocalDate
 
 @ReadOnlyUseCase
 class GetModelStudentsUseCase(
-    @Qualifier("getStudentServiceImpl") private val getStudentService: GetStudentService
+    private val StudentService: StudentService
 ) {
     fun execute(date: LocalDate): List<ModelStudentResponse> {
-        return getStudentService.getModelStudentList(date)
+        return StudentService.getModelStudentList(date)
     }
 
 }
