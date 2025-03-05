b3d82e1dd03f719884a6f833cb650e537e81cd39
tae.y <0211ilyoil@gmail.com>
2025-03-05 18:30:28 +0900

    fix: (#dev) flyway 수정

--- dms-infrastructure/src/main/resources/db/migration/V12__create_tbl_vote.sql
+++ dms-infrastructure/src/main/resources/db/migration/V12__create_tbl_vote.sql
@@ -12,13 +12,3 @@ CREATE TABLE tbl_vote
     FOREIGN KEY (selected_student_id) REFERENCES tbl_student (id),
     FOREIGN KEY (selected_option_id) REFERENCES tbl_voting_option (id)
 );
-
-create table tbl_voting_option
-(
-    created_at      datetime(6)  not null,
-    id              binary(16)   not null
-        primary key,
-    option_name     varchar(255) null,
-    voting_topic_id binary(16)   not null,
-    foreign key (voting_topic_id) references tbl_voting_topic (id)
-);
