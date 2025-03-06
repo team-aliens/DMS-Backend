18cfe88f0dfe8ad38a6f0e19a40224d9f17448bf
김경민 <kkm07133@naver.com>
2025-03-06 20:25:29 +0900

    refactor: (#808) Return값 void로 변경

--- dms-core/src/main/kotlin/team/aliens/dms/domain/vote/usecase/CreateVotingOptionUseCase.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/vote/usecase/CreateVotingOptionUseCase.kt
@@ -6,19 +6,18 @@ import team.aliens.dms.domain.vote.exception.VoteTypeMismatchException
 import team.aliens.dms.domain.vote.model.VoteType
 import team.aliens.dms.domain.vote.model.VotingOption
 import team.aliens.dms.domain.vote.service.VoteService
-import java.util.UUID
 
 @UseCase
 class CreateVotingOptionUseCase(
     private val voteService: VoteService
 ) {
 
-    fun execute(request: CreateVotingOptionRequest): UUID {
-        val votingTopic = voteService.getVotingTopic(request.votingTopicId)
-        if (votingTopic!!.voteType != VoteType.OPTION_VOTE) {
+    fun execute(request: CreateVotingOptionRequest) {
+        val votingTopic = voteService.getVotingTopicById(request.votingTopicId)
+        if (votingTopic.voteType != VoteType.OPTION_VOTE) {
             throw VoteTypeMismatchException
         }
-        return voteService.createVotingOption(
+        voteService.createVotingOption(
             VotingOption(
                 votingTopicId = votingTopic.id,
                 optionName = request.name
