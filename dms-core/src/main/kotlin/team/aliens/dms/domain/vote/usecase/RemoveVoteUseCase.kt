88d1bc87db01e10ec57d6b922f70ce7a3809cd32
김경민 <kkm07133@naver.com>
2025-03-07 15:51:32 +0900

    feat: (#812) 투표 삭제 api 추가

---
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/vote/usecase/RemoveVoteUseCase.kt
@@ -0,0 +1,16 @@
+package team.aliens.dms.domain.vote.usecase
+
+import team.aliens.dms.common.annotation.UseCase
+import team.aliens.dms.domain.vote.model.Vote
+import team.aliens.dms.domain.vote.service.VoteService
+import java.util.UUID
+@UseCase
+class RemoveVoteUseCase(
+    private val voteService: VoteService
+) {
+
+    fun execute(voteId: UUID) {
+        val vote: Vote = voteService.getVoteById(voteId)
+        voteService.deleteVote(vote.id)
+    }
+}
