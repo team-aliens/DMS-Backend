5828ee82a7cdba275a7370de85945078bfef0cc1
김경민 <kkm07133@naver.com>
2025-04-08 17:08:46 +0900

    revert: (#860) flyway V14, V15 삭제

--- dms-infrastructure/src/main/resources/db/migration/V14__drop_foreign_key.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE tbl_voting_option
-DROP FOREIGN KEY tbl_voting_option_ibfk_1;
-ALTER TABLE tbl_vote
-DROP FOREIGN KEY tbl_vote_ibfk_1,
-    DROP FOREIGN KEY tbl_vote_ibfk_2,
-    DROP FOREIGN KEY tbl_vote_ibfk_3,
-    DROP FOREIGN KEY tbl_vote_ibfk_4
