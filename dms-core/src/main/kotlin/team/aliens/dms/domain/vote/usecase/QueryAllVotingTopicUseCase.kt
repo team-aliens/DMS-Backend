adba2a86e88e2215efe50e7e70e966a5645686fb
김경민 <kkm07133@naver.com>
2025-06-09 23:38:48 +0900

    fix: (#882) usecase 변경 누락 수정

--- dms-core/src/main/kotlin/team/aliens/dms/domain/vote/usecase/QueryAllVotingTopicUseCase.kt
+++ dms-core/src/main/kotlin/team/aliens/dms/domain/vote/usecase/QueryAllVotingTopicUseCase.kt
@@ -1,17 +1,21 @@
 package team.aliens.dms.domain.vote.usecase
 
 import team.aliens.dms.common.annotation.ReadOnlyUseCase
+import team.aliens.dms.domain.user.model.User
+import team.aliens.dms.domain.user.service.UserService
 import team.aliens.dms.domain.vote.dto.response.VotingTopicResponse
 import team.aliens.dms.domain.vote.dto.response.VotingTopicsResponse
 import team.aliens.dms.domain.vote.service.VoteService
 
 @ReadOnlyUseCase
 class QueryAllVotingTopicUseCase(
-    val voteService: VoteService
+    val voteService: VoteService,
+    val userService: UserService
 ) {
 
     fun execute(): VotingTopicsResponse {
-        val voteTopic = voteService.getAllVotingTopics()
+        val user: User = userService.getCurrentUser()
+        val voteTopic = voteService.getAllVotingTopics(user.id)
 
         return VotingTopicsResponse(
             voteTopic.map {
