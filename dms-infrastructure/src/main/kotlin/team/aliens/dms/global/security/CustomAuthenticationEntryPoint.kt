e1738334f764145c16922dc8d44d39a3c6e56ad7
박지민 <pjmlove2008@gmail.com>
2025-07-14 00:43:29 +0900

    fix: (#893) text/html 반환 오류  JSON 형식으로 응답 반환 수정

--- dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/CustomAuthenticationEntryPoint.kt
+++ dms-infrastructure/src/main/kotlin/team/aliens/dms/global/security/CustomAuthenticationEntryPoint.kt
@@ -1,20 +1,38 @@
 package team.aliens.dms.global.security
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
-import team.aliens.dms.global.security.exception.ForbiddenException
+import team.aliens.dms.global.security.exception.error.SecurityErrorCode
 
 @Component
-class CustomAuthenticationEntryPoint : AuthenticationEntryPoint {
+class CustomAuthenticationEntryPoint(
+    private val objectMapper: ObjectMapper
+) : AuthenticationEntryPoint {
 
     override fun commence(
         request: HttpServletRequest?,
         response: HttpServletResponse?,
         authException: AuthenticationException?
     ) {
-        throw ForbiddenException
+
+        val errorCode = SecurityErrorCode.FORBIDDEN
+
+        response?.apply {
+            contentType = "application/json"
+            characterEncoding = "UTF-8"
+            status = errorCode.status()
+
+        val errorResponse = mapOf(
+            "status" to errorCode.status(),
+            "message" to errorCode.message(),
+            "code" to errorCode.code()
+        )
+
+        writer.write(objectMapper.writeValueAsString(errorResponse))
+        }
     }
 }
