364cbb330ad58796a5884cf54e67679f78d3de12
RyuJeaMin <kariel7673@gmail.com>
2025-08-17 22:45:17 +0900

    setting: (#899) 현재 서버 모듈만 include

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,7 @@
 rootProject.name = "DMS-Backend"
 
+include("dms-main")
 include("dms-main:main-core")
 include("dms-main:main-persistence")
 include("dms-main:main-infrastructure")
 include("dms-main:main-presentation")
-
-include("dms-main")
-include("dms-notificatoin")
-include("dms-gateway")
