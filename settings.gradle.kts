1277bc6dd507cf19198401f3035a5a2b0c5d234f
RyuJeaMin <kariel7673@gmail.com>
2025-08-17 22:27:34 +0900

    setting: (#899) 각 서버 모듈 include

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,7 @@ include("dms-main:main-core")
 include("dms-main:main-persistence")
 include("dms-main:main-infrastructure")
 include("dms-main:main-presentation")
+
+include("dms-main")
+include("dms-notificatoin")
+include("dms-gateway")
