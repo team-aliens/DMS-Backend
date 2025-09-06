4127ffea255a8d37712acdbff522658353fda76f
RyuJeaMin <kariel7673@gmail.com>
2025-09-06 22:06:50 +0900

    refactor: (#898) eventHandler 수정

--- dms-main/main-infrastructure/src/main/kotlin/team/aliens/dms/event/EventHandler.kt
+++ dms-main/main-infrastructure/src/main/kotlin/team/aliens/dms/event/EventHandler.kt
@@ -1,5 +1,6 @@
 package team.aliens.dms.event
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
@@ -7,11 +8,10 @@ import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 import team.aliens.dms.GroupNotificationInfo
 import team.aliens.dms.SingleNotificationInfo
-import team.aliens.dms.thirdparty.rabbitmq.port.RabbitMqPort
 
 @Component
 class NotificationEventHandler(
-    private val rabbitMqPort: RabbitMqPort
+    private val rabbitTemplate: RabbitTemplate
 ) {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
@@ -20,7 +20,7 @@ class NotificationEventHandler(
         event.run {
             when (this) {
                 is SingleNotificationEvent -> {
-                    rabbitMqPort.sendMessage("exchange","notification.single",
+                    rabbitTemplate.convertAndSend("exchange","notification.single",
                         SingleNotificationInfo(
                             userId = userId,
                             notificationInfo = this.notificationInfo
@@ -29,7 +29,7 @@ class NotificationEventHandler(
                 }
 
                 is GroupNotificationEvent -> {
-                    rabbitMqPort.sendMessage("exchange","notification.group",
+                    rabbitTemplate.convertAndSend("exchange","notification.group",
                         GroupNotificationInfo(
                             userIds = userIds,
                             notificationInfo = this.notificationInfo
@@ -38,7 +38,7 @@ class NotificationEventHandler(
                 }
 
                 else -> {
-                    rabbitMqPort.sendMessage("exchange","notification.topic",
+                    rabbitTemplate.convertAndSend("exchange","notification.topic",
                         this.notificationInfo
                     )
                 }
--- dms-main/main-infrastructure/src/main/kotlin/team/aliens/dms/thirdparty/rabbitmq/port/RabbitMqPort.kt
+++
@@ -1,5 +0,0 @@
-package team.aliens.dms.thirdparty.rabbitmq.port
-
-interface RabbitMqPort {
-    fun sendMessage(exchangeName: String, routingKey: String, message: Any)
-}
