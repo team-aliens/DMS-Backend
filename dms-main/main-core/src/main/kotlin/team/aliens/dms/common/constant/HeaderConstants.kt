10f38e40d2a6068dddc51aa6109777569abeac6d
박지민 <pjmlove2008@gmail.com>
2025-09-11 13:11:09 +0900

    chore: (#895) 사용되지 않는 클래스 제거

--- dms-main/main-core/src/main/kotlin/team/aliens/dms/common/constant/HeaderConstants.kt
+++
@@ -1,7 +0,0 @@
-package team.aliens.dms.common.constant
-
-object HeaderConstants {
-    const val USER_ID = "X-User-Id"
-    const val USER_AUTHORITY = "X-User-Authority"
-    const val SCHOOL_ID = "X-School-Id"
-}
