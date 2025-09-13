ffba9a2cc750d18e62585feb8c7b60febc75016e
RyuJeaMin <kariel7673@gmail.com>
2025-09-13 16:49:08 +0900

    refactor: (#898) passport 필터 적용

--- dms-notification/notification-infrastructure/src/main/kotlin/team/aliens/dms/global/config/FilterConfig.kt
+++ dms-notification/notification-infrastructure/src/main/kotlin/team/aliens/dms/global/config/FilterConfig.kt
@@ -1,4 +1,4 @@
-package team.aliens.dms.global.config
+package team.aliens.dms.global.filter
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.security.config.annotation.SecurityConfigurer
@@ -6,19 +6,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.stereotype.Component
-import team.aliens.dms.global.filter.ExceptionFilter
-import team.aliens.dms.global.filter.JwtFilter
-import team.aliens.dms.global.security.token.JwtParser
+import team.aliens.dms.global.security.passport.PassportValidator
+import team.aliens.dms.thirdparty.slack.SlackAdapter
 
 @Component
 class FilterConfig(
-    private val jwtParser: JwtParser,
-    private val objectMapper: ObjectMapper
+    private val passportValidator: PassportValidator,
+    private val objectMapper: ObjectMapper,
+    private val slackAdapter: SlackAdapter
 ) : SecurityConfigurer<DefaultSecurityFilterChain, HttpSecurity> {
     override fun init(builder: HttpSecurity?) {}
 
     override fun configure(builder: HttpSecurity) {
-        builder.addFilterBefore(JwtFilter(jwtParser), UsernamePasswordAuthenticationFilter::class.java)
-        builder.addFilterBefore(ExceptionFilter(objectMapper), JwtFilter::class.java)
+        builder.addFilterBefore(PassportFilter(passportValidator, objectMapper), UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(ExceptionFilter(objectMapper, slackAdapter), PassportFilter::class.java)
     }
 }
