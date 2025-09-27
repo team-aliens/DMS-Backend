5d16b475c8fbfd728ff8fa5ffd126cdd3401e55e
RyuJeaMin <kariel7673@gmail.com>
2025-09-27 15:05:53 +0900

    init: (#895) health check 용 rest webAdapter 추가

---
+++ dms-notification/notification-presentation/src/main/kotlin/team/aliens/dms/domain/HealthCheckWebAdapter.kt
@@ -0,0 +1,11 @@
+package team.aliens.dms.domain
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckWebAdapter {
+
+    @GetMapping
+    fun healthCheck() = "OK"
+}
