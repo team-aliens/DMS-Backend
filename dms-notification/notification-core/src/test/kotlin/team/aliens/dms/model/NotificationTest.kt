3e5c55e44ccf4bffc4e6845c466faafda371470f
uyjeamin <kariel7673@gmail.com>
2025-11-06 19:30:12 +0900

    test: (#933) Notification 도메인 모델 테스트 추가

--- dms-notification/notification-core/src/test/kotlin/team/aliens/dms/model/NotificationTest.kt
+++ dms-notification/notification-core/src/test/kotlin/team/aliens/dms/model/NotificationTest.kt
@@ -1,4 +1,36 @@
 package team.aliens.dms.model
 
-class NotificationTest {
-}
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import team.aliens.dms.stub.createNotificationStub
+
+class NotificationTest : DescribeSpec({
+        describe("IsSaveRequiredTest") {
+
+            var called = false
+
+            context("저장 요청이 true 라면") {
+                val notification = createNotificationStub(isSaveRequired = true)
+
+                it("함수를 실행시킨다") {
+                    notification.runIfSaveRequired {
+                        called = true
+                    }
+                    called shouldBe true
+                }
+            }
+
+            context("저장 요청이 false 라면") {
+                val notification = createNotificationStub(isSaveRequired = false)
+                called = false
+
+                it("함수를 실행시키지 않는다.") {
+                    notification.runIfSaveRequired {
+                        called = true
+                    }
+                    called shouldBe false
+                }
+            }
+        }
+    }
+)
