fdb94ce4d7faaff189c16ff6c2667f6934aeceee
uyjeamin <kariel7673@gmail.com>
2025-11-06 19:30:12 +0900

    test: (#933) Notification 도메인 모델 테스트 추가

--- dms-notification/notification-core/src/test/kotlin/team/aliens/dms/stub/NotificationStub.kt
+++ dms-notification/notification-core/src/test/kotlin/team/aliens/dms/stub/NotificationStub.kt
@@ -1,4 +1,26 @@
 package team.aliens.dms.stub
 
-class NotificationStub {
-}
+import team.aliens.dms.contract.model.notification.Topic
+import team.aliens.dms.domain.notification.model.Notification
+import java.time.LocalDateTime
+import java.util.UUID
+
+internal fun createNotificationStub(
+    schoolId: UUID = UUID.randomUUID(),
+    topic: Topic = Topic.NOTICE,
+    linkIdentifier: String? = null,
+    title: String = "Test Notification Title",
+    content: String = "Test notification content",
+    threadId: String = UUID.randomUUID().toString(),
+    createdAt: LocalDateTime = LocalDateTime.now(),
+    isSaveRequired: Boolean = true
+) = Notification(
+    schoolId = schoolId,
+    topic = topic,
+    linkIdentifier = linkIdentifier,
+    title = title,
+    content = content,
+    threadId = threadId,
+    createdAt = createdAt,
+    isSaveRequired = isSaveRequired
+)
