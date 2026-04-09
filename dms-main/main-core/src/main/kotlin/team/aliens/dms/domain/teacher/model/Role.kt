7e739ae81cb37bed1964c23ec5d8ddd6d2a0789d
yellowstarr0323 <ihansaem60@gmail.com>
2026-04-09 11:43:56 +0900

    feat: (#982) teacher 도메인 생성

---
+++ dms-main/main-core/src/main/kotlin/team/aliens/dms/domain/teacher/model/Role.kt
@@ -0,0 +1,6 @@
+package team.aliens.dms.domain.teacher.model
+
+enum class Role(korean: String) {
+    GENERAL_TEACHER("일반 선생님"),
+    HEAD_TEACHER("부장 선생님")
+}
---
+++ dms-main/main-core/src/main/kotlin/team/aliens/dms/domain/teacher/model/Teacher.kt
@@ -0,0 +1,10 @@
+package team.aliens.dms.domain.teacher.model
+
+import team.aliens.dms.common.annotation.Aggregate
+import java.util.UUID
+
+@Aggregate
+data class Teacher(
+    val id: UUID,
+    val name: String,
+)
