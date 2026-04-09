dca59599dc2ddef831703652e7b7e2274188a9c9
yellowstarr0323 <ihansaem60@gmail.com>
2026-04-09 18:47:29 +0900

    feat: (#982) 영속성 모델 생성

---
+++ dms-main/main-persistence/src/main/kotlin/team/aliens/dms/persistence/daybreak/entity/DaybreakStudyTypeJpaEntity.kt
@@ -0,0 +1,26 @@
+package team.aliens.dms.persistence.daybreak.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import team.aliens.dms.persistence.BaseUUIDEntity
+import team.aliens.dms.persistence.school.entity.SchoolJpaEntity
+import java.util.UUID
+
+@Entity
+@Table(name = "tbl_daybreak_study_type")
+class DaybreakStudyTypeJpaEntity(
+
+    id: UUID?,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "school_id", nullable = false)
+    val schoolJpaEntity: SchoolJpaEntity,
+
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false)
+    val name: String
+
+): BaseUUIDEntity(id)
---
+++ dms-main/main-persistence/src/main/kotlin/team/aliens/dms/persistence/teacher/entity/TeacherJpaEntity.kt
@@ -0,0 +1,38 @@
+package team.aliens.dms.persistence.teacher.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.MapsId
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+import jakarta.persistence.Id
+import team.aliens.dms.domain.teacher.model.Role
+import team.aliens.dms.persistence.user.entity.UserJpaEntity
+import java.util.UUID
+
+@Table(name = "tbl_teacher")
+@Entity
+class TeacherJpaEntity(
+
+    @Id
+    @Column(name = "user_id")
+    val id: UUID,
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
+    val user: UserJpaEntity,
+
+    @Column(columnDefinition = "VARCHAR(10)", nullable = false)
+    val name: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false)
+    val role: Role
+)
