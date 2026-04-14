95fc2a20ff5696c4bc1eec51777dba349cbc49d2
yellowstarr0323 <ihansaem60@gmail.com>
2026-04-14 15:06:28 +0900

    fix: (#982) mapper에 studyTypeId로 변경된 컬럼 적용

--- dms-main/main-persistence/src/main/kotlin/team/aliens/dms/persistence/daybreak/mapper/DaybreakStudyApplicationMapper.kt
+++ dms-main/main-persistence/src/main/kotlin/team/aliens/dms/persistence/daybreak/mapper/DaybreakStudyApplicationMapper.kt
@@ -1,6 +1,7 @@
 package team.aliens.dms.persistence.daybreak.mapper
 
 import jakarta.persistence.EntityManager
+import org.springframework.stereotype.Component
 import team.aliens.dms.domain.daybreak.model.DaybreakStudyApplication
 import team.aliens.dms.persistence.GenericMapper
 import team.aliens.dms.persistence.daybreak.entity.DaybreakStudyApplicationJpaEntity
@@ -9,16 +10,17 @@ import team.aliens.dms.persistence.school.entity.SchoolJpaEntity
 import team.aliens.dms.persistence.student.entity.StudentJpaEntity
 import team.aliens.dms.persistence.teacher.entity.TeacherJpaEntity
 
+@Component
 class DaybreakStudyApplicationMapper(
     private val entityManager: EntityManager,
     private val daybreakStudyTypeMapper: DaybreakStudyTypeMapper
-): GenericMapper<DaybreakStudyApplication, DaybreakStudyApplicationJpaEntity>{
+) : GenericMapper<DaybreakStudyApplication, DaybreakStudyApplicationJpaEntity> {
 
     override fun toDomain(entity: DaybreakStudyApplicationJpaEntity?): DaybreakStudyApplication? {
         return entity?.let {
             DaybreakStudyApplication(
                 id = it.id!!,
-                studyType = daybreakStudyTypeMapper.toDomain(it.daybreakStudyTypeJpaEntity)!!,
+                studyTypeId = daybreakStudyTypeMapper.toDomain(it.daybreakStudyTypeJpaEntity)!!.id,
                 startDate = it.startDate,
                 endDate = it.endDate,
                 reason = it.reason,
@@ -37,7 +39,7 @@ class DaybreakStudyApplicationMapper(
             endDate = domain.endDate,
             reason = domain.reason,
             status = domain.status,
-            daybreakStudyTypeJpaEntity = entityManager.getReference(DaybreakStudyTypeJpaEntity::class.java, domain.studyType.id),
+            daybreakStudyTypeJpaEntity = entityManager.getReference(DaybreakStudyTypeJpaEntity::class.java, domain.studyTypeId),
             studentJpaEntity = entityManager.getReference(StudentJpaEntity::class.java, domain.studentId),
             teacherJpaEntity = entityManager.getReference(TeacherJpaEntity::class.java, domain.teacherId),
             schoolJpaEntity = entityManager.getReference(SchoolJpaEntity::class.java, domain.schoolId)
--- dms-main/main-persistence/src/main/kotlin/team/aliens/dms/persistence/daybreak/mapper/DaybreakStudyTypeMapper.kt
+++ dms-main/main-persistence/src/main/kotlin/team/aliens/dms/persistence/daybreak/mapper/DaybreakStudyTypeMapper.kt
@@ -1,14 +1,16 @@
 package team.aliens.dms.persistence.daybreak.mapper
 
 import jakarta.persistence.EntityManager
+import org.springframework.stereotype.Component
 import team.aliens.dms.domain.daybreak.model.DaybreakStudyType
 import team.aliens.dms.persistence.GenericMapper
 import team.aliens.dms.persistence.daybreak.entity.DaybreakStudyTypeJpaEntity
 import team.aliens.dms.persistence.school.entity.SchoolJpaEntity
 
+@Component
 class DaybreakStudyTypeMapper(
     private val entityManager: EntityManager
-): GenericMapper<DaybreakStudyType, DaybreakStudyTypeJpaEntity>{
+) : GenericMapper<DaybreakStudyType, DaybreakStudyTypeJpaEntity> {
 
     override fun toDomain(entity: DaybreakStudyTypeJpaEntity?): DaybreakStudyType? {
 
@@ -22,7 +24,6 @@ class DaybreakStudyTypeMapper(
     }
 
     override fun toEntity(domain: DaybreakStudyType): DaybreakStudyTypeJpaEntity {
-
 
         return DaybreakStudyTypeJpaEntity(
             id = domain.id,
