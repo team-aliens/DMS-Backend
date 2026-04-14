ca145e9ab4fa7d1947d4308dc64ef21f9690c611
yellowstarr0323 <ihansaem60@gmail.com>
2026-04-14 22:10:40 +0900

    fix: (#982) flyway V18 수정

--- dms-main/main-infrastructure/src/main/resources/db/migration/V18__create_tbl_teacher.sql
+++ dms-main/main-infrastructure/src/main/resources/db/migration/V18__create_tbl_teacher.sql
@@ -1,6 +1,8 @@
 CREATE TABLE tbl_teacher (
     user_id     BINARY(16)   NOT NULL primary key,
     name        VARCHAR(10)  NOT NULL,
-    role        VARCHAR(20)  NOT NULL,
     FOREIGN KEY (user_id) REFERENCES tbl_user (id)
 );
+
+ALTER TABLE tbl_user
+MODIFY authority VARCHAR(20) NOT NULL;
