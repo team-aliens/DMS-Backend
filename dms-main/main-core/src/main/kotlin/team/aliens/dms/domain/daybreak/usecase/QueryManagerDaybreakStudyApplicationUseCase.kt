283f1e798800422fa26f413c11fd2f1d1f1111b5
yellowstarr0323 <ihansaem60@gmail.com>
2026-04-21 12:06:34 +0900

    fix: (#987) UseCase -> ReadOnlyUseCase 어노테이션으로 수정

--- dms-main/main-core/src/main/kotlin/team/aliens/dms/domain/daybreak/usecase/QueryManagerDaybreakStudyApplicationUseCase.kt
+++ dms-main/main-core/src/main/kotlin/team/aliens/dms/domain/daybreak/usecase/QueryManagerDaybreakStudyApplicationUseCase.kt
@@ -1,11 +1,11 @@
 package team.aliens.dms.domain.daybreak.usecase
 
-import team.aliens.dms.common.annotation.UseCase
+import team.aliens.dms.common.annotation.ReadOnlyUseCase
 import team.aliens.dms.common.dto.PageData
 import team.aliens.dms.domain.daybreak.dto.response.DaybreakStudyApplicationResponse
 import team.aliens.dms.domain.daybreak.service.DaybreakService
 
-@UseCase
+@ReadOnlyUseCase
 class QueryManagerDaybreakStudyApplicationUseCase(
     private val daybreakService: DaybreakService,
 ) {
