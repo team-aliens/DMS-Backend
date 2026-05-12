df96cc9b2293169a9cf21c703cb9abcf7462e680
ryujaemin <kariel7673@gmail.com>
2026-05-12 14:42:18 +0900

    refactor: (#944) 비즈니스 로직 처리 후, ack 하도록 변경

--- dms-notification/notification-infrastructure/src/main/kotlin/team/aliens/dms/thirdparty/messagebroker/DeviceTokenConsumer.kt
+++ dms-notification/notification-infrastructure/src/main/kotlin/team/aliens/dms/thirdparty/messagebroker/DeviceTokenConsumer.kt
@@ -1,6 +1,9 @@
 package team.aliens.dms.thirdparty.messagebroker
 
+import com.rabbitmq.client.Channel
 import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.amqp.support.AmqpHeaders
+import org.springframework.messaging.handler.annotation.Header
 import org.springframework.stereotype.Component
 import team.aliens.dms.contract.remote.rabbitmq.DeleteDeviceTokenMessage
 import team.aliens.dms.contract.remote.rabbitmq.DeviceTokenMessage
@@ -14,16 +17,29 @@ class DeviceTokenConsumer(
 ) {
 
     @RabbitListener(queues = ["device_token_queue"])
-    fun handleDeviceTokenMessage(message: DeviceTokenMessage) {
-        when (message) {
-            is SaveDeviceTokenMessage -> {
-                notificationService.saveDeviceToken(
-                    DeviceToken.from(message.deviceTokenInfo)
-                )
+    fun handleDeviceTokenMessage(
+        message: DeviceTokenMessage,
+        channel: Channel,
+        @Header(AmqpHeaders.DELIVERY_TAG) deliveryTag: Long
+    ) {
+        try {
+            when (message) {
+                is SaveDeviceTokenMessage -> {
+                    notificationService.saveDeviceToken(
+                        DeviceToken.from(message.deviceTokenInfo)
+                    )
+                }
+
+                is DeleteDeviceTokenMessage -> {
+                    notificationService.deleteDeviceTokenByUserId(message.userId)
+                }
             }
-            is DeleteDeviceTokenMessage -> {
-                notificationService.deleteDeviceTokenByUserId(message.userId)
-            }
+
+            channel.basicAck(deliveryTag, false)
+
+        } catch (e: Exception) {
+            channel.basicNack(deliveryTag, false, true)
+            throw e
         }
     }
 }
