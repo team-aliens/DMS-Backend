cdcb238f7953a32f99d904f6ccbf036c211010e9
hansaem <ihansaem60@gmail.com>
2026-06-04 09:29:26 +0900

    fix: (#992) UseCase어노테이션 대신 SchedulerUseCase 사용

--- dms-main/main-core/src/main/kotlin/team/aliens/dms/domain/daybreak/usecase/ExpireDaybreakStudyApplicationUseCase.kt
+++ dms-main/main-core/src/main/kotlin/team/aliens/dms/domain/daybreak/usecase/ExpireDaybreakStudyApplicationUseCase.kt
@@ -1,10 +1,10 @@
 package team.aliens.dms.domain.daybreak.usecase
 
-import team.aliens.dms.common.annotation.UseCase
+import team.aliens.dms.common.annotation.SchedulerUseCase
 import team.aliens.dms.domain.daybreak.model.Status
 import team.aliens.dms.domain.daybreak.service.DaybreakService
 
-@UseCase
+@SchedulerUseCase
 class ExpireDaybreakStudyApplicationUseCase(
     private val daybreakService: DaybreakService
 ) {
