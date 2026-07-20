58e688c60ea38d05faeb4159b3178136975b7e97
hansaem <ihansaem60@gmail.com>
2026-07-20 23:16:34 +0900

    chore: (#1030) 삭제된 도메인 엑셀 출력 삭제

--- dms-main/main-core/src/main/kotlin/team/aliens/dms/domain/file/service/WriteFileService.kt
+++ dms-main/main-core/src/main/kotlin/team/aliens/dms/domain/file/service/WriteFileService.kt
@@ -2,13 +2,9 @@ package team.aliens.dms.domain.file.service
 
 import team.aliens.dms.domain.daybreak.spi.vo.DaybreakStudyApplicationVO
 import team.aliens.dms.domain.manager.spi.vo.StudentWithTag
-import team.aliens.dms.domain.outing.spi.vo.OutingApplicationVO
 import team.aliens.dms.domain.point.model.PointHistory
 import team.aliens.dms.domain.remain.dto.StudentRemainInfo
 import team.aliens.dms.domain.student.model.Student
-import team.aliens.dms.domain.studyroom.model.TimeSlot
-import team.aliens.dms.domain.studyroom.spi.vo.StudentSeatInfo
-import java.io.File
 
 interface WriteFileService {
 
@@ -19,19 +15,6 @@ interface WriteFileService {
     fun writePointHistoriesExcelFile(pointHistories: List<PointHistory>, students: List<StudentWithTag>): ByteArray
 
     fun writeRemainStatusExcelFile(studentRemainInfos: List<StudentRemainInfo>): ByteArray
-
-    fun addStudyRoomApplicationStatusExcelFile(
-        baseFile: File,
-        timeSlots: List<TimeSlot>,
-        studentSeatsMap: Map<Pair<String, String>, StudentSeatInfo>
-    ): ByteArray
-
-    fun writeStudyRoomApplicationStatusExcelFile(
-        timeSlots: List<TimeSlot>,
-        studentSeats: List<StudentSeatInfo>
-    ): ByteArray
-
-    fun writeOutingApplicationExcelFile(outingApplicationExcelVos: List<OutingApplicationVO>): ByteArray
 
     fun writeDaybreakStudyApplicationExcelFile(applications: List<DaybreakStudyApplicationVO>): ByteArray
 }
--- dms-main/main-core/src/main/kotlin/team/aliens/dms/domain/file/service/WriteFileServiceImpl.kt
+++ dms-main/main-core/src/main/kotlin/team/aliens/dms/domain/file/service/WriteFileServiceImpl.kt
@@ -4,13 +4,9 @@ import team.aliens.dms.common.annotation.Service
 import team.aliens.dms.domain.daybreak.spi.vo.DaybreakStudyApplicationVO
 import team.aliens.dms.domain.file.spi.WriteFilePort
 import team.aliens.dms.domain.manager.spi.vo.StudentWithTag
-import team.aliens.dms.domain.outing.spi.vo.OutingApplicationVO
 import team.aliens.dms.domain.point.model.PointHistory
 import team.aliens.dms.domain.remain.dto.StudentRemainInfo
 import team.aliens.dms.domain.student.model.Student
-import team.aliens.dms.domain.studyroom.model.TimeSlot
-import team.aliens.dms.domain.studyroom.spi.vo.StudentSeatInfo
-import java.io.File
 
 @Service
 class WriteFileServiceImpl(
@@ -28,20 +24,6 @@ class WriteFileServiceImpl(
 
     override fun writeRemainStatusExcelFile(studentRemainInfos: List<StudentRemainInfo>) =
         writeFilePort.writeRemainStatusExcelFile(studentRemainInfos)
-
-    override fun addStudyRoomApplicationStatusExcelFile(
-        baseFile: File,
-        timeSlots: List<TimeSlot>,
-        studentSeatsMap: Map<Pair<String, String>, StudentSeatInfo>
-    ) = writeFilePort.addStudyRoomApplicationStatusExcelFile(baseFile, timeSlots, studentSeatsMap)
-
-    override fun writeStudyRoomApplicationStatusExcelFile(
-        timeSlots: List<TimeSlot>,
-        studentSeats: List<StudentSeatInfo>
-    ) = writeFilePort.writeStudyRoomApplicationStatusExcelFile(timeSlots, studentSeats)
-
-    override fun writeOutingApplicationExcelFile(outingApplicationExcelVos: List<OutingApplicationVO>) =
-        writeFilePort.writeOutingApplicationExcelFile(outingApplicationExcelVos)
 
     override fun writeDaybreakStudyApplicationExcelFile(applications: List<DaybreakStudyApplicationVO>) =
         writeFilePort.writeDaybreakStudyApplicationExcelFile(applications)
