f028cac49b237f517dbd2d8ef900ebaa8e3a09a1
hansaem <ihansaem60@gmail.com>
2026-07-22 09:41:23 +0900

    chore: (#1034) dms-gateway 모듈 삭제

--- dms-gateway/gateway-core/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-    // Spring Cloud Gateway
-    implementation(Dependencies.SPRING_CLOUD_GATEWAY)
-}
-
-tasks.getByName<Jar>("jar") {
-    enabled = true
-    archiveClassifier.set("")
-}
